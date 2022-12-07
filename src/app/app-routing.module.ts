7d9b63e8c30bbc07a7aef3ee3a7bddca6b2b2e8c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-07 16:25:59 +0100

    Add MainPageComponent to the routing, make a use of router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './pages/main/main-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
