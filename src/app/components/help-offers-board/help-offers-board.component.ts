b86f068db9a3b5801eeb7b4229b326baf362ffc7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-08 13:34:11 +0100

    Create HelpOffersBoardComponent

---
+++ src/app/components/help-offers-board/help-offers-board.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-offers-board',
+  templateUrl: './help-offers-board.component.html',
+  styleUrls: ['./help-offers-board.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpOffersBoardComponent {
+
+}
