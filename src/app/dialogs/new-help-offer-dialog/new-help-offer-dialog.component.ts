8f7326f35f24bbd62e3fd5e27c847637d1b27a79
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:54:48 +0100

    Create base NewHelpOfferDialogComponent

---
+++ src/app/dialogs/new-help-offer-dialog/new-help-offer-dialog.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-help-offer-dialog',
+  templateUrl: './new-help-offer-dialog.component.html',
+  styleUrls: ['./new-help-offer-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NewHelpOfferDialogComponent {
+
+}
