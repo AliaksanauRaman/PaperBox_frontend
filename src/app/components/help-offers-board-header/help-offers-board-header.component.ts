fd003f9a3e03afd40f36b3441b6dac8aa5c39af3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:56:19 +0100

    Handle help button click and make a use of new service

--- src/app/components/help-offers-board-header/help-offers-board-header.component.ts
+++ src/app/components/help-offers-board-header/help-offers-board-header.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { HelpOffersService } from './../../services/help-offers.service';
+
 @Component({
   selector: 'app-help-offers-board-header',
   templateUrl: './help-offers-board-header.component.html',
@@ -7,5 +9,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HelpOffersBoardHeaderComponent {
+  constructor(
+    private readonly helpOffersService: HelpOffersService,
+  ) {}
 
+  protected handleHelpButtonClick(): void {
+    this.helpOffersService.openNewHelpOfferDialog();
+  }
 }
