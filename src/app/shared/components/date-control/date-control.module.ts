be1aebb94ff865a77c782a1e08a040f5a3fd1883
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 15:10:52 +0100

    Add CommonModule to the imports of DateControlModule

--- src/app/shared/components/date-control/date-control.module.ts
+++ src/app/shared/components/date-control/date-control.module.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { DateControlComponent } from './date-control.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   declarations: [
     DateControlComponent,
   ],
