f1c3200f7c7aaad88b83536976b8c047b54b6659
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:24:39 +0100

    Create GridOfHelpOffersComponent, add some layoting here

---
+++ src/app/components/grid-of-help-offers/grid-of-help-offers.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-of-help-offers',
+  templateUrl: './grid-of-help-offers.component.html',
+  styleUrls: ['./grid-of-help-offers.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GridOfHelpOffersComponent {
+
+}
