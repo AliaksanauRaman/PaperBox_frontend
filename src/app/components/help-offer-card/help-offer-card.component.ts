4a5b23cfe99f66b142e475d9c1b79dad68cdedcb
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:24:12 +0100

    Create HelpOfferCardComponent, add some layoting here

---
+++ src/app/components/help-offer-card/help-offer-card.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-offer-card',
+  templateUrl: './help-offer-card.component.html',
+  styleUrls: ['./help-offer-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpOfferCardComponent {
+
+}
