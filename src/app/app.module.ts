0836b22c211cada79ef690b7b77797f94c164c79
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:25:51 +0100

    Import MatCardModule and MatButtonModule to the AppModule, declare GridOfHelpOffersComponent, HelpOfferCardComponent and HelpOffersBoardHeaderComponent here as well

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -13,6 +15,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
 import { HelpOffersBoardComponent } from './components/help-offers-board/help-offers-board.component';
 import { HelpRequestsBoardComponent } from './components/help-requests-board/help-requests-board.component';
+import { GridOfHelpOffersComponent } from './components/grid-of-help-offers/grid-of-help-offers.component';
+import { HelpOfferCardComponent } from './components/help-offer-card/help-offer-card.component';
+import { HelpOffersBoardHeaderComponent } from './components/help-offers-board-header/help-offers-board-header.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,9 @@ import { HelpRequestsBoardComponent } from './components/help-requests-board/hel
     ContentComponent,
     HelpOffersBoardComponent,
     HelpRequestsBoardComponent,
+    GridOfHelpOffersComponent,
+    HelpOfferCardComponent,
+    HelpOffersBoardHeaderComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +38,8 @@ import { HelpRequestsBoardComponent } from './components/help-requests-board/hel
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
