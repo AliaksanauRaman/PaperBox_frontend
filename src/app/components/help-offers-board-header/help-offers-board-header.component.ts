1b29f78e4cc28f228b9e48f9311453cbeb47c6ba
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-14 14:23:34 +0100

    Create HelpOffersBoardHeaderComponent, add some layoting here

---
+++ src/app/components/help-offers-board-header/help-offers-board-header.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-offers-board-header',
+  templateUrl: './help-offers-board-header.component.html',
+  styleUrls: ['./help-offers-board-header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpOffersBoardHeaderComponent {
+
+}
