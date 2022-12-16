370a7e01b0ad03536466c353ced6937b2bbb2d9e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-16 15:44:14 +0100

    Rework rouring in the app, use new pages and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MainPageComponent } from './pages/main/main-page.component';
+import { InnerPageLayoutComponent } from './layouts/inner-page-layout/inner-page-layout.component';
+
+import { HelpOffersPageComponent } from './pages/help-offers-page/help-offers-page.component';
+import { HelpRequestsPageComponent } from './pages/help-requests-page/help-requests-page.component';
+
+// TODO
+// import { MainPageComponent } from './pages/main/main-page.component';
 
 const routes: Routes = [
+  // TODO
+  // {
+  //   path: '',
+  //   component: MainPageComponent,
+  // }
   {
     path: '',
-    component: MainPageComponent,
-  }
+    component: InnerPageLayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/help-offers',
+      },
+      {
+        path: 'help-offers',
+        component: HelpOffersPageComponent,
+      },
+      {
+        path: 'help-requests',
+        component: HelpRequestsPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '/help-offers',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
