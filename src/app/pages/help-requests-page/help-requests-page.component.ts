fd3b61e044ebce63011c2d96f3d1fafcee2b99fc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-16 15:40:20 +0100

    Create HelpRequestsPageComponent

---
+++ src/app/pages/help-requests-page/help-requests-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-requests-page',
+  templateUrl: './help-requests-page.component.html',
+  styleUrls: ['./help-requests-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpRequestsPageComponent {
+
+}
