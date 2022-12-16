967f0c1a259d7253062160fc1ee8a60d4421c11f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-16 15:40:52 +0100

    Create HelpOffersPageComponent

---
+++ src/app/pages/help-offers-page/help-offers-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-offers-page',
+  templateUrl: './help-offers-page.component.html',
+  styleUrls: ['./help-offers-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpOffersPageComponent {
+
+}
