8c07919b860e3158a98c89a6841fe4d42eaf76a2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 11:45:26 +0100

    Create AdminLoginPageComponent

---
+++ src/app/admin/pages/admin-login-page/admin-login-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login-page',
+  templateUrl: './admin-login-page.component.html',
+  styleUrls: ['./admin-login-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdminLoginPageComponent {
+
+}
