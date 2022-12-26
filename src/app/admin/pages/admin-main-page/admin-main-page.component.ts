75669de5550db1ac5a553c0d40600d91e3be109a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-26 11:45:42 +0100

    Create AdminMainPageComponent

---
+++ src/app/admin/pages/admin-main-page/admin-main-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-main-page',
+  templateUrl: './admin-main-page.component.html',
+  styleUrls: ['./admin-main-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdminMainPageComponent {
+
+}
