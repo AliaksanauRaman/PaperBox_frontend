8304b9dca8e6a124b49826835cbbc21077d55261
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-27 17:24:11 +0100

    Facilitate RoutingService, leave only methods which are still used

--- src/app/services/routing.service.ts
+++ src/app/services/routing.service.ts
@@ -9,16 +9,8 @@ export class RoutingService {
     private readonly router: Router,
   ) {}
 
-  public async navigateToHomePage(): Promise<boolean> {
-    return this.router.navigate(['/']);
-  }
-
-  public async navigateToAdminLoginPage(): Promise<boolean> {
-    return this.router.navigate(['/admin/login']);
-  }
-
   public async navigateToAdminMainPage(): Promise<boolean> {
-    return this.router.navigate(['/admin/main']);
+    return this.router.navigate(['/admin/']);
   }
 }
 
