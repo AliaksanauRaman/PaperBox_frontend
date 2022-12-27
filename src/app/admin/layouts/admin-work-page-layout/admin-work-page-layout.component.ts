c20673cdf3c4fa2ae5ac541f320a8a3cc82d4978
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-27 17:21:19 +0100

    Create AdminWorkPageLayoutComponent

---
+++ src/app/admin/layouts/admin-work-page-layout/admin-work-page-layout.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-work-page-layout',
+  templateUrl: './admin-work-page-layout.component.html',
+  styleUrls: ['./admin-work-page-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdminWorkPageLayoutComponent {
+
+}
