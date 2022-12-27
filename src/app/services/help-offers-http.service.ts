c1b1d50af977ce13ff7a22b4c0a2ca520c852dc7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-27 11:38:07 +0100

    Create HelpOffersHttpService and add a couple of methods there

---
+++ src/app/services/help-offers-http.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { API_URL } from "../shared/dependencies/api-url";
+import { CreateHelpOfferDto } from "../shared/dtos/create-help-offer.dto";
+import { HelpOfferPublicPreviewType } from "../shared/types/help-offer-public-preview.type";
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HelpOffersHttpService {
+  constructor(
+    @Inject(API_URL)
+    private readonly apiUrl: string,
+    private readonly httpClient: HttpClient,
+  ) {}
+
+  public getPublicPreviewsOfPublished(): Observable<Array<HelpOfferPublicPreviewType>> {
+    return this.httpClient.get<Array<HelpOfferPublicPreviewType>>(
+      `${this.apiUrl}/help-offers/public-previews-of-published`
+    );
+  }
+
+  public createOneUnpublished(createHelpOfferDto: CreateHelpOfferDto): Observable<unknown> {
+    return this.httpClient.post(
+      `${this.apiUrl}/help-offers/create-one-unpublished`,
+      createHelpOfferDto,
+    );
+  }
+}
