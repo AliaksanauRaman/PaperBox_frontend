292c6dc6c5cf436712b1d6b47936b93972b72f54
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-27 15:09:41 +0100

    Create DestroyEmitter abstract class

---
+++ src/app/shared/abstracts/destroy-emitter.class.ts
@@ -0,0 +1,12 @@
+import { Injectable, OnDestroy } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Injectable()
+export abstract class DestroyEmitter implements OnDestroy {
+  protected readonly destroy$ = new Subject<void>();
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
