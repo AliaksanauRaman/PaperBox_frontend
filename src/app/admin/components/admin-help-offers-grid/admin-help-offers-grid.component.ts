5de714d40953815fab8f2b55356b00a3ec0cdc10
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-29 15:48:55 +0100

    Create AdminHelpOffersGridComponent and add its base implementation

---
+++ src/app/admin/components/admin-help-offers-grid/admin-help-offers-grid.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { HelpOfferFullPreviewType } from '../../../shared/types/help-offer-full-preview.type';
+
+@Component({
+  selector: 'app-admin-help-offers-grid',
+  templateUrl: './admin-help-offers-grid.component.html',
+  styleUrls: ['./admin-help-offers-grid.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdminHelpOffersGridComponent {
+  @Input()
+  set fullPreviewsOfAllHelpOffers(value: Array<HelpOfferFullPreviewType>) {
+    // TODO: More strict types check?
+    if (!Array.isArray(value)) {
+      throw new Error('Only arrays are allowed here!');
+    }
+
+    this._fullPreviewsOfAllHelpOffers = value;
+  }
+
+  protected _fullPreviewsOfAllHelpOffers: Array<HelpOfferFullPreviewType> = [];
+}
