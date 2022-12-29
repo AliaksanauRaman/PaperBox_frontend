fbf40d228cbc5a77e36db1c25589b2da09e72037
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-29 15:51:03 +0100

    Declare new components, import modules and provide services to AdminModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,7 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+import { HttpErrorResponseCardModule } from './../shared/components/http-error-response-card/http-error-response-card.module';
 import { AdminRoutingModule } from './admin-routing.module';
 
 import { AdminLoginPageComponent } from './pages/admin-login-page/admin-login-page.component';
@@ -10,14 +12,19 @@ import { AdminLoginCardComponent } from './components/admin-login-card/admin-log
 import { AdminHelpOffersPageComponent } from './pages/admin-help-offers-page/admin-help-offers-page.component';
 import { AdminWorkPageLayoutComponent } from './layouts/admin-work-page-layout/admin-work-page-layout.component';
 import { AdminWorkPageHeaderComponent } from './components/admin-work-page-header/admin-work-page-header.component';
+import { AdminHelpOffersGridComponent } from './components/admin-help-offers-grid/admin-help-offers-grid.component';
+import { AdminHelpOfferCardComponent } from './components/admin-help-offer-card/admin-help-offer-card.component';
 
 import { AdminHelpOffersHttpService } from './services/admin-help-offers-http.service';
+import { AdminHelpOffersService } from './services/admin-help-offers.service';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
+    HttpErrorResponseCardModule,
     AdminRoutingModule,
   ],
   declarations: [
@@ -25,10 +32,13 @@ import { AdminHelpOffersHttpService } from './services/admin-help-offers-http.se
     AdminLoginCardComponent,
     AdminHelpOffersPageComponent,
     AdminWorkPageLayoutComponent,
-    AdminWorkPageHeaderComponent
+    AdminWorkPageHeaderComponent,
+    AdminHelpOffersGridComponent,
+    AdminHelpOfferCardComponent
   ],
   providers: [
     AdminHelpOffersHttpService,
+    AdminHelpOffersService,
   ],
   exports: [],
 })
