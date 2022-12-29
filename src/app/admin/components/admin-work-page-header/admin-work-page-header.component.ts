73f33c3645259709f2cacf8e5b57993823ff183e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-29 15:44:36 +0100

    Enhance and finish AdminWorkPageHeaderComponent

--- src/app/admin/components/admin-work-page-header/admin-work-page-header.component.ts
+++ src/app/admin/components/admin-work-page-header/admin-work-page-header.component.ts
@@ -1,23 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+
+import { LIST_OF_NAVIGATION_LINKS, NAVIGATION_LINKS } from './admin-work-page-toolbar.config';
+import { NavigationLinkType } from '../../../shared/types/navigation-link.type';
 
 @Component({
   selector: 'app-admin-work-page-header',
   templateUrl: './admin-work-page-header.component.html',
   styleUrls: ['./admin-work-page-header.component.scss'],
+  providers: [
+    {
+      provide: NAVIGATION_LINKS,
+      useValue: LIST_OF_NAVIGATION_LINKS,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminWorkPageHeaderComponent {
-  // TODO: Temp
-  protected readonly links = [
-    {
-      label: 'Help offers',
-      path: '/admin/help-offers',
-      disabled: false,
-    },
-    {
-      label: 'Help requests',
-      path: '/admin/help-requests',
-      disabled: true,
-    },
-  ];
+  constructor(
+    @Inject(NAVIGATION_LINKS)
+    public readonly navigationLinks: ReadonlyArray<NavigationLinkType>,
+  ) {}
 }
