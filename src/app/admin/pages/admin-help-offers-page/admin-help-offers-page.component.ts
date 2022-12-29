fee6c7547bab4979303c75b2bc8720b5e8c6399d
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-29 15:46:05 +0100

    Finish AdminHelpOffersPageComponent

--- src/app/admin/pages/admin-help-offers-page/admin-help-offers-page.component.ts
+++ src/app/admin/pages/admin-help-offers-page/admin-help-offers-page.component.ts
@@ -1,18 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
 
-import { AdminHelpOffersHttpService } from '../../services/admin-help-offers-http.service';
+import { AdminHelpOffersService } from '../../services/admin-help-offers.service';
+
+import { DestroyEmitter } from '../../../shared/abstracts/destroy-emitter.class';
+import { HttpRequestStateName } from '../../../shared/enums/http-request-state-name.enum';
 
 @Component({
   selector: 'app-admin-help-offers-page',
   templateUrl: './admin-help-offers-page.component.html',
   styleUrls: ['./admin-help-offers-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminHelpOffersPageComponent {
-  protected readonly fullPreviewsOfAllHelpOffers$ = this.adminHelpOffersHttpService
-    .getFullPreviewsOfAll();
+export class AdminHelpOffersPageComponent
+  extends DestroyEmitter
+  implements OnInit
+{
+  public readonly getFullPreviewsOfAllRequestStateStream =
+    this.adminHelpOffersService.getFullPreviewsOfAllRequestStateStream;
+  public readonly httpRequestStateName = HttpRequestStateName;
 
-  constructor(
-    private readonly adminHelpOffersHttpService: AdminHelpOffersHttpService,
-  ) {}
+  constructor(private readonly adminHelpOffersService: AdminHelpOffersService) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.adminHelpOffersService
+      .makeGetFullPreviewsOfAllRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe();
+  }
 }
