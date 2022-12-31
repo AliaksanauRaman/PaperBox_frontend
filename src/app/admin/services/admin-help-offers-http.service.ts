77b55751aa8acf91bbe7eb7104e56c0ba527d851
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-12-31 11:52:06 +0100

    Add getOneFullById and publishOneWithId methods to AdminHelpOffersHttpService

--- src/app/admin/services/admin-help-offers-http.service.ts
+++ src/app/admin/services/admin-help-offers-http.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Inject } from "@angular/core";
+import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { API_URL } from '../../shared/dependencies/api-url/injection-token';
 import { HelpOfferFullPreviewType } from '../../shared/types/help-offer-full-preview.type';
+import { FullHelpOfferType } from '../types/full-help-offer.type';
 
 @Injectable()
 export class AdminHelpOffersHttpService {
   constructor(
     @Inject(API_URL)
     private readonly apiUrl: string,
-    private readonly httpClient: HttpClient,
+    private readonly httpClient: HttpClient
   ) {}
 
   public getFullPreviewsOfAll(): Observable<Array<HelpOfferFullPreviewType>> {
@@ -18,5 +19,18 @@ export class AdminHelpOffersHttpService {
       `${this.apiUrl}/help-offers/full-previews-of-all`
     );
   }
-}
 
+  public getOneFullById(helpOfferId: string): Observable<FullHelpOfferType> {
+    return this.httpClient.get<FullHelpOfferType>(
+      `${this.apiUrl}/help-offers/full/${helpOfferId}`
+    );
+  }
+
+  public publishOneWithId(helpOfferId: string): Observable<unknown> {
+    // TODO: Think about empty body
+    return this.httpClient.patch<unknown>(
+      `${this.apiUrl}/help-offers/publish-one/${helpOfferId}`,
+      {}
+    );
+  }
+}
