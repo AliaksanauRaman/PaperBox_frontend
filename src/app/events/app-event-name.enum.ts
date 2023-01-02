5f12287df2301d78e4a708e13f71db6beca62f13
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 13:41:36 +0100

    Introduce events and AppEventName enum

---
+++ src/app/events/app-event-name.enum.ts
@@ -0,0 +1,13 @@
+export enum AppEventName {
+  MAKE_GET_FULL_PREVIEWS_OF_ALL_HELP_OFFERS_REQUEST = 'make-get-full-previews-of-all-help-offers-request',
+  SUCCESS_GET_FULL_PREVIEWS_OF_ALL_HELP_OFFERS = 'success-get-full-previews-of-all-help-offers',
+  FAILED_GET_FULL_PREVIEWS_OF_ALL_HELP_OFFERS = 'failed-get-full-previews-of-all-help-offers',
+
+  MAKE_GET_ONE_FULL_HELP_OFFER_REQUEST = 'make-get-one-full-help-offer-request',
+  SUCCESS_GET_ONE_FULL_HELP_OFFER = 'success-get-one-full-help-offer',
+  FAILED_GET_ONE_FULL_HELP_OFFER = 'failed-get-one-full-help-offer',
+
+  MAKE_HELP_OFFER_STATUS_UPDATE_REQUEST = 'make-help-offer-status-update-request',
+  SUCCESS_HELP_OFFER_STATUS_UPDATE = 'success-help-offer-status-update',
+  FAILED_HELP_OFFER_STATUS_UPDATE = 'failed-help-offer-status-update',
+}
