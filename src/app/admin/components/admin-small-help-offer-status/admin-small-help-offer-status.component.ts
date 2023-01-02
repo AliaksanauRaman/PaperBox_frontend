b71686f774ec36e82f66064381449aefe878efab
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 15:33:15 +0100

    Add sizes logic to the AdminSmallHelpOfferStatusComponent

--- src/app/admin/components/admin-small-help-offer-status/admin-small-help-offer-status.component.ts
+++ src/app/admin/components/admin-small-help-offer-status/admin-small-help-offer-status.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { HelpOfferStatus } from '../../../shared/enums/help-offer-status.enum';
 
+type StatusSize = 'small' | 'big';
+
 @Component({
   selector: 'app-admin-small-help-offer-status',
   templateUrl: './admin-small-help-offer-status.component.html',
@@ -14,6 +16,12 @@ export class AdminSmallHelpOfferStatusComponent {
     this._status = value;
   }
 
+  @Input()
+  public set size(value: StatusSize) {
+    this._size = value;
+  }
+
   protected _status?: HelpOfferStatus;
+  protected _size = 'small';
   protected readonly helpOfferStatus = HelpOfferStatus;
 }
