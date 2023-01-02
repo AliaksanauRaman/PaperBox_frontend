7a1bbc0a7f9f5cd0a3fd0b453d1b229c2c98a260
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 15:10:07 +0100

    Create AdminSmallHelpOfferStatusComponent

---
+++ src/app/admin/components/admin-small-help-offer-status/admin-small-help-offer-status.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-small-help-offer-status',
+  templateUrl: './admin-small-help-offer-status.component.html',
+  styleUrls: ['./admin-small-help-offer-status.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdminSmallHelpOfferStatusComponent {
+
+}
