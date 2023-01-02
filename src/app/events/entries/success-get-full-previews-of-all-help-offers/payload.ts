1d33d57b280b87ae642305971b3eecb83d6c2acc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 13:45:20 +0100

    Create SuccessGetFullPreviewsOfAllHelpOffers event

---
+++ src/app/events/entries/success-get-full-previews-of-all-help-offers/payload.ts
@@ -0,0 +1,4 @@
+import { HelpOfferFullPreviewType } from './../../../shared/types/help-offer-full-preview.type';
+
+export type SuccessGetFullPreviewsOfAllHelpOffersPayload =
+  ReadonlyArray<HelpOfferFullPreviewType>;
