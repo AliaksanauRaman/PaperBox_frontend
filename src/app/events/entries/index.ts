d4220c2bb87e7933f10d1a351def665698413789
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 13:47:14 +0100

    Create index.ts files for events

---
+++ src/app/events/entries/index.ts
@@ -0,0 +1,11 @@
+export * from './make-get-full-previews-of-all-help-offers-request';
+export * from './success-get-full-previews-of-all-help-offers';
+export * from './failed-get-full-previews-of-all-help-offers';
+
+export * from './make-get-one-full-help-offer-request';
+export * from './success-get-one-full-help-offer';
+export * from './failed-get-one-full-help-offer';
+
+export * from './make-help-offer-status-update-request';
+export * from './success-help-offer-status-update';
+export * from './failed-help-offer-status-update';
---
+++ src/app/events/index.ts
@@ -0,0 +1,4 @@
+export * from './app-event-bus.service';
+export * from './app-event-name.enum';
+
+export * from './entries';
