bfeb90e495d072d077ae13a94f988e70423af6a7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 14:03:32 +0100

    Add handleManageButtonClick to AdminHelpOfferCardComponent

--- src/app/admin/components/admin-help-offer-card/admin-help-offer-card.component.ts
+++ src/app/admin/components/admin-help-offer-card/admin-help-offer-card.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+import { AdminHelpOffersService } from '../../services/admin-help-offers.service';
+
 import { HelpOfferFullPreviewType } from '../../../shared/types/help-offer-full-preview.type';
 import { HelpOfferStatus } from '../../../shared/enums/help-offer-status.enum';
 
@@ -17,4 +19,18 @@ export class AdminHelpOfferCardComponent {
 
   protected _helpOfferFullPreview?: HelpOfferFullPreviewType;
   protected readonly helpOfferStatus = HelpOfferStatus;
+
+  constructor(
+    private readonly adminHelpOffersService: AdminHelpOffersService,
+  ) {}
+
+  public handleManageButtonClick(): void {
+    if (this._helpOfferFullPreview === undefined) {
+      throw new Error('Help offer full preview cannot be undefined!');
+    }
+
+    this.adminHelpOffersService.openManageHelpOfferDialog(
+      this._helpOfferFullPreview.id,
+    );
+  }
 }
