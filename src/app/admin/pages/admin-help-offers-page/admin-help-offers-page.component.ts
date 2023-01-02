c483c0c5a13991e3de1641ed2aa52ebd5173f5aa
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-02 14:14:04 +0100

    Rework AdminHelpOffersPageComponent make it to rely on event bus instead of specific services streams

--- src/app/admin/pages/admin-help-offers-page/admin-help-offers-page.component.ts
+++ src/app/admin/pages/admin-help-offers-page/admin-help-offers-page.component.ts
@@ -1,33 +1,91 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { takeUntil } from 'rxjs';
+import { BehaviorSubject, takeUntil, tap } from 'rxjs';
 
-import { AdminHelpOffersService } from '../../services/admin-help-offers.service';
+import { AdminHelpOffersPageService } from './admin-help-offers-page.service';
 
+import {
+  AppEventBusService,
+  AppEventName,
+  MakeGetFullPreviewsOfAllHelpOffersRequest,
+  SuccessGetFullPreviewsOfAllHelpOffersPayload,
+  SuccessHelpOfferStatusUpdatePayload,
+} from './../../../events';
 import { DestroyEmitter } from '../../../shared/abstracts/destroy-emitter.class';
 import { HttpRequestStateName } from '../../../shared/enums/http-request-state-name.enum';
+import { HelpOfferFullPreviewType } from '../../../shared/types/help-offer-full-preview.type';
 
 @Component({
   selector: 'app-admin-help-offers-page',
   templateUrl: './admin-help-offers-page.component.html',
   styleUrls: ['./admin-help-offers-page.component.scss'],
+  providers: [AdminHelpOffersPageService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminHelpOffersPageComponent
   extends DestroyEmitter
   implements OnInit
 {
-  public readonly getFullPreviewsOfAllRequestStateStream =
-    this.adminHelpOffersService.getFullPreviewsOfAllRequestStateStream;
+  private readonly _fullPreviewsOfAllHelpOffers$ = new BehaviorSubject<
+    ReadonlyArray<HelpOfferFullPreviewType>
+  >([]);
+  public readonly fullPreviewsOfAllHelpOffers$ =
+    this._fullPreviewsOfAllHelpOffers$.asObservable();
+
   public readonly httpRequestStateName = HttpRequestStateName;
 
-  constructor(private readonly adminHelpOffersService: AdminHelpOffersService) {
+  constructor(
+    private readonly eventBusService: AppEventBusService,
+    private readonly service: AdminHelpOffersPageService
+  ) {
     super();
   }
 
   public ngOnInit(): void {
-    this.adminHelpOffersService
-      .makeGetFullPreviewsOfAllRequest()
-      .pipe(takeUntil(this.destroy$))
+    this.eventBusService.emit(new MakeGetFullPreviewsOfAllHelpOffersRequest());
+
+    this.subToSuccessGetFullPreviewsOfAllHelpOffers();
+    this.subToSuccessHelpOfferStatusUpdate();
+  }
+
+  private subToSuccessGetFullPreviewsOfAllHelpOffers(): void {
+    this.eventBusService
+      .on<SuccessGetFullPreviewsOfAllHelpOffersPayload>(
+        AppEventName.SUCCESS_GET_FULL_PREVIEWS_OF_ALL_HELP_OFFERS
+      )
+      .pipe(
+        tap((fullPreviewsOfAllHelpOffers) =>
+          this._fullPreviewsOfAllHelpOffers$.next(fullPreviewsOfAllHelpOffers)
+        ),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  // TODO: Move logic to services
+  private subToSuccessHelpOfferStatusUpdate(): void {
+    this.eventBusService
+      .on<SuccessHelpOfferStatusUpdatePayload>(
+        AppEventName.SUCCESS_HELP_OFFER_STATUS_UPDATE
+      )
+      .pipe(
+        tap(({ id, newStatus }) => {
+          const currentFullPreviewsOfAllHelpOffers =
+            this._fullPreviewsOfAllHelpOffers$.getValue();
+          this._fullPreviewsOfAllHelpOffers$.next(
+            currentFullPreviewsOfAllHelpOffers.map((preview) => {
+              if (preview.id !== id) {
+                return preview;
+              }
+
+              return {
+                ...preview,
+                status: newStatus,
+              };
+            })
+          );
+        }),
+        takeUntil(this.destroy$)
+      )
       .subscribe();
   }
 }
