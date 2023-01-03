44755288c90b65469592d3785594deb3d6323df8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-03 15:35:35 +0100

    Enhance AdminHelpOffersHttpService a bit, add deleteOneById method

--- src/app/admin/services/admin-help-offers-http.service.ts
+++ src/app/admin/services/admin-help-offers-http.service.ts
@@ -7,9 +7,12 @@ import { HelpOfferFullPreviewType } from '../../shared/types/help-offer-full-pre
 import { FullHelpOfferType } from '../types/full-help-offer.type';
 import { UpdateHelpOfferStatusResponseType } from '../types/update-help-offer-status-response.type';
 import { HelpOfferStatus } from '../../shared/enums/help-offer-status.enum';
+import { DeleteHelpOfferResponseType } from '../types/delete-help-offer-response.type';
 
 @Injectable()
 export class AdminHelpOffersHttpService {
+  private readonly helpOffersApiUrl = `${this.apiUrl}/help-offers`;
+
   constructor(
     @Inject(API_URL)
     private readonly apiUrl: string,
@@ -18,13 +21,13 @@ export class AdminHelpOffersHttpService {
 
   public getFullPreviewsOfAll(): Observable<Array<HelpOfferFullPreviewType>> {
     return this.httpClient.get<Array<HelpOfferFullPreviewType>>(
-      `${this.apiUrl}/help-offers/full-previews-of-all`
+      `${this.helpOffersApiUrl}/full-previews-of-all`
     );
   }
 
   public getOneFullById(helpOfferId: string): Observable<FullHelpOfferType> {
     return this.httpClient.get<FullHelpOfferType>(
-      `${this.apiUrl}/help-offers/full/${helpOfferId}`
+      `${this.helpOffersApiUrl}/one-full/${helpOfferId}`
     );
   }
 
@@ -33,8 +36,16 @@ export class AdminHelpOffersHttpService {
     newStatus: HelpOfferStatus
   ): Observable<UpdateHelpOfferStatusResponseType> {
     return this.httpClient.patch<UpdateHelpOfferStatusResponseType>(
-      `${this.apiUrl}/help-offers/update-status-of-one/${helpOfferId}`,
+      `${this.helpOffersApiUrl}/update-status-of-one/${helpOfferId}`,
       { newStatus }
+    );
+  }
+
+  public deleteOneById(
+    helpOfferId: string
+  ): Observable<DeleteHelpOfferResponseType> {
+    return this.httpClient.delete<DeleteHelpOfferResponseType>(
+      `${this.helpOffersApiUrl}/one/${helpOfferId}`
     );
   }
 }
