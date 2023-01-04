af81f85c301fccb5db045cf24aed34177262331b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-04 13:47:31 +0100

    Finish AdminManageHelpOfferDialogComponent

--- src/app/admin/dialogs/admin-manage-help-offer-dialog/admin-manage-help-offer-dialog.component.ts
+++ src/app/admin/dialogs/admin-manage-help-offer-dialog/admin-manage-help-offer-dialog.component.ts
@@ -4,14 +4,24 @@ import {
   Inject,
   OnInit,
 } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  BehaviorSubject,
+  delay,
+  of,
+  timer,
+  tap,
+  take,
+  combineLatest,
+  map,
+  takeUntil,
+} from 'rxjs';
 
 import { DestroyEmitter } from '../../../shared/abstracts/destroy-emitter.class';
 import { ManageHelpOfferDialogData } from '../../classes/manage-help-offer-dialog-data.class';
 import { HelpOfferStatus } from '../../../shared/enums/help-offer-status.enum';
 
 import { AdminOneFullHelpOfferService } from '../../services/admin-one-full-help-offer.service';
-import { combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-admin-manage-help-offer-dialog',
@@ -24,14 +34,15 @@ export class AdminManageHelpOfferDialogComponent
   extends DestroyEmitter
   implements OnInit
 {
-  protected readonly helpOfferStatus = HelpOfferStatus;
-
   public readonly viewContext$ = combineLatest([
     this.oneFullHelpOfferService.fullHelpOffer$,
     this.oneFullHelpOfferService.getByIdInProgress$,
     this.oneFullHelpOfferService.getByIdError$,
     this.oneFullHelpOfferService.updateStatusInProgress$,
     this.oneFullHelpOfferService.updateStatusError$,
+    this.oneFullHelpOfferService.deleteByIdInProgress$,
+    this.oneFullHelpOfferService.deleteByIdError$,
+    this.oneFullHelpOfferService.deleted$,
   ]).pipe(
     map(
       ([
@@ -40,19 +51,39 @@ export class AdminManageHelpOfferDialogComponent
         getByIdError,
         updateStatusInProgress,
         updateStatusError,
+        deleteByIdInProgress,
+        deleteByIdError,
+        deleted,
       ]) => ({
         fullHelpOffer,
         getByIdInProgress,
         getByIdError,
         updateStatusInProgress,
         updateStatusError,
+        deleteByIdInProgress,
+        deleteByIdError,
+        deleted,
       })
     )
   );
 
+  protected readonly helpOfferStatus = HelpOfferStatus;
+  protected readonly deleteButtonEnabled$ = of(true).pipe(delay(3000));
+
+  // TODO: Refactor, move to a separate service
+  protected readonly spinnerLength$ = new BehaviorSubject(100);
+  private myTimer = timer(0, 3000 / 100)
+    .pipe(
+      tap(() => this.spinnerLength$.next(this.spinnerLength$.getValue() - 1)),
+      take(100),
+      takeUntil(this.destroy$)
+    )
+    .subscribe();
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
     private readonly data: ManageHelpOfferDialogData,
+    private readonly dialogRef: MatDialogRef<AdminManageHelpOfferDialogComponent>,
     private readonly oneFullHelpOfferService: AdminOneFullHelpOfferService
   ) {
     super();
@@ -60,6 +91,10 @@ export class AdminManageHelpOfferDialogComponent
 
   public ngOnInit(): void {
     this.oneFullHelpOfferService.makeRequestToGetById(this.data.helpOfferId);
+  }
+
+  public closeDialog(): void {
+    this.dialogRef.close();
   }
 
   protected handlePublishButtonClick(): void {
@@ -81,5 +116,9 @@ export class AdminManageHelpOfferDialogComponent
       this.data.helpOfferId,
       this.helpOfferStatus.REJECTED
     );
+  }
+
+  protected handleDeleteButtonClick(): void {
+    this.oneFullHelpOfferService.makeRequestToDeleteById(this.data.helpOfferId);
   }
 }
