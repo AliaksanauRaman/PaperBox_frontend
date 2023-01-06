160a00b3b7535b3ab93a4723291f8be924b9a5cb
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 13:16:07 +0100

    Create MainComponent and delcare it in CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { IndexPageLayoutComponent } from './layouts/index-page-layout/index-page-layout.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 const CORE_DECLARATIONS = [
   IndexPageLayoutComponent,
   HeaderComponent,
+  MainComponent,
   FooterComponent,
 ];
 
