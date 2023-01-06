cade202d179fbe92376630c08336a7c0ec5ba641
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 12:46:22 +0100

    Create HelpRequestsMainViewComponent

---
+++ src/app/help-requests/views/help-requests-main-view/help-requests-main-view.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-requests-main-view',
+  templateUrl: './help-requests-main-view.component.html',
+  styleUrls: ['./help-requests-main-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpRequestsMainViewComponent {
+
+}
