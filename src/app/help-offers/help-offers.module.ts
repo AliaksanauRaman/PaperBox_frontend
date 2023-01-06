c3538971950b99e33aea947bb85ee7651a66bec0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 12:40:26 +0100

    Create HelpOffersModule

---
+++ src/app/help-offers/help-offers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { HelpOffersMainViewComponent } from './views/help-offers-main-view/help-offers-main-view.component';
+
+@NgModule({
+  imports: [],
+  declarations: [
+    HelpOffersMainViewComponent
+  ],
+  providers: [],
+  exports: [],
+})
+export class HelpOffersModule {}
