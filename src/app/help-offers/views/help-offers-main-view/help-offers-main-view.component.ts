ed5cdc392bfdbaa1475b9dace9e29a02707c0e3e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 12:40:03 +0100

    Create HelpOffersMainViewComponent

---
+++ src/app/help-offers/views/help-offers-main-view/help-offers-main-view.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-offers-main-view',
+  templateUrl: './help-offers-main-view.component.html',
+  styleUrls: ['./help-offers-main-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpOffersMainViewComponent {
+
+}
