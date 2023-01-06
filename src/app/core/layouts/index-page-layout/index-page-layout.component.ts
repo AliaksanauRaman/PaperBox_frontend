4177c058d98c4d695feff19448e1535914b0e04b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 12:56:21 +0100

    Create IndexPageLayoutComponent

---
+++ src/app/core/layouts/index-page-layout/index-page-layout.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-page-layout',
+  templateUrl: './index-page-layout.component.html',
+  styleUrls: ['./index-page-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IndexPageLayoutComponent {
+
+}
