8df83e42d4be8f7c21fa6fd71062f8b195c5fddd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 13:28:36 +0100

    Create LanguageSwitcherComponent and declare it in CoreModule

---
+++ src/app/core/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LanguageSwitcherComponent {
+
+}
