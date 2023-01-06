fbebd3985f58056abb0c552e7afa232782ac6308
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 16:21:05 +0100

    Create NavigationMenuComponent and declare it in CoreModule, import RouterModule and PlusIconModule there as well

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { PlusIconModule } from '../shared/icons/plus-icon/plus-icon.module';
 
 import { IndexPageLayoutComponent } from './layouts/index-page-layout/index-page-layout.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -6,6 +9,7 @@ import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
 import { ActionsComponent } from './components/actions/actions.component';
 import { MainComponent } from './components/main/main.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 const CORE_DECLARATIONS = [
@@ -15,11 +19,15 @@ const CORE_DECLARATIONS = [
   LanguageSwitcherComponent,
   ActionsComponent,
   MainComponent,
+  NavigationMenuComponent,
   FooterComponent,
 ];
 
 @NgModule({
-  imports: [],
+  imports: [
+    RouterModule,
+    PlusIconModule,
+  ],
   declarations: CORE_DECLARATIONS,
   exports: CORE_DECLARATIONS,
 })
