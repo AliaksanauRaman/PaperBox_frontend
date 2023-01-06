1cc5e54166d7b49507d538adf7b9d19763d09d20
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 13:22:33 +0100

    Create ActionsComponent and declare it in CoreModule

---
+++ src/app/core/components/actions/actions.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actions',
+  templateUrl: './actions.component.html',
+  styleUrls: ['./actions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ActionsComponent {
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { IndexPageLayoutComponent } from './layouts/index-page-layout/index-page-layout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ActionsComponent } from './components/actions/actions.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -10,6 +11,7 @@ const CORE_DECLARATIONS = [
   IndexPageLayoutComponent,
   HeaderComponent,
   ToolbarComponent,
+  ActionsComponent,
   MainComponent,
   FooterComponent,
 ];
