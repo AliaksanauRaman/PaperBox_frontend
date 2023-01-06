09d662a68fb4445acaff354568d2e54c0504f35b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 12:46:40 +0100

    Create HelpRequestsModule

---
+++ src/app/help-requests/help-requests.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HelpRequestsMainViewComponent } from './views/help-requests-main-view/help-requests-main-view.component';
+
+@NgModule({
+  imports: [],
+  declarations: [HelpRequestsMainViewComponent],
+  providers: [],
+  exports: [],
+})
+export class HelpRequestsModule {}
