bec963ee47da8cae4b497f7c6d54aaa1c4b1dcaa
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-06 16:59:28 +0100

    Create shared image FeedbackImageModule

---
+++ src/app/shared/images/feedback-image/feedback-image.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-image',
+  templateUrl: './feedback-image.component.html',
+  styleUrls: ['./feedback-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FeedbackImageComponent {
+
+}
---
+++ src/app/shared/images/feedback-image/feedback-image.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { FeedbackImageComponent } from './feedback-image.component';
+
+@NgModule({
+  declarations: [FeedbackImageComponent],
+  exports: [FeedbackImageComponent],
+})
+export class FeedbackImageModule {}
