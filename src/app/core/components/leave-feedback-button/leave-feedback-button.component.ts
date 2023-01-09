c0a5b342a428ac55b6c9ec008480ae98d04650db
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-09 10:17:18 +0100

    Create LeaveFeedbackButtonComponent and add it to CoreModule declarations

---
+++ src/app/core/components/leave-feedback-button/leave-feedback-button.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-feedback-button',
+  templateUrl: './leave-feedback-button.component.html',
+  styleUrls: ['./leave-feedback-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LeaveFeedbackButtonComponent {
+
+}
