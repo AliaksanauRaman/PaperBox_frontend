0857e06e456f2c1e1c32aaed2a18f3b70a53a204
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-09 12:28:37 +0100

    Create shared arrow down icon ArrowDownIconModule

---
+++ src/app/shared/icons/arrow-down-icon/arrow-down-icon.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow-down-icon',
+  templateUrl: './arrow-down-icon.component.html',
+  styleUrls: ['./arrow-down-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ArrowDownIconComponent {
+
+}
---
+++ src/app/shared/icons/arrow-down-icon/arrow-down-icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ArrowDownIconComponent } from './arrow-down-icon.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ArrowDownIconComponent],
+  providers: [],
+  exports: [ArrowDownIconComponent],
+})
+export class ArrowDownIconModule {}
