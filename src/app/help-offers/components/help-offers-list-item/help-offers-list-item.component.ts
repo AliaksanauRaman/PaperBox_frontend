c799ed70fa3269adf192301d37d9d75007c00cbe
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-09 15:55:27 +0100

    Add isHovered functionality to HelpOffersListItemComponent, add layouting and styling

--- src/app/help-offers/components/help-offers-list-item/help-offers-list-item.component.ts
+++ src/app/help-offers/components/help-offers-list-item/help-offers-list-item.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-help-offers-list-item',
@@ -7,5 +7,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HelpOffersListItemComponent {
+  @HostBinding('class.hovered')
+  public get isHovered(): boolean {
+    return this._isHovered;
+  }
 
+  @HostListener('mouseenter')
+  public handleHostMouseenter(): void {
+    this._isHovered = true;
+  }
+
+  @HostListener('mouseleave')
+  public handleHostMouseleave(): void {
+    this._isHovered = false;
+  }
+
+  protected _isHovered = false;
 }
