292e0dc33dbc2077e980396e72405e74aa41446f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 22:37:17 +0100

    Add base textarea field control component implementation

--- src/app/shared/components/textarea-field-control/textarea-field-control.component.ts
+++ src/app/shared/components/textarea-field-control/textarea-field-control.component.ts
@@ -1,11 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  forwardRef,
+  Input,
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { UniqueIdGeneratorService } from '../../../services/unique-id-generator.service';
+
+import { CustomControl } from '../../abstracts/custom-control.class';
 
 @Component({
   selector: 'app-textarea-field-control',
   templateUrl: './textarea-field-control.component.html',
   styleUrls: ['./textarea-field-control.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextareaFieldControlComponent),
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TextareaFieldControlComponent {
+export class TextareaFieldControlComponent extends CustomControl<string> {
+  @Input()
+  public set label(value: string) {
+    this.controlLabel = value;
+  }
 
+  protected controlValue = '';
+
+  constructor(
+    uniqueIdGeneratorService: UniqueIdGeneratorService,
+    cdRef: ChangeDetectorRef
+  ) {
+    super(uniqueIdGeneratorService, cdRef);
+  }
+
+  public writeValue(value: unknown): void {
+    if (!this.isStringOrNull(value)) {
+      throw new Error('Only strings and null are allowed!');
+    }
+
+    this.controlValue = value === null ? '' : value;
+    this.cdRef.markForCheck();
+  }
+
+  public handleControlValueChange(newValue: string): void {
+    this.controlValue = newValue;
+    this.onChange(this.controlValue);
+  }
+
+  // TODO: Move to a shared place
+  private isStringOrNull(value: unknown): value is string | null {
+    return typeof value === 'string' || value === null;
+  }
 }
--- src/app/shared/components/textarea-field-control/textarea-field-control.module.ts
+++ src/app/shared/components/textarea-field-control/textarea-field-control.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { TextareaFieldControlComponent } from './textarea-field-control.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [TextareaFieldControlComponent],
   providers: [],
   exports: [TextareaFieldControlComponent],
