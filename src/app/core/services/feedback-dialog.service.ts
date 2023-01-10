b572a903c8f67f0d845a57f53184447f8ed788be
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 20:48:15 +0100

    Create FeedbackDialogService

---
+++ src/app/core/services/feedback-dialog.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+
+import { FeedbackDialogComponent } from '../dialogs/feedback-dialog/feedback-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedbackDialogService {
+  constructor(
+    private readonly dialog: Dialog,
+  ) { }
+
+  public openDialog(): void {
+    this.dialog.open(FeedbackDialogComponent);
+  }
+}
