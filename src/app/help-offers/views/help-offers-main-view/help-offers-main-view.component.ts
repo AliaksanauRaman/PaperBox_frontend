c7999a03c1ce788e7db14f265c5d382c2169fadd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 20:07:50 +0100

    Add active view functionality to HelpOffersMainViewComponent

--- src/app/help-offers/views/help-offers-main-view/help-offers-main-view.component.ts
+++ src/app/help-offers/views/help-offers-main-view/help-offers-main-view.component.ts
@@ -1,11 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+} from '@angular/core';
+
+import { ActiveViewService } from '../../../core/services/active-view.service';
+
+import { ViewName } from '../../../shared/enums/view-name.enum';
 
 @Component({
   selector: 'app-help-offers-main-view',
   templateUrl: './help-offers-main-view.component.html',
   styleUrls: ['./help-offers-main-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelpOffersMainViewComponent {
+export class HelpOffersMainViewComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
+  constructor(private readonly activeViewService: ActiveViewService) {}
 
+  // TODO: Empty
+  public ngOnInit(): void {}
+
+  // TODO: Strange workaround, it is better to find a better solution
+  public ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.activeViewService.setActiveView(ViewName.HELP_OFFERS);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.activeViewService.resetActiveView();
+  }
 }
