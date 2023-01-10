1b71c430af897fd1a1803e3810735422db94bb9e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 22:25:20 +0100

    Create shared textaread field control component

---
+++ src/app/shared/components/textarea-field-control/textarea-field-control.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textarea-field-control',
+  templateUrl: './textarea-field-control.component.html',
+  styleUrls: ['./textarea-field-control.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TextareaFieldControlComponent {
+
+}
---
+++ src/app/shared/components/textarea-field-control/textarea-field-control.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { TextareaFieldControlComponent } from './textarea-field-control.component';
+
+@NgModule({
+  imports: [],
+  declarations: [TextareaFieldControlComponent],
+  providers: [],
+  exports: [TextareaFieldControlComponent],
+})
+export class TextareaFieldControlModule {}
