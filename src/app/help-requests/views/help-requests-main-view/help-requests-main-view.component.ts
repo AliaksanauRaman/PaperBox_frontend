83d5e5ef0a267027fd3dfd1a8c6340ebfb0037b3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 20:08:09 +0100

    Add active view functionality to HelpRequestsMainViewComponent

--- src/app/help-requests/views/help-requests-main-view/help-requests-main-view.component.ts
+++ src/app/help-requests/views/help-requests-main-view/help-requests-main-view.component.ts
@@ -1,11 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
+import { ActiveViewService } from '../../../core/services/active-view.service';
+
+import { ViewName } from '../../../shared/enums/view-name.enum';
 
 @Component({
   selector: 'app-help-requests-main-view',
   templateUrl: './help-requests-main-view.component.html',
   styleUrls: ['./help-requests-main-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelpRequestsMainViewComponent {
+export class HelpRequestsMainViewComponent
+  implements OnInit, AfterViewInit, OnDestroy
+{
+  constructor(private readonly activeViewService: ActiveViewService) {}
 
+  // TODO: Empty
+  public ngOnInit(): void {}
+
+  // TODO: Strange workaround, it is better to find a better solution
+  public ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.activeViewService.setActiveView(ViewName.HELP_REQUESTS);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.activeViewService.resetActiveView();
+  }
 }
