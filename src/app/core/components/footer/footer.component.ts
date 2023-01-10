a977eeff0b069843ba59e2a120c2990454636eba
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-10 20:47:28 +0100

    Add functionality to open feedback dialog to FooterComponent

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { FeedbackDialogService } from '../../services/feedback-dialog.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
+  constructor(private readonly feedbackDialogService: FeedbackDialogService) {}
 
+  public handleLeaveFeedbackButtonClick(): void {
+    this.feedbackDialogService.openDialog();
+  }
 }
