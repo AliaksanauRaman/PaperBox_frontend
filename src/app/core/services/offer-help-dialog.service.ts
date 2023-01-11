a880be02f5f5cb238e67859249c42c4a56a126e9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-11 12:13:38 +0100

    Create OfferHelpDialogService

---
+++ src/app/core/services/offer-help-dialog.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+
+import { OfferHelpDialogComponent } from '../dialogs/offer-help-dialog/offer-help-dialog.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OfferHelpDialogService {
+  constructor(private readonly dialog: Dialog) {}
+
+  public openDialog(): void {
+    this.dialog.open(OfferHelpDialogComponent);
+  }
+}
