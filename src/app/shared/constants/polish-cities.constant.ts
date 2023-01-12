230c2596b5c9690cc55da0fa893980cdf9095ba6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 21:24:57 +0100

    Create POLISH_CITIES constant

---
+++ src/app/shared/constants/polish-cities.constant.ts
@@ -0,0 +1,33 @@
+import { CityType } from '../types/city.type';
+import { PolishCityValue } from '../enums/polish-city-value.enum';
+
+export const POLISH_CITIES: ReadonlyArray<CityType<PolishCityValue>> = [
+  {
+    value: PolishCityValue.WARSAW,
+    label: 'city.polish.warsaw',
+  },
+  {
+    value: PolishCityValue.KRAKOW,
+    label: 'city.polish.krakow',
+  },
+  {
+    value: PolishCityValue.WROCLAW,
+    label: 'city.polish.wroclaw',
+  },
+  {
+    value: PolishCityValue.GDANSK,
+    label: 'city.polish.gdansk',
+  },
+  {
+    value: PolishCityValue.LODZ,
+    label: 'city.polish.lodz',
+  },
+  {
+    value: PolishCityValue.POZNAN,
+    label: 'city.polish.poznan',
+  },
+  {
+    value: PolishCityValue.OTHER,
+    label: 'city.other',
+  },
+];
