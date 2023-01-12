16fc9fff647438ef96d26043d9e12eece7d4801e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 21:22:57 +0100

    Create AnyCityValueType

---
+++ src/app/shared/types/any-city-value.type.ts
@@ -0,0 +1,4 @@
+import { BelarusianCityValue } from '../enums/belarusian-city-value.enum';
+import { PolishCityValue } from '../enums/polish-city-value.enum';
+
+export type AnyCityValueType = BelarusianCityValue | PolishCityValue;
