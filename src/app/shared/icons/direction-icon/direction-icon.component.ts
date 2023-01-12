cc00366a5e33c5495917ae783c39ffd057f3a154
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 11:57:00 +0100

    Create direction icon shared component

---
+++ src/app/shared/icons/direction-icon/direction-icon.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direction-icon',
+  templateUrl: './direction-icon.component.html',
+  styleUrls: ['./direction-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DirectionIconComponent {
+
+}
---
+++ src/app/shared/icons/direction-icon/direction-icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { DirectionIconComponent } from './direction-icon.component';
+
+@NgModule({
+  imports: [],
+  declarations: [DirectionIconComponent],
+  providers: [],
+  exports: [DirectionIconComponent],
+})
+export class DirectionIconModule {}
