d098390a7502753cdea4ac57fd4f8f1450c91262
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 13:44:41 +0100

    Create location icon shared icon component

---
+++ src/app/shared/icons/location-icon/location-icon.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-icon',
+  templateUrl: './location-icon.component.html',
+  styleUrls: ['./location-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LocationIconComponent {
+
+}
---
+++ src/app/shared/icons/location-icon/location-icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { LocationIconComponent } from './location-icon.component';
+
+@NgModule({
+  imports: [],
+  declarations: [LocationIconComponent],
+  providers: [],
+  exports: [LocationIconComponent],
+})
+export class LocationIconModule {}
