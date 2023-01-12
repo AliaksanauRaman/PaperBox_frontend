ca701568244ec426acaa5feb67abbf3d99821f94
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 13:44:24 +0100

    Create calendar icon shared icon component

---
+++ src/app/shared/icons/calendar-icon/calendar-icon.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { CalendarIconComponent } from './calendar-icon.component';
+
+@NgModule({
+  imports: [],
+  declarations: [CalendarIconComponent],
+  providers: [],
+  exports: [CalendarIconComponent],
+})
+export class CalendarIconModule {}
