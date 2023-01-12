16c1e4086f028158031acd151aa6e664f5936196
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-12 21:38:24 +0100

    Create autocomplete text field control shared component

---
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete-text-field-control',
+  templateUrl: './autocomplete-text-field-control.component.html',
+  styleUrls: ['./autocomplete-text-field-control.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AutocompleteTextFieldControlComponent {
+
+}
---
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AutocompleteTextFieldControlComponent } from './autocomplete-text-field-control.component';
+
+@NgModule({
+  imports: [],
+  declarations: [AutocompleteTextFieldControlComponent],
+  providers: [],
+  exports: [AutocompleteTextFieldControlComponent],
+})
+export class AutocompleteTextFieldControlModule {}
