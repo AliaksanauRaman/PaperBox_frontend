5f4b799fd56c1c564db11f740614d812b78e14c8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-13 14:21:01 +0100

    Enhance CityType, add countryValue field

--- src/app/shared/types/city.type.ts
+++ src/app/shared/types/city.type.ts
@@ -1,6 +1,8 @@
 import { AnyCityValueType } from './any-city-value.type';
+import { CountryValue } from '../enums/country-value.enum';
 
 export type CityType<T extends AnyCityValueType = AnyCityValueType> = Readonly<{
+  countryValue: CountryValue;
   value: T;
   label: string;
 }>;
