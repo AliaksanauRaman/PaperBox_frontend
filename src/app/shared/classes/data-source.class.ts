1c3f62006924af8849b261e34b73089461123074
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-13 18:04:37 +0100

    Create DataSource class

---
+++ src/app/shared/classes/data-source.class.ts
@@ -0,0 +1,22 @@
+import { LocationType } from '../types/location.type';
+import { DataSourceOption } from './data-source-option.class';
+import { buildLocationLabel } from '../utils/build-location-label.util';
+
+// TODO: Refactor, make more OOP-like
+export class DataSource<T = unknown> {
+  public static createFromLocations(
+    locations: ReadonlyArray<LocationType>
+  ): DataSource<string> {
+    const options = locations.map(
+      ({ country, city, value }) =>
+        new DataSourceOption(
+          buildLocationLabel(country.label, city.label),
+          value
+        )
+    );
+
+    return new DataSource(options);
+  }
+
+  constructor(public readonly options: ReadonlyArray<DataSourceOption<T>>) {}
+}
