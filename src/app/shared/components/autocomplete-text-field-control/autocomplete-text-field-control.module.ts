95523adf6eb04e5b02a7696df0939e5c2c963de7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-13 18:04:01 +0100

    Import CommonModule and MatAutocompleteModule to AutocompleteTextFieldControlModule

--- src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { AutocompleteTextFieldControlComponent } from './autocomplete-text-field-control.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, MatAutocompleteModule],
   declarations: [AutocompleteTextFieldControlComponent],
   providers: [],
   exports: [AutocompleteTextFieldControlComponent],
