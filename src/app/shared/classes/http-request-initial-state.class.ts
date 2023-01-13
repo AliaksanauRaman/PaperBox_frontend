58cb966d00a1c2ef937ba15b97de40d6116836f2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-13 16:48:42 +0100

    Remove unused stuff

--- src/app/shared/classes/http-request-initial-state.class.ts
+++
@@ -1,7 +0,0 @@
-import { HttpRequestStateName } from '../enums/http-request-state-name.enum';
-
-export class HttpRequestInitialState {
-  public readonly name = HttpRequestStateName.INITIAL as const;
-  public readonly data = null;
-  public readonly error = null;
-}
--- src/app/shared/types/http-request-state.type.ts
+++
@@ -1,8 +0,0 @@
-import { HttpRequestInitialState } from '../classes/http-request-initial-state.class';
-import { HttpRequestSuccessState } from '../classes/http-request-success-state.class';
-import { HttpRequestFailureState } from '../classes/http-request-failure-state.class';
-
-export type HttpRequestStateType<T> =
-  | HttpRequestInitialState
-  | HttpRequestSuccessState<T>
-  | HttpRequestFailureState;
