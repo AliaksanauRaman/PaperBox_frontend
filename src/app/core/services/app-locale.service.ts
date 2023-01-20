e15b79c6de8e554941e7c0c6f92e4d66df3c889f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-20 11:38:49 +0100

    Create AppLocaleService

---
+++ src/app/core/services/app-locale.service.ts
@@ -0,0 +1,33 @@
+import { Inject, Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { LOCAL_STORAGE, LocalStorageType } from '../dependencies/local-storage';
+
+import { LocalStorageKey } from '../../shared/enums/local-storage-key.enum';
+
+const DEFAULT_APP_LOCALE = 'be-BY';
+
+@Injectable({
+  providedIn: 'root',
+})
+// TODO: Check if refactoring is needed
+export class AppLocaleService {
+  private readonly _currentLocale$ = new BehaviorSubject(
+    this.localStorage.getItem(LocalStorageKey.APP_CURRENT_LOCALE) ||
+      DEFAULT_APP_LOCALE
+  );
+
+  public get currentLocale(): string {
+    return this._currentLocale$.getValue();
+  }
+
+  constructor(
+    @Inject(LOCAL_STORAGE)
+    private readonly localStorage: LocalStorageType
+  ) {}
+
+  public setCurrentLocale(locale: string): void {
+    this._currentLocale$.next(locale);
+    this.localStorage.setItem(LocalStorageKey.APP_CURRENT_LOCALE, locale);
+  }
+}
