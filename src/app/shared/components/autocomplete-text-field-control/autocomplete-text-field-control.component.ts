10cca54270f8ede6fbf1ded997b3c65cc2ed94a9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-24 22:27:51 +0100

    Enhance autocomplete text field control shared component

--- src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.component.ts
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.component.ts
@@ -4,10 +4,19 @@ import {
   Component,
   forwardRef,
   Input,
+  OnInit,
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { BehaviorSubject, combineLatest, map } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  distinctUntilChanged,
+  map,
+  startWith,
+  takeUntil,
+  tap,
+} from 'rxjs';
 
 import { UniqueIdGeneratorService } from '../../../services/unique-id-generator.service';
 
@@ -27,7 +36,10 @@ import { DataSource } from '../../classes/data-source.class';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AutocompleteTextFieldControlComponent extends CustomControl<string> {
+export class AutocompleteTextFieldControlComponent
+  extends CustomControl<string>
+  implements OnInit
+{
   @Input()
   public set label(value: string) {
     this.controlLabel = value;
@@ -41,17 +53,25 @@ export class AutocompleteTextFieldControlComponent extends CustomControl<string>
   private readonly controlDataSource$ = new BehaviorSubject<DataSource<string>>(
     new DataSource<string>([])
   );
-  private readonly controlValue$ = new BehaviorSubject<string>('');
+
+  protected readonly inputControl = new FormControl('');
+  protected readonly controlValue$ = new BehaviorSubject<string>('');
 
   protected readonly filteredOptions$ = combineLatest([
     this.controlDataSource$.asObservable(),
-    this.controlValue$.asObservable(),
+    this.inputControl.valueChanges.pipe(startWith('')),
   ]).pipe(
-    map(([controlDataSource, controlValue]) =>
-      controlDataSource.options.filter((option) =>
-        option.label.toLowerCase().includes(controlValue.toLowerCase())
-      )
-    )
+    map(([controlDataSource, inputValue]) => {
+      this.controlValue$.next('');
+
+      if (inputValue === '' || inputValue === null) {
+        return controlDataSource.options;
+      }
+
+      return controlDataSource.options.filter((option) =>
+        option.label.toLowerCase().includes(inputValue.toLowerCase())
+      );
+    })
   );
 
   constructor(
@@ -61,37 +81,48 @@ export class AutocompleteTextFieldControlComponent extends CustomControl<string>
     super(uniqueIdGeneratorService, cdRef);
   }
 
+  public ngOnInit(): void {
+    this.controlValue$
+      .pipe(
+        distinctUntilChanged(),
+        tap((value) => this.onChange(value)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
   public writeValue(value: unknown): void {
     if (!this.isStringOrNull(value)) {
       throw new Error('Only strings and null are allowed!');
     }
 
-    // TODO
-    // this.controlValue = value === null ? '' : value;
-    this.cdRef.markForCheck();
-  }
+    if (value === null || value === '') {
+      this.controlValue$.next('');
+      return;
+    }
 
-  public handleControlValueChange(newValue: string): void {
-    // TODO
-    this.controlValue$.next(newValue);
-    // this.controlValue = newValue;
-    // this.onChange(this.controlValue);
+    const foundOption = this.controlDataSource$
+      .getValue()
+      .options.find((item) => item.value === value);
+
+    if (foundOption === undefined) {
+      throw new Error('Option with such value is not found!');
+    }
+
+    this.inputControl.setValue(foundOption.label);
+    this.controlValue$.next(value);
   }
 
   public handleOptionSelection(event: MatAutocompleteSelectedEvent): void {
-    const foundOptions = this.controlDataSource$
+    const foundOption = this.controlDataSource$
       .getValue()
-      .options.filter((item) => item.label === event.option.value);
+      .options.find((item) => item.label === event.option.value);
 
-    if (foundOptions.length === 0) {
+    if (foundOption === undefined) {
       throw new Error('It cannot be that no option was found!');
     }
 
-    if (foundOptions.length > 1) {
-      throw new Error('Several options has equal labels!');
-    }
-
-    this.onChange(foundOptions[0].value);
+    this.controlValue$.next(foundOption.value);
   }
 
   // TODO: Move to a shared place
--- src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { AutocompleteTextFieldControlComponent } from './autocomplete-text-field-control.component';
 
 @NgModule({
-  imports: [CommonModule, MatAutocompleteModule],
+  imports: [CommonModule, ReactiveFormsModule, MatAutocompleteModule],
   declarations: [AutocompleteTextFieldControlComponent],
   providers: [],
   exports: [AutocompleteTextFieldControlComponent],
