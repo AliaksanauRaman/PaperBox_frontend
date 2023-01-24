c6cdc8a33907681ea2200930469f50c97710dea1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-24 12:27:09 +0100

    Finish phone control shared component

--- src/app/shared/components/phone-control/phone-control.component.ts
+++ src/app/shared/components/phone-control/phone-control.component.ts
@@ -2,14 +2,25 @@ import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
   Component,
+  EventEmitter,
   forwardRef,
+  Inject,
   Input,
+  OnInit,
+  Output,
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, FormBuilder } from '@angular/forms';
+import { map, tap, takeUntil } from 'rxjs';
 
-import { UniqueIdGeneratorService } from './../../../services/unique-id-generator.service';
+import { UniqueIdGeneratorService } from '../../../services/unique-id-generator.service';
+import {
+  PHONE_DIALLING_CODES,
+  PhoneDiallingCodesType,
+} from './../../../core/dependencies/phone-dialling-codes';
 
 import { CustomControl } from '../../abstracts/custom-control.class';
+import { DataSource } from '../../classes/data-source.class';
+import { PhoneType } from '../../types/phone.type';
 
 @Component({
   selector: 'app-phone-control',
@@ -24,24 +35,85 @@ import { CustomControl } from '../../abstracts/custom-control.class';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PhoneControlComponent extends CustomControl<string> {
+export class PhoneControlComponent
+  extends CustomControl<PhoneType>
+  implements OnInit
+{
   @Input()
   public set label(value: string) {
     this.controlLabel = value;
   }
 
-  protected controlValue = '';
+  @Input()
+  public set showPlus(value: boolean) {
+    this._showPlus = value;
+  }
+
+  @Input()
+  public set showMinus(value: boolean) {
+    this._showMinus = value;
+  }
+
+  public get showPlus(): boolean {
+    return this._showPlus;
+  }
+
+  public get showMinus(): boolean {
+    return this._showMinus;
+  }
+
+  @Output()
+  public readonly plusClick = new EventEmitter<void>();
+
+  @Output()
+  public readonly minusClick = new EventEmitter<void>();
+
+  protected readonly phoneDiallingCodesDataSource =
+    DataSource.createFromPhoneDiallingCodes(this.phoneDiallingCodes);
+  protected readonly phoneForm = this.formBuilder.group({
+    diallingCode: [''],
+    number: [''],
+  });
+  protected readonly controlValue$ = this.phoneForm.valueChanges.pipe(
+    map(({ diallingCode, number }) => ({
+      diallingCode: diallingCode || '',
+      number: number || '',
+    }))
+  );
+
+  private _showPlus = false;
+  private _showMinus = false;
 
   constructor(
     uniqueIdGeneratorService: UniqueIdGeneratorService,
-    cdRef: ChangeDetectorRef
+    cdRef: ChangeDetectorRef,
+    private readonly formBuilder: FormBuilder,
+    @Inject(PHONE_DIALLING_CODES)
+    private readonly phoneDiallingCodes: PhoneDiallingCodesType
   ) {
     super(uniqueIdGeneratorService, cdRef);
   }
 
-  public writeValue(value: unknown): void {
-    throw new Error('Method not implemented.');
+  public ngOnInit(): void {
+    this.controlValue$
+      .pipe(
+        tap((value) => this.onChange(value)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 
-  public handleControlValueChange(newValue: string): void {}
+  public writeValue(value: PhoneType): void {
+    // TODO: Assert value type
+
+    this.phoneForm.setValue(value);
+  }
+
+  public handlePlusClick(): void {
+    this.plusClick.emit();
+  }
+
+  public handleMinusClick(): void {
+    this.minusClick.emit();
+  }
 }
--- src/app/shared/components/phone-control/phone-control.module.ts
+++ src/app/shared/components/phone-control/phone-control.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { DropdownControlModule } from '../dropdown-control/dropdown-control.module';
 
 import { PhoneControlComponent } from './phone-control.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    DropdownControlModule,
+  ],
   declarations: [PhoneControlComponent],
   providers: [],
   exports: [PhoneControlComponent],
