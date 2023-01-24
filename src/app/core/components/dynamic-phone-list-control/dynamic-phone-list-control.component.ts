bfa5397dc68590c51120ffa6f653a5262a56d867
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-24 12:28:18 +0100

    Finish dynamic phone list control component

--- src/app/core/components/dynamic-phone-list-control/dynamic-phone-list-control.component.ts
+++ src/app/core/components/dynamic-phone-list-control/dynamic-phone-list-control.component.ts
@@ -1,11 +1,85 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  forwardRef,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { takeUntil, tap } from 'rxjs';
+
+import { UniqueIdGeneratorService } from '../../../services/unique-id-generator.service';
+
+import { CustomControl } from '../../../shared/abstracts/custom-control.class';
+import { PhoneType } from '../../../shared/types/phone.type';
+
+const MAX_PHONES_AMOUNT = 3;
+const EMPTY_PHONE: PhoneType = {
+  diallingCode: '',
+  number: '',
+};
 
 @Component({
   selector: 'app-dynamic-phone-list-control',
   templateUrl: './dynamic-phone-list-control.component.html',
   styleUrls: ['./dynamic-phone-list-control.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DynamicPhoneListControlComponent),
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DynamicPhoneListControlComponent {
+export class DynamicPhoneListControlComponent
+  extends CustomControl<ReadonlyArray<PhoneType>>
+  implements OnInit
+{
+  protected readonly maxPhonesAmount = MAX_PHONES_AMOUNT;
+  protected readonly phoneControlsFormArray = this.formBuilder.array([
+    this.createPhoneControl(),
+  ]);
 
+  constructor(
+    uniqueIdGeneratorService: UniqueIdGeneratorService,
+    cdRef: ChangeDetectorRef,
+    private readonly formBuilder: FormBuilder
+  ) {
+    super(uniqueIdGeneratorService, cdRef);
+  }
+
+  public ngOnInit(): void {
+    this.phoneControlsFormArray.valueChanges
+      .pipe(
+        tap((value) => this.onChange(value)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  public writeValue(value: ReadonlyArray<PhoneType>): void {
+    // TODO: Assert value type
+
+    this.phoneControlsFormArray.clear();
+
+    for (const phone of value) {
+      this.phoneControlsFormArray.push(this.createPhoneControl(phone));
+    }
+  }
+
+  public appendEmptyPhoneControl(): void {
+    this.phoneControlsFormArray.push(this.createPhoneControl());
+  }
+
+  public removePhoneControlByIndex(controlIndex: number): void {
+    this.phoneControlsFormArray.removeAt(controlIndex);
+  }
+
+  private createPhoneControl(
+    controlValue = EMPTY_PHONE
+  ): FormControl<PhoneType> {
+    // TODO: as
+    return this.formBuilder.control(controlValue) as FormControl<PhoneType>;
+  }
 }
