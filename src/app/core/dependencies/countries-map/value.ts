8dd2c21aa42305e4a0ee07522945f9b13c022520
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-25 09:54:42 +0100

    Add GEORGIA to COUNTRIES_MAP_VALUE

--- src/app/core/dependencies/countries-map/value.ts
+++ src/app/core/dependencies/countries-map/value.ts
@@ -9,8 +9,13 @@ const POLAND = {
   value: CountryValue.POLAND,
   label: 'country.poland',
 };
+const GEORGIA = {
+  value: CountryValue.GEORGIA,
+  label: 'country.georgia',
+};
 
 export const COUNTRIES_MAP_VALUE: CountriesMapType = new Map([
   [CountryValue.BELARUS, BELARUS],
   [CountryValue.POLAND, POLAND],
+  [CountryValue.GEORGIA, GEORGIA],
 ]);
