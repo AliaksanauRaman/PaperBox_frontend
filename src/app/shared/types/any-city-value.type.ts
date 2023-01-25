c1d1d0608f047eb3968897483e8f32545652f1ca
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-25 09:53:07 +0100

    Extend AnyCityValueType with GeorgianCityValue

--- src/app/shared/types/any-city-value.type.ts
+++ src/app/shared/types/any-city-value.type.ts
@@ -1,4 +1,8 @@
 import { BelarusianCityValue } from '../enums/belarusian-city-value.enum';
 import { PolishCityValue } from '../enums/polish-city-value.enum';
+import { GeorgianCityValue } from '../enums/georgian-city-value.enum';
 
-export type AnyCityValueType = BelarusianCityValue | PolishCityValue;
+export type AnyCityValueType =
+  | BelarusianCityValue
+  | PolishCityValue
+  | GeorgianCityValue;
