a44eb9e22a1dd56da31ebda140f3ea4220f8401b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-25 14:31:23 +0100

    Introduce the base implementation of find help dialog component

--- src/app/core/dialogs/find-help-dialog/find-help-dialog.component.ts
+++ src/app/core/dialogs/find-help-dialog/find-help-dialog.component.ts
@@ -1,11 +1,50 @@
+import { FormBuilder } from '@angular/forms';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+
+import { DialogComponent } from '../../../shared/abstracts/dialog-component.class';
 
 @Component({
   selector: 'app-find-help-dialog',
   templateUrl: './find-help-dialog.component.html',
   styleUrls: ['./find-help-dialog.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FindHelpDialogComponent {
+export class FindHelpDialogComponent extends DialogComponent {
+  protected readonly offerHelpForm = this.formBuilder.group({
+    locations: [
+      {
+        from: '',
+        to: '',
+      },
+    ],
+    date: [
+      {
+        start: null,
+        end: null,
+      },
+    ],
+    comment: [''],
+    fullName: [''],
+    phones: [
+      [
+        {
+          diallingCode: '',
+          number: '',
+        },
+      ],
+    ],
+  });
 
+  constructor(
+    dialogRef: DialogRef<void>,
+    private readonly formBuilder: FormBuilder
+  ) {
+    super(dialogRef);
+  }
+
+  public handleSendButtonClick(): void {
+    // TODO: Temp
+    console.log(JSON.stringify(this.offerHelpForm.getRawValue(), null, 2));
+  }
 }
