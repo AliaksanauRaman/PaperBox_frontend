778f386b16596800a1d720ce0c2c86cab4a6d178
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-25 15:49:50 +0100

    Make a use of DestructureLocationLabelPipe in AutocompleteTextFieldControlComponent

--- src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
+++ src/app/shared/components/autocomplete-text-field-control/autocomplete-text-field-control.module.ts
@@ -4,10 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 import { AutocompleteTextFieldControlComponent } from './autocomplete-text-field-control.component';
+import { DestructureLocationLabelPipe } from './destructure-location-label.pipe';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, MatAutocompleteModule],
-  declarations: [AutocompleteTextFieldControlComponent],
+  declarations: [
+    AutocompleteTextFieldControlComponent,
+    DestructureLocationLabelPipe,
+  ],
   providers: [],
   exports: [AutocompleteTextFieldControlComponent],
 })
