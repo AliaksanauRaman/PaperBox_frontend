32e873b7da2fe36766341ea24215f79c5ab3c264
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-01-27 15:21:48 +0100

    Add internal validations of date control component, ad isFocused state

--- src/app/shared/components/date-control/date-control.component.ts
+++ src/app/shared/components/date-control/date-control.component.ts
@@ -6,7 +6,15 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
-import { NG_VALUE_ACCESSOR, FormGroup, FormControl } from '@angular/forms';
+import {
+  NG_VALUE_ACCESSOR,
+  FormGroup,
+  FormControl,
+  NG_VALIDATORS,
+  Validator,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { MatDateRangePicker } from '@angular/material/datepicker';
 import { DateAdapter } from '@angular/material/core';
 import { map, takeUntil, tap } from 'rxjs';
@@ -16,7 +24,7 @@ import { AppLocaleService } from '../../../core/services/app-locale.service';
 
 import { CustomControl } from '../../abstracts/custom-control.class';
 
-type DateValue = Readonly<{
+type DateControlValue = Readonly<{
   start: Date | null;
   end: Date | null;
 }>;
@@ -31,13 +39,18 @@ type DateValue = Readonly<{
       useExisting: forwardRef(() => DateControlComponent),
       multi: true,
     },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => DateControlComponent),
+      multi: true,
+    },
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 // TODO: Refactor this all!
 export class DateControlComponent
-  extends CustomControl<DateValue>
-  implements OnInit
+  extends CustomControl<DateControlValue>
+  implements OnInit, Validator
 {
   @Input()
   public set label(value: string) {
@@ -74,6 +87,8 @@ export class DateControlComponent
     })
   );
 
+  protected isFocused = false;
+
   constructor(
     uniqueIdGeneratorService: UniqueIdGeneratorService,
     cdRef: ChangeDetectorRef,
@@ -87,6 +102,7 @@ export class DateControlComponent
     this.dateAdapter.setLocale(this.localeService.currentLocale);
     this.dateForm.valueChanges
       .pipe(
+        // TODO: Prevent redundant calls!!!
         tap(({ start, end }) => {
           if (start === undefined || end === undefined) {
             throw new Error('Values cannot be undefined!');
@@ -94,20 +110,51 @@ export class DateControlComponent
 
           if (start?.getTime() === end?.getTime()) {
             this.onChange({ start, end: null });
+            this.onValidatorChange();
             return;
           }
 
           this.onChange({ start, end });
+          this.onValidatorChange();
         }),
         takeUntil(this.destroy$)
       )
       .subscribe();
   }
 
-  public writeValue(value: DateValue): void {
+  public writeValue(value: DateControlValue): void {
     // TODO: Assert value type
 
     this.dateForm.setValue(value);
+  }
+
+  public validate(thisControl: AbstractControl): ValidationErrors | null {
+    const thisControlValue: DateControlValue | null = thisControl.getRawValue();
+
+    if (
+      thisControlValue === null ||
+      (thisControlValue.start === null && thisControlValue.end === null)
+    ) {
+      return { invalid: true };
+    }
+
+    return null;
+  }
+
+  public registerOnValidatorChange(fn: () => void): void {
+    this.onValidatorChange = fn;
+  }
+
+  public handleControlFocus(dateRangePicker: MatDateRangePicker<Date>): void {
+    this.isFocused = true;
+
+    this.toggleDateRangePicker(dateRangePicker);
+  }
+
+  public override handleControlBlur(): void {
+    super.handleControlBlur();
+
+    this.isFocused = false;
   }
 
   public toggleDateRangePicker(
@@ -121,13 +168,12 @@ export class DateControlComponent
   }
 
   private formatDate(date: Date): string {
-    return new Intl.DateTimeFormat(
-      this.localeService.currentLocale,
-      {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-      }
-    ).format(date);
+    return new Intl.DateTimeFormat(this.localeService.currentLocale, {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    }).format(date);
   }
+
+  private onValidatorChange = () => {};
 }
