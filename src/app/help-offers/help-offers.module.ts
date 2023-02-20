0a05206339b832616af253614d18f29251dbe1d8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-20 20:20:44 +0100

    Add TranslateModule, SpinnerModule and DepartureFromLocationModule to imports and HelpOffersEventsProcessorService to providers of HelpOffersModule, set up processors on HelpOffersModule creation

--- src/app/help-offers/help-offers.module.ts
+++ src/app/help-offers/help-offers.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ArrowDownIconModule } from '../shared/icons/arrow-down-icon/arrow-down-icon.module';
+import { SpinnerModule } from '../shared/components/spinner/spinner.module';
+import { DepartureFromLocationModule } from '../shared/pipes/departure-from-location/departure-from-location.module';
 import { HelpOffersRoutingModule } from './help-offers-routing.module';
 
 import { HelpOffersMainViewComponent } from './views/help-offers-main-view/help-offers-main-view.component';
@@ -9,14 +12,29 @@ import { HelpOffersMainViewComponent } from './views/help-offers-main-view/help-
 import { HelpOffersListComponent } from './components/help-offers-list/help-offers-list.component';
 import { HelpOffersListItemComponent } from './components/help-offers-list-item/help-offers-list-item.component';
 
+import { HelpOffersEventsProcessorService } from './services/help-offers-events-processor.service';
+
 @NgModule({
-  imports: [CommonModule, ArrowDownIconModule, HelpOffersRoutingModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    ArrowDownIconModule,
+    SpinnerModule,
+    DepartureFromLocationModule,
+    HelpOffersRoutingModule,
+  ],
   declarations: [
     HelpOffersMainViewComponent,
     HelpOffersListComponent,
     HelpOffersListItemComponent,
   ],
-  providers: [],
+  providers: [HelpOffersEventsProcessorService],
   exports: [],
 })
-export class HelpOffersModule {}
+export class HelpOffersModule {
+  constructor(
+    private readonly helpOffersEventsProcessorService: HelpOffersEventsProcessorService
+  ) {
+    this.helpOffersEventsProcessorService.setUpProcessors();
+  }
+}
