d01d7c5b554808df7782ea39644a016000d402e9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-20 16:19:21 +0100

    Create shared ManWithMailImageComponent

---
+++ src/app/shared/images/man-with-mail-image/man-with-mail-image.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-with-mail-image',
+  templateUrl: './man-with-mail-image.component.html',
+  styleUrls: ['./man-with-mail-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ManWithMailImageComponent {}
