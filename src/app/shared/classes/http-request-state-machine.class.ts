1f19a0163f8ce00db47edb651bc34d1dea880c42
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-20 20:16:23 +0100

    Create HttpRequestStateMachine class and its related types

---
+++ src/app/shared/classes/http-request-state-machine.class.ts
@@ -0,0 +1,59 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+class InitialState {
+  constructor(
+    public readonly inProgress = false,
+    public readonly data = null,
+    public readonly error = null
+  ) {}
+}
+
+class LoadingState {
+  constructor(
+    public readonly inProgress = true,
+    public readonly data = null,
+    public readonly error = null
+  ) {}
+}
+
+class SuccessResponseState<T> {
+  constructor(
+    public readonly data: T,
+    public readonly inProgress = false,
+    public readonly error = null
+  ) {}
+}
+
+class FailedResponseState {
+  constructor(
+    public readonly error: HttpErrorResponse,
+    public readonly inProgress = false,
+    public readonly data = null
+  ) {}
+}
+
+export type HttpRequestState<T> =
+  | InitialState
+  | LoadingState
+  | SuccessResponseState<T>
+  | FailedResponseState;
+
+export class HttpRequestStateMachine<T> {
+  private readonly _currentState$ = new BehaviorSubject<HttpRequestState<T>>(
+    new InitialState()
+  );
+  public readonly currentState$ = this._currentState$.asObservable();
+
+  public handleMakeRequest(): void {
+    this._currentState$.next(new LoadingState());
+  }
+
+  public handleSuccessResponse(responseData: T): void {
+    this._currentState$.next(new SuccessResponseState(responseData));
+  }
+
+  public handleFailedResponse(responseError: HttpErrorResponse): void {
+    this._currentState$.next(new FailedResponseState(responseError));
+  }
+}
