a81fc7c9a756d57581c8be8e5dfb18cd613691ac
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-22 14:54:31 +0100

    Create HelpRequestsListItemComponent and add it to HelpRequestsModule declarations

---
+++ src/app/help-requests/components/help-requests-list-item/help-requests-list-item.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-requests-list-item',
+  templateUrl: './help-requests-list-item.component.html',
+  styleUrls: ['./help-requests-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HelpRequestsListItemComponent {
+
+}
--- src/app/help-requests/help-requests.module.ts
+++ src/app/help-requests/help-requests.module.ts
@@ -5,10 +5,11 @@ import { HelpRequestsRoutingModule } from './help-requests-routing.module';
 import { HelpRequestsMainViewComponent } from './views/help-requests-main-view/help-requests-main-view.component';
 
 import { HelpRequestsListComponent } from './components/help-requests-list/help-requests-list.component';
+import { HelpRequestsListItemComponent } from './components/help-requests-list-item/help-requests-list-item.component';
 
 @NgModule({
   imports: [HelpRequestsRoutingModule],
-  declarations: [HelpRequestsMainViewComponent, HelpRequestsListComponent],
+  declarations: [HelpRequestsMainViewComponent, HelpRequestsListComponent, HelpRequestsListItemComponent],
   providers: [],
   exports: [],
 })
