aa94cdf396fd3804df9d6af4730d48b2ad74d8bf
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-22 15:23:50 +0100

    Create shared FoldableComponent

---
+++ src/app/shared/components/foldable/foldable-component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { FoldableComponent } from './foldable.component';
+
+@NgModule({
+  imports: [],
+  declarations: [FoldableComponent],
+  providers: [],
+  exports: [FoldableComponent],
+})
+export class FoldableComponentModule {}
---
+++ src/app/shared/components/foldable/foldable.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foldable',
+  templateUrl: './foldable.component.html',
+  styleUrls: ['./foldable.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FoldableComponent {
+
+}
