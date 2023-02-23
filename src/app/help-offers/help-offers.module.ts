81ea6c9263b3fe0a1aca6730dd2d179a4cad6b39
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 09:11:22 +0100

    Rename folder name for LaconicPlaceLabelFromLocationValuePipeModule

--- src/app/help-offers/help-offers.module.ts
+++ src/app/help-offers/help-offers.module.ts
@@ -5,7 +5,7 @@ import { MatRippleModule } from '@angular/material/core';
 
 import { ArrowDownIconModule } from '../shared/icons/arrow-down-icon/arrow-down-icon.module';
 import { SpinnerModule } from '../shared/components/spinner/spinner.module';
-import { LaconicPlaceLabelFromLocationValuePipeModule } from '../shared/pipes/departure-from-location/laconic-place-label-from-location-value-pipe.module';
+import { LaconicPlaceLabelFromLocationValuePipeModule } from '../shared/pipes/laconic-place-label-from-location-value/laconic-place-label-from-location-value-pipe.module';
 import { BuildPhoneHrefPipeModule } from '../shared/pipes/build-phone-href/build-phone-href-pipe.module';
 import { ViewPhonePipeModule } from '../shared/pipes/view-phone/view-phone-pipe.module';
 import { FoldableComponentModule } from '../shared/components/foldable/foldable-component.module';
