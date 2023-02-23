cac1e37480b0f80bfa14e661b48768a93dfa8340
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 11:18:18 +0100

    Finish the major part of HelpRequestsListItemComponent implementation

--- src/app/help-requests/components/help-requests-list-item/help-requests-list-item.component.ts
+++ src/app/help-requests/components/help-requests-list-item/help-requests-list-item.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  ViewChild,
+} from '@angular/core';
+
+import { FoldableComponent } from '../../../shared/components/foldable/foldable.component';
+import { HoverableDirective } from '../../../shared/directives/hoverable/hoverable.directive';
 
 import { PublishedHelpRequestType } from '../../../shared/types/published-help-request.type';
 
@@ -6,7 +14,7 @@ import { PublishedHelpRequestType } from '../../../shared/types/published-help-r
   selector: 'app-help-requests-list-item',
   templateUrl: './help-requests-list-item.component.html',
   styleUrls: ['./help-requests-list-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelpRequestsListItemComponent {
   @Input()
@@ -14,5 +22,44 @@ export class HelpRequestsListItemComponent {
     this._publishedHelpRequest = value;
   }
 
+  @ViewChild(FoldableComponent)
+  private readonly foldableComponentRef?: FoldableComponent;
+
+  @ViewChild(HoverableDirective)
+  private readonly hoverableDirectiveRef?: HoverableDirective;
+
+  public get isUnfolded(): boolean {
+    if (this.foldableComponentRef === undefined) {
+      return false;
+    }
+
+    return this.foldableComponentRef.isUnfolded;
+  }
+
+  public get isHovered(): boolean {
+    if (this.hoverableDirectiveRef === undefined) {
+      return false;
+    }
+
+    return this.hoverableDirectiveRef.isHovered;
+  }
+
   public _publishedHelpRequest?: PublishedHelpRequestType;
+
+  public handleCardClick(): void {
+    if (this.foldableComponentRef === undefined) {
+      throw new Error(`Foldable component ref is not defined!`);
+    }
+
+    if (this.hoverableDirectiveRef === undefined) {
+      throw new Error(`Hoverable directive ref is not defined!`);
+    }
+
+    if (this.isUnfolded) {
+      this.foldableComponentRef.fold();
+      this.hoverableDirectiveRef.unhover();
+    } else {
+      this.foldableComponentRef.unfold();
+    }
+  }
 }
