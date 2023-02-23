243f53a8527c6465374ed4d8ccf6e1b2fda6b88e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 21:54:52 +0100

    Refactor HttpPostRequestServiceBase a bit

--- src/app/shared/abstracts/http-post-request-service-base.class.ts
+++ src/app/shared/abstracts/http-post-request-service-base.class.ts
@@ -2,13 +2,13 @@ import { HttpRequestServiceBase } from './http-request-service-base.class';
 import { AppEventUnion } from '../../events/app-event.union';
 
 export abstract class HttpPostRequestServiceBase<
-  T,
-  O
-> extends HttpRequestServiceBase<T> {
-  public abstract buildMakeRequestEvent(createDto: O): AppEventUnion;
-
-  public makeRequest(createDto: O): void {
+  SuccessResponseDataType,
+  DtoType
+> extends HttpRequestServiceBase<SuccessResponseDataType> {
+  public makeRequest(createDto: DtoType): void {
     this.httpRequestStateMachine.handleMakeRequest();
     this.eventBusService.emit(this.buildMakeRequestEvent(createDto));
   }
+
+  protected abstract buildMakeRequestEvent(createDto: DtoType): AppEventUnion;
 }
