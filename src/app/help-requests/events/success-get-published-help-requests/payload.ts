55038053079e10623da2bae6be980e2c416d05e8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 09:18:21 +0100

    Create SuccessGetPublishedHelpRequests event

---
+++ src/app/help-requests/events/success-get-published-help-requests/payload.ts
@@ -0,0 +1,4 @@
+import { PublishedHelpRequestListType } from '../../../shared/types/published-help-request-list.type';
+
+export type SuccessGetPublishedHelpRequestsPayload =
+  PublishedHelpRequestListType;
