1c7057fa531017cd21a6e46a3091deb35b6e9c84
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 09:40:29 +0100

    Add basic implementation of HelpRequestsListComponent

--- src/app/help-requests/components/help-requests-list/help-requests-list.component.ts
+++ src/app/help-requests/components/help-requests-list/help-requests-list.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+import { PublishedHelpRequestsService } from '../../services/published-help-requests.service';
 
 @Component({
   selector: 'app-help-requests-list',
   templateUrl: './help-requests-list.component.html',
   styleUrls: ['./help-requests-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [PublishedHelpRequestsService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HelpRequestsListComponent {
+export class HelpRequestsListComponent implements OnInit {
+  public readonly publishedHelpRequestsState$ =
+    this.publishedHelpRequestsService.state$;
 
+  constructor(
+    private readonly publishedHelpRequestsService: PublishedHelpRequestsService
+  ) {}
+
+  public ngOnInit(): void {
+    this.publishedHelpRequestsService.makeGetHttpRequest();
+  }
 }
