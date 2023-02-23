f0f19d178496027d7510b7753a588e1a7d8cc303
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 09:28:07 +0100

    Import all needed modules and services for HelpRequestsModule work

--- src/app/help-requests/help-requests.module.ts
+++ src/app/help-requests/help-requests.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatRippleModule } from '@angular/material/core';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { SpinnerModule } from '../shared/components/spinner/spinner.module';
+import { FoldableComponentModule } from '../shared/components/foldable/foldable-component.module';
+import { ArrowDownIconModule } from '../shared/icons/arrow-down-icon/arrow-down-icon.module';
+import { LaconicPlaceLabelFromLocationValuePipeModule } from '../shared/pipes/laconic-place-label-from-location-value/laconic-place-label-from-location-value-pipe.module';
+import { BuildPhoneHrefPipeModule } from '../shared/pipes/build-phone-href/build-phone-href-pipe.module';
+import { ViewPhonePipeModule } from '../shared/pipes/view-phone/view-phone-pipe.module';
 import { HelpRequestsRoutingModule } from './help-requests-routing.module';
 
 import { HelpRequestsMainViewComponent } from './views/help-requests-main-view/help-requests-main-view.component';
@@ -7,10 +16,33 @@ import { HelpRequestsMainViewComponent } from './views/help-requests-main-view/h
 import { HelpRequestsListComponent } from './components/help-requests-list/help-requests-list.component';
 import { HelpRequestsListItemComponent } from './components/help-requests-list-item/help-requests-list-item.component';
 
+import { HelpRequestsEventsProcessorService } from './services/help-requests-events-processor.service';
+
 @NgModule({
-  imports: [HelpRequestsRoutingModule],
-  declarations: [HelpRequestsMainViewComponent, HelpRequestsListComponent, HelpRequestsListItemComponent],
-  providers: [],
+  imports: [
+    CommonModule,
+    MatRippleModule,
+    TranslateModule,
+    SpinnerModule,
+    FoldableComponentModule,
+    ArrowDownIconModule,
+    LaconicPlaceLabelFromLocationValuePipeModule,
+    BuildPhoneHrefPipeModule,
+    ViewPhonePipeModule,
+    HelpRequestsRoutingModule,
+  ],
+  declarations: [
+    HelpRequestsMainViewComponent,
+    HelpRequestsListComponent,
+    HelpRequestsListItemComponent,
+  ],
+  providers: [HelpRequestsEventsProcessorService],
   exports: [],
 })
-export class HelpRequestsModule {}
+export class HelpRequestsModule {
+  constructor(
+    private readonly helpRequestsEventsProcessorService: HelpRequestsEventsProcessorService
+  ) {
+    this.helpRequestsEventsProcessorService.setUpProcessors();
+  }
+}
