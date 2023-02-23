0880064dd8db661688da13d2c9074ba433983514
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 11:19:11 +0100

    Import HoverableDirectiveModule to HelpRequestsModule

--- src/app/help-requests/help-requests.module.ts
+++ src/app/help-requests/help-requests.module.ts
@@ -9,6 +9,7 @@ import { ArrowDownIconModule } from '../shared/icons/arrow-down-icon/arrow-down-
 import { LaconicPlaceLabelFromLocationValuePipeModule } from '../shared/pipes/laconic-place-label-from-location-value/laconic-place-label-from-location-value-pipe.module';
 import { BuildPhoneHrefPipeModule } from '../shared/pipes/build-phone-href/build-phone-href-pipe.module';
 import { ViewPhonePipeModule } from '../shared/pipes/view-phone/view-phone-pipe.module';
+import { HoverableDirectiveModule } from '../shared/directives/hoverable/hoverable-directive.module';
 import { HelpRequestsRoutingModule } from './help-requests-routing.module';
 
 import { HelpRequestsMainViewComponent } from './views/help-requests-main-view/help-requests-main-view.component';
@@ -29,6 +30,7 @@ import { HelpRequestsEventsProcessorService } from './services/help-requests-eve
     LaconicPlaceLabelFromLocationValuePipeModule,
     BuildPhoneHrefPipeModule,
     ViewPhonePipeModule,
+    HoverableDirectiveModule,
     HelpRequestsRoutingModule,
   ],
   declarations: [
