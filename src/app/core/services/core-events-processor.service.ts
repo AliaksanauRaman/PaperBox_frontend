5de376dc90a43ca7838c49e1a57747f459ddbac0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-23 22:03:13 +0100

    Create CoreEventsProcessorService

---
+++ src/app/core/services/core-events-processor.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { tap, catchError, of, takeUntil } from 'rxjs';
+
+import { HelpOffersHttpService } from '../../help-offers/services/help-offers-http.service';
+
+import { DestroyEmitter } from '../../shared/abstracts/destroy-emitter.class';
+import {
+  AppEventBusService,
+  AppEventName,
+  FailedCreateHelpOffer,
+  MakeCreateHelpOfferRequestPayloadType,
+  SuccessCreateHelpOffer,
+} from '../../events';
+
+@Injectable()
+export class CoreEventsProcessorService extends DestroyEmitter {
+  constructor(
+    private readonly eventBusService: AppEventBusService,
+    private readonly helpOffersHttpService: HelpOffersHttpService
+  ) {
+    super();
+  }
+
+  public setUpProcessors(): void {
+    this.processCreateHelpOfferRequests();
+  }
+
+  private processCreateHelpOfferRequests(): void {
+    this.eventBusService
+      .on<MakeCreateHelpOfferRequestPayloadType>(
+        AppEventName.MAKE_CREATE_HELP_OFFER_REQUEST
+      )
+      .pipe(
+        tap((payload) => {
+          this.helpOffersHttpService
+            .createOne(payload)
+            .pipe(
+              tap((response) =>
+                this.eventBusService.emit(new SuccessCreateHelpOffer(response))
+              ),
+              catchError((error: HttpErrorResponse) => {
+                this.eventBusService.emit(new FailedCreateHelpOffer(error));
+                return of(null);
+              }),
+              takeUntil(this.destroy$)
+            )
+            .subscribe();
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+}
