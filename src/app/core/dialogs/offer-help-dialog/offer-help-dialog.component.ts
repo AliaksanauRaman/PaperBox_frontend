4d6c5535bee44b9d32543167ee69c5ac21f363eb
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-27 21:07:07 +0100

    Finish the major part of offer help dialog component implementation

--- src/app/core/dialogs/offer-help-dialog/offer-help-dialog.component.ts
+++ src/app/core/dialogs/offer-help-dialog/offer-help-dialog.component.ts
@@ -1,8 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { DialogRef } from '@angular/cdk/dialog';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+} from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { DialogRef } from '@angular/cdk/dialog';
+import { BehaviorSubject, tap } from 'rxjs';
+
+import { CreateHelpOfferService } from '../../services/create-help-offer.service';
 
 import { DialogComponent } from '../../../shared/abstracts/dialog-component.class';
+
+import { CreateHelpOfferDto } from '../../../shared/dtos/create-help-offer.dto';
+
+const NORMAL_TITLE = 'dialogs.offerHelp.title';
+const LOADING_TITLE = 'dialogs.offerHelp.loading';
+const SUCCESS_TITLE = 'dialogs.offerHelp.success';
 
 @Component({
   selector: 'app-offer-help-dialog',
@@ -11,9 +24,44 @@ import { DialogComponent } from '../../../shared/abstracts/dialog-component.clas
     './offer-help-dialog.component.scss',
     '../find-and-offer-help-dialogs-common-styles.scss',
   ],
+  providers: [CreateHelpOfferService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OfferHelpDialogComponent extends DialogComponent {
+  @HostListener('document:keydown.enter', ['$event'])
+  private handleEnterPress(event: KeyboardEvent): void {
+    event.stopImmediatePropagation();
+
+    if (!event.isTrusted) {
+      console.log('Nice try');
+      return;
+    }
+
+    if (!this.offerHelpForm.valid) {
+      return;
+    }
+
+    this.createHelpOffer();
+  }
+
+  private readonly _dialogTitle$ = new BehaviorSubject<string>(NORMAL_TITLE);
+  public readonly dialogTitle$ = this._dialogTitle$.asObservable();
+
+  public readonly createHelpOfferRequestState$ =
+    this.createHelpOfferRequestService.state$.pipe(
+      tap((state) => {
+        if (state.inProgress) {
+          this._dialogTitle$.next(LOADING_TITLE);
+          this.offerHelpForm.disable();
+        } else if (state.error !== null) {
+          this._dialogTitle$.next(NORMAL_TITLE);
+          this.offerHelpForm.enable();
+        } else if (state.data !== null) {
+          this._dialogTitle$.next(SUCCESS_TITLE);
+        }
+      })
+    );
+
   protected readonly offerHelpForm = this.formBuilder.group({
     locations: [
       {
@@ -41,27 +89,35 @@ export class OfferHelpDialogComponent extends DialogComponent {
 
   constructor(
     dialogRef: DialogRef<void>,
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly createHelpOfferRequestService: CreateHelpOfferService
   ) {
     super(dialogRef);
   }
 
-  public handleSendButtonClick(): void {
-    // TODO: Temp
-    const result = this.offerHelpForm.getRawValue();
-    console.log(JSON.stringify(result, null, 2));
-    console.log(result);
+  public handleSendButtonClick(event: MouseEvent): void {
+    if (!event.isTrusted) {
+      console.log('Nice try');
+      return;
+    }
 
-    const mappedResult = {
-      locationFrom: result.locations?.from,
-      locationTo: result.locations?.to,
-      startDate: result.date?.start,
-      endDate: result.date?.end,
-      comment: result.comment,
-      fullName: result.fullName,
-      phones: result.phones,
-    };
+    this.createHelpOffer();
+  }
 
-    console.log({ MAPPED: mappedResult });
+  private createHelpOffer(): void {
+    const { locations, date, comment, fullName, phones } =
+      this.offerHelpForm.getRawValue();
+
+    this.createHelpOfferRequestService.performRequest(
+      new CreateHelpOfferDto(
+        locations!.from,
+        locations!.to,
+        date!.start,
+        date!.end,
+        comment,
+        fullName,
+        phones
+      )
+    );
   }
 }
