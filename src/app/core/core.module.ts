a5eef68029ffd0faf8b8d6646b25b760ca6acf67
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-27 19:35:31 +0100

    Create SuccessCreateHelpOfferSectionComponent and add it to CoreModule declarations

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -31,6 +31,7 @@ import { LeaveFeedbackButtonComponent } from './components/leave-feedback-button
 import { DynamicPhoneListControlComponent } from './components/dynamic-phone-list-control/dynamic-phone-list-control.component';
 import { LocationsControlComponent } from './components/locations-control/locations-control.component';
 import { SuccessCreateFeedbackViewComponent } from './components/success-create-feedback-view/success-create-feedback-view.component';
+import { SuccessCreateHelpOfferSectionComponent } from './sections/success-create-help-offer-section/success-create-help-offer-section.component';
 import { CreateFeedbackDialogComponent } from './dialogs/feedback-dialog/create-feedback-dialog.component';
 import { OfferHelpDialogComponent } from './dialogs/offer-help-dialog/offer-help-dialog.component';
 import { FindHelpDialogComponent } from './dialogs/find-help-dialog/find-help-dialog.component';
@@ -79,6 +80,7 @@ const CORE_MODULE_DECLARATIONS = [
   DynamicPhoneListControlComponent,
   LocationsControlComponent,
   SuccessCreateFeedbackViewComponent,
+  SuccessCreateHelpOfferSectionComponent,
   CreateFeedbackDialogComponent,
   OfferHelpDialogComponent,
   FindHelpDialogComponent,
---
+++ src/app/core/sections/success-create-help-offer-section/success-create-help-offer-section.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-create-help-offer-section',
+  templateUrl: './success-create-help-offer-section.component.html',
+  styleUrls: ['./success-create-help-offer-section.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SuccessCreateHelpOfferSectionComponent {
+
+}
