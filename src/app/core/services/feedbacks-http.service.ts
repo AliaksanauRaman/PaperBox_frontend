40ce0aa08f68dff7f3183d2a4cab8315d402d7c5
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-27 10:32:10 +0100

    Create FeedbacksHttpService with temp data

---
+++ src/app/core/services/feedbacks-http.service.ts
@@ -0,0 +1,32 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of, delay } from 'rxjs';
+
+import { API_URL } from '../../shared/dependencies/api-url/injection-token';
+import { CreateFeedbackDto } from '../../shared/dtos/create-feedback.dto';
+import { SuccessCreateFeedbackResponseDataType } from '../../shared/types/success-create-feedback-response-data.type';
+
+@Injectable()
+export class FeedbacksHttpService {
+  private readonly feedbacksApiUrl = `${this.apiUrl}/api/feedbacks`;
+
+  constructor(
+    @Inject(API_URL)
+    private readonly apiUrl: string,
+    private readonly httpClient: HttpClient
+  ) {}
+
+  public createOne(
+    createFeedbackDto: CreateFeedbackDto
+  ): Observable<SuccessCreateFeedbackResponseDataType> {
+    const { fullName, email, comment } = createFeedbackDto.toRequestBody();
+
+    // TODO: Temp data
+    return of({
+      id: '123123123',
+      fullName,
+      email,
+      comment,
+    }).pipe(delay(800));
+  }
+}
