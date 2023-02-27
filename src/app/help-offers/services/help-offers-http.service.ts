5bb5a6ba8831bc75ef6bf196e03f749cde2bcf55
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-27 19:27:29 +0100

    Finish HelpOffersHttpService implementation

--- src/app/help-offers/services/help-offers-http.service.ts
+++ src/app/help-offers/services/help-offers-http.service.ts
@@ -1,9 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { API_URL } from '../../shared/dependencies/api-url/injection-token';
 import { PublishedHelpOfferListType } from '../../shared/types/published-help-offer-list.type';
+import { SuccessCreateHelpOfferResponseDataType } from '../../shared/types/success-create-help-offer-response-data.type';
+import { CreateHelpOfferDto } from '../../shared/dtos/create-help-offer.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -18,15 +20,17 @@ export class HelpOffersHttpService {
   ) {}
 
   public getPublished(): Observable<PublishedHelpOfferListType> {
-    // TODO: !IMPORTANT Make get request once fixed instead
-    return this.httpClient.post<PublishedHelpOfferListType>(
-      `${this.helpOffersApiUrl}/published`,
-      {}
+    return this.httpClient.get<PublishedHelpOfferListType>(
+      `${this.helpOffersApiUrl}/published`
     );
   }
 
-  // TODO: Types
-  public createOne(createHelpOfferDto: unknown): Observable<unknown> {
-    return this.httpClient.post(this.helpOffersApiUrl, createHelpOfferDto);
+  public createOne(
+    createHelpOfferDto: CreateHelpOfferDto
+  ): Observable<SuccessCreateHelpOfferResponseDataType> {
+    return this.httpClient.post<SuccessCreateHelpOfferResponseDataType>(
+      this.helpOffersApiUrl,
+      createHelpOfferDto.toRequestBody()
+    );
   }
 }
