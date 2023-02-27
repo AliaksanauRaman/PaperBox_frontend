8abbf3d7cdb8973729ae78d720a465f74f822169
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-27 10:31:15 +0100

    Refactor HttpRequestWithParamsBaseService a bit

--- src/app/shared/abstracts/http-request-with-params-base-service.class.ts
+++ src/app/shared/abstracts/http-request-with-params-base-service.class.ts
@@ -3,11 +3,12 @@ import { Observable } from 'rxjs';
 import { HttpRequestBaseService } from './http-request-base-service.class';
 
 export abstract class HttpRequestWithParamsBaseService<
-  T
+  T,
+  ParamsType
 > extends HttpRequestBaseService<T> {
-  protected abstract doRequest<P>(params: P): Observable<T>;
+  protected abstract doRequest(params: ParamsType): Observable<T>;
 
-  public performRequest<P>(params: P): void {
+  public performRequest(params: ParamsType): void {
     this.httpRequestStateMachine.handleMakeRequest();
     this.handleRequest(this.doRequest(params));
   }
