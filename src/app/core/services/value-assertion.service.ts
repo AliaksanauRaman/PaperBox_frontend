3cfe019547c8b00557fc6f58ce247a6e202e5c3d
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-02-28 13:17:35 +0100

    Create ValueAssertionService

---
+++ src/app/core/services/value-assertion.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+import { emailRegExp } from '../../shared/regexps/email.regexp';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValueAssertionService {
+  public isNonEmptyString(value: string): boolean {
+    return value !== '';
+  }
+
+  public doesStringLengthEqualOrMoreThan(
+    value: string,
+    length: number
+  ): boolean {
+    return value.length >= length;
+  }
+
+  public isEmailString(value: string): boolean {
+    return emailRegExp.test(value);
+  }
+}
