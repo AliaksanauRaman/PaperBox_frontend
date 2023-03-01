bf52b3a5985f9742b1a18baf7d445e81a0749ffe
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-01 13:08:34 +0100

    Create helpRequestsHttpServiceFactory and make a use of MockHelpRequestsHttpService

--- src/app/core/services/help-requests-http.service.ts
+++ src/app/core/services/help-requests-http.service.ts
@@ -1,13 +1,20 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, delay } from 'rxjs';
 
+import { MockHelpRequestsHttpService } from '../mocks/mock-help-requests-http.service';
 import { API_URL } from '../../shared/dependencies/api-url/injection-token';
-
+import { HelpRequestsHttpServiceInterface } from '../interfaces/help-requests-http-service.interface';
 import { PublishedHelpRequestListType } from '../../shared/types/published-help-request-list.type';
 
-@Injectable({ providedIn: 'root' })
-export class HelpRequestsHttpService {
+@Injectable({
+  providedIn: 'root',
+  useFactory: helpRequestsHttpServiceFactory,
+  deps: [API_URL, HttpClient],
+})
+export class HelpRequestsHttpService
+  implements HelpRequestsHttpServiceInterface
+{
   private readonly helpRequestsApiUrl = `${this.apiUrl}/api/help-requests`;
 
   constructor(
@@ -21,4 +28,15 @@ export class HelpRequestsHttpService {
       `${this.helpRequestsApiUrl}/published`
     );
   }
+}
+
+function helpRequestsHttpServiceFactory(
+  apiUrl: string,
+  httpClient: HttpClient
+): HelpRequestsHttpServiceInterface {
+  if (isDevMode()) {
+    return new MockHelpRequestsHttpService();
+  }
+
+  return new HelpRequestsHttpService(apiUrl, httpClient);
 }
