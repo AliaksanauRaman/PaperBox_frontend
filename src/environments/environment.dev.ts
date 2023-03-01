562e3b47961ef46e92533b9a36476bf4f87ec2da
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-01 12:54:38 +0100

    Create environment files

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+import { EnvironmentType } from './environment.type';
+
+export const environment: EnvironmentType = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { EnvironmentType } from './environment.type';
+
+export const environment: EnvironmentType = {
+  production: true,
+  apiUrl: 'http://localhost:3000',
+};
