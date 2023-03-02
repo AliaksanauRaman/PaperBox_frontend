e7410cd7bab36c3b5578b2c9875dc597507c4bca
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-02 18:00:12 +0100

    Create LoginMainPageComponent

---
+++ src/app/login/pages/login-main-page/login-main-page.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-main-page',
+  templateUrl: './login-main-page.component.html',
+  styleUrls: ['./login-main-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginMainPageComponent {
+
+}
