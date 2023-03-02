3245bef058433ce730c8b88e6f4deffe7d7dd65b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-02 15:07:02 +0100

    Create feedbacksHttpServiceFactory

--- src/app/core/services/feedbacks-http.service.ts
+++ src/app/core/services/feedbacks-http.service.ts
@@ -1,14 +1,17 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { FeedbacksHttpServiceInterface } from '../interfaces/feedbacks-http-service.interface';
+import { MockFeedbacksHttpService } from '../mocks/mock-feedbacks-http.service';
 import { API_URL } from '../../shared/dependencies/api-url/injection-token';
 import { CreateFeedbackDto } from '../../shared/dtos/create-feedback.dto';
 import { SuccessCreateFeedbackResponseDataType } from '../../shared/types/success-create-feedback-response-data.type';
 
 @Injectable({
   providedIn: 'root',
+  useFactory: feedbacksHttpServiceFactory,
+  deps: [API_URL, HttpClient],
 })
 export class FeedbacksHttpService implements FeedbacksHttpServiceInterface {
   private readonly feedbacksApiUrl = `${this.apiUrl}/api/feedbacks`;
@@ -27,4 +30,15 @@ export class FeedbacksHttpService implements FeedbacksHttpServiceInterface {
       createFeedbackDto
     );
   }
+}
+
+function feedbacksHttpServiceFactory(
+  apiUrl: string,
+  httpClient: HttpClient
+): FeedbacksHttpServiceInterface {
+  if (isDevMode()) {
+    return new MockFeedbacksHttpService();
+  }
+
+  return new FeedbacksHttpService(apiUrl, httpClient);
 }
