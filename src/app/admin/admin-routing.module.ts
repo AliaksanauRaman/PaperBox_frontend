03677a749e651aef10ffcf0da3edb5d06c53d900
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-03 16:24:27 +0100

    Add all new admin pages to the routing configuration

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,11 +1,18 @@
-import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AdminWorkPageLayoutComponent } from './layouts/admin-work-page-layout/admin-work-page-layout.component';
 import { AdminLoginPageComponent } from './pages/admin-login-page/admin-login-page.component';
+import { AdminWorkPageLayoutComponent } from './layouts/admin-work-page-layout/admin-work-page-layout.component';
 import { AdminHelpOffersPageComponent } from './pages/admin-help-offers-page/admin-help-offers-page.component';
+import { AdminHelpRequestsPageComponent } from './pages/admin-help-requests-page/admin-help-requests-page.component';
+import { AdminUsersPageComponent } from './pages/admin-users-page/admin-users-page.component';
+import { AdminSettingsPageComponent } from './pages/admin-settings-page/admin-settings-page.component';
 
 const adminRoutes: Routes = [
+  {
+    path: 'login',
+    component: AdminLoginPageComponent,
+  },
   {
     path: '',
     component: AdminWorkPageLayoutComponent,
@@ -19,11 +26,19 @@ const adminRoutes: Routes = [
         path: 'help-offers',
         component: AdminHelpOffersPageComponent,
       },
+      {
+        path: 'help-requests',
+        component: AdminHelpRequestsPageComponent,
+      },
+      {
+        path: 'users',
+        component: AdminUsersPageComponent,
+      },
+      {
+        path: 'settings',
+        component: AdminSettingsPageComponent,
+      },
     ],
-  },
-  {
-    path: 'login',
-    component: AdminLoginPageComponent,
   },
   {
     path: '**',
