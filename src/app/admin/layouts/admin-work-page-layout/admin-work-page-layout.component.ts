d31c5ec982552c7ccfc5c74ff39ed3eceb526eac
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-03 15:58:47 +0100

    Rework admin work page layout component

--- src/app/admin/layouts/admin-work-page-layout/admin-work-page-layout.component.ts
+++ src/app/admin/layouts/admin-work-page-layout/admin-work-page-layout.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { NavigationLinkType } from '../../../shared/types/navigation-link.type';
+
 @Component({
   selector: 'app-admin-work-page-layout',
   templateUrl: './admin-work-page-layout.component.html',
@@ -7,5 +9,26 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminWorkPageLayoutComponent {
-
+  public readonly navLinks: ReadonlyArray<NavigationLinkType> = [
+    {
+      label: 'Help offers',
+      path: '/admin/help-offers',
+      disabled: false,
+    },
+    {
+      label: 'Help requests',
+      path: '/admin/help-requests',
+      disabled: true,
+    },
+    {
+      label: 'Users',
+      path: '/admin/users',
+      disabled: true,
+    },
+    {
+      label: 'Settings',
+      path: '/admin/settings',
+      disabled: true,
+    },
+  ];
 }
