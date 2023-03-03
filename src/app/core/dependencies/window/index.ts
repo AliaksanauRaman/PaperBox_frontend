c9f0cb079661528011097fbf2efdd5a13f9a459f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-03 10:41:56 +0100

    Create WINDOW dependency

---
+++ src/app/core/dependencies/window/index.ts
@@ -0,0 +1,3 @@
+export * from './injection-token';
+export * from './type';
+export * from './value';
---
+++ src/app/core/dependencies/window/injection-token.ts
@@ -0,0 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
+import { WindowType } from './type';
+
+export const WINDOW = new InjectionToken<WindowType>('WINDOW');
