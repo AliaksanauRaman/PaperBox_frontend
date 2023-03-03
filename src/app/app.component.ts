c8b8d804f29a72630a0ce58d0d043d84f23ff490
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-03 10:46:14 +0100

    Add commands panel to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
 
 import { AppLanguagesService } from './core/services/app-languages.service';
+import { KeyboardService } from './core/services/keyboard.service';
+import { CommandsPanelService } from './core/commands/commands-panel.service';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +14,16 @@ import { AppLanguagesService } from './core/services/app-languages.service';
 export class AppComponent implements OnInit {
   constructor(
     private readonly languagesService: AppLanguagesService,
+    private readonly keyboardService: KeyboardService,
+    private readonly commandsPanelService: CommandsPanelService
   ) {}
 
   public ngOnInit(): void {
     this.languagesService.setUp();
+
+    this.keyboardService
+      .onKeyup(KeyboardService.KeyCode.F8)
+      .pipe(tap(() => this.commandsPanelService.show()))
+      .subscribe();
   }
 }
