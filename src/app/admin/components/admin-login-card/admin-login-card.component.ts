2ffda0d097b1696793937a3c5507bbb1d7e6f98e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-03 14:32:42 +0100

    Finish AdminLoginCardComponent implementation

--- src/app/admin/components/admin-login-card/admin-login-card.component.ts
+++ src/app/admin/components/admin-login-card/admin-login-card.component.ts
@@ -1,22 +1,53 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { tap } from 'rxjs';
+
+import { LoginService } from '../../../shared/services/login.service';
+
+import { LoginDto } from '../../../shared/dtos/login.dto';
 
 @Component({
   selector: 'app-admin-login-card',
   templateUrl: './admin-login-card.component.html',
   styleUrls: ['./admin-login-card.component.scss'],
+  providers: [LoginService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminLoginCardComponent {
+export class AdminLoginCardComponent implements OnDestroy {
+  public readonly loginState$ = this.loginService.state$.pipe(
+    tap((state) => {
+      if (state.inProgress) {
+        this.loginForm.disable();
+      } else {
+        this.loginForm.enable();
+      }
+    })
+  );
   public readonly loginForm = this.formBuilder.group({
-    username: ['', [Validators.required]],
+    number: ['', [Validators.required]],
     password: ['', [Validators.required]],
   });
 
-  constructor(private readonly formBuilder: NonNullableFormBuilder) {}
+  constructor(
+    private readonly formBuilder: NonNullableFormBuilder,
+    private readonly loginService: LoginService
+  ) {}
 
-  public handleAdminLoginSubmit(event: SubmitEvent): void {
-    // TODO
-    console.log(this.loginForm.getRawValue());
+  public ngOnDestroy(): void {
+    this.loginService.destroyRequest();
+  }
+
+  public handleAdminLoginSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const formValue = this.loginForm.getRawValue();
+    this.loginService.performRequest(
+      new LoginDto(
+        { diallingCode: '', number: formValue.number },
+        formValue.password
+      )
+    );
   }
 }
