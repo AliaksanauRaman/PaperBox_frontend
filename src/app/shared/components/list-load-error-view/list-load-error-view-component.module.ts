f3788c578baea43a1444b220ed4ef8042fa193cf
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-07 17:20:45 +0100

    Finish ListLoadErrorViewComponent implementation

--- src/app/shared/components/list-load-error-view/list-load-error-view-component.module.ts
+++ src/app/shared/components/list-load-error-view/list-load-error-view-component.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ListLoadErrorViewComponent } from './list-load-error-view.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   declarations: [ListLoadErrorViewComponent],
   providers: [],
   exports: [ListLoadErrorViewComponent],
--- src/app/shared/components/list-load-error-view/list-load-error-view.component.ts
+++ src/app/shared/components/list-load-error-view/list-load-error-view.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-list-load-error-view',
   templateUrl: './list-load-error-view.component.html',
   styleUrls: ['./list-load-error-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListLoadErrorViewComponent {
+  @Output()
+  public readonly reloadClick = new EventEmitter<void>();
 
+  public handleReloadButtonClick(): void {
+    this.reloadClick.emit();
+  }
 }
