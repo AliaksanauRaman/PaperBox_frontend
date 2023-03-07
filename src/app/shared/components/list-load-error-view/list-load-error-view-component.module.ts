a2aa6e646f8045eecdcfa391a0035969eae54687
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-07 13:04:01 +0100

    Create shared ListLoadErrorViewComponent

---
+++ src/app/shared/components/list-load-error-view/list-load-error-view-component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ListLoadErrorViewComponent } from './list-load-error-view.component';
+
+@NgModule({
+  imports: [],
+  declarations: [ListLoadErrorViewComponent],
+  providers: [],
+  exports: [ListLoadErrorViewComponent],
+})
+export class ListLoadErrorViewComponentModule {}
---
+++ src/app/shared/components/list-load-error-view/list-load-error-view.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-load-error-view',
+  templateUrl: './list-load-error-view.component.html',
+  styleUrls: ['./list-load-error-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListLoadErrorViewComponent {
+
+}
