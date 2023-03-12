1fd221c7d6ff9a7205bf21fc125128f2298cc55c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-12 12:29:17 +0100

    Enhance ListCardCommentComponent styles and layout, add show label functionality

--- src/app/shared/components/list-card-comment/list-card-comment.component.ts
+++ src/app/shared/components/list-card-comment/list-card-comment.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   selector: 'app-list-card-comment',
   templateUrl: './list-card-comment.component.html',
   styleUrls: ['./list-card-comment.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListCardCommentComponent {
   @Input()
@@ -16,5 +16,15 @@ export class ListCardCommentComponent {
     return this._text.trim();
   }
 
+  @Input()
+  public set showLabel(value: boolean) {
+    this._showLabel = value;
+  }
+
+  public get showLabel(): boolean {
+    return this._showLabel;
+  }
+
   private _text = '';
+  private _showLabel = true;
 }
