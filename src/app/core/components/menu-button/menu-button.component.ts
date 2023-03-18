85459fbc85210df11202b31c9f19768ae6082ee8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-18 11:41:28 +0100

    Remove redundant styles of MenuButtonComponent and set its encapsulation as None

--- src/app/core/components/menu-button/menu-button.component.ts
+++ src/app/core/components/menu-button/menu-button.component.ts
@@ -1,10 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-menu-button',
   templateUrl: './menu-button.component.html',
-  styleUrls: ['./menu-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class MenuButtonComponent {
   @Input()
