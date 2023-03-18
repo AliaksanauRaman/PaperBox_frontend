fe088f58f8bcc49e30b4d3cc3a7008c773b4ab68
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-18 12:25:06 +0100

    Finish ToolsComponent implementation, add isCompact and isUserLoggedIn @Input()-s, handleLeaveFeedbackButtonClick() methhod, open menu on account button click

--- src/app/core/components/tools/tools.component.ts
+++ src/app/core/components/tools/tools.component.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { CreateFeedbackDialogService } from '../../services/create-feedback-dialog.service';
 
 @Component({
   selector: 'app-tools',
   templateUrl: './tools.component.html',
   styleUrls: ['./tools.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolsComponent {
+  @Input()
+  public set isCompact(value: boolean) {
+    this._isCompact = value;
+  }
 
+  @Input()
+  public set isUserLoggedIn(value: boolean) {
+    this._isUserLoggedIn = value;
+  }
+
+  protected _isCompact = false;
+  protected _isUserLoggedIn = false;
+
+  constructor(
+    private readonly createFeedbackDialogService: CreateFeedbackDialogService
+  ) {}
+
+  public handleLeaveFeedbackButtonClick(): void {
+    this.createFeedbackDialogService.openDialog();
+  }
 }
