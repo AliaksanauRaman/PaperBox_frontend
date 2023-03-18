91679b7026d47c858478fee7ca17dbfa0258d0f0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-18 12:10:47 +0100

    Create LogoutService

---
+++ src/app/core/services/logout.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LogoutService {
+  public doLogout(): void {
+    // TODO
+    console.log('aga');
+  }
+}
