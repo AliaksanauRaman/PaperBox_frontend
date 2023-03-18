d8878f6b1028d9faebb0b6afd59035d4eb7e0790
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-18 11:39:22 +0100

    Remove redundant styles of AccountButtonComponent and set its encapsulation as None

--- src/app/core/components/account-button/account-button.component.ts
+++ src/app/core/components/account-button/account-button.component.ts
@@ -1,9 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-account-button',
   templateUrl: './account-button.component.html',
-  styleUrls: ['./account-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class AccountButtonComponent {}
