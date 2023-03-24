74b5f655c910049c8607cc4d10cf5163f20e23f0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-24 15:41:37 +0100

    Create ErrorNotificationService

---
+++ src/app/core/services/error-notification.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { ErrorSnackBarComponent } from '../components/error-snack-bar/error-snack-bar.component';
+
+const ERROR_NOTIFICATION_DURATION = 7000; // In milliseconds
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorNotificationService {
+  constructor(private readonly _matSnackBar: MatSnackBar) {}
+
+  public showMessage(messageTranslateKey: string): void {
+    this._matSnackBar.openFromComponent(ErrorSnackBarComponent, {
+      panelClass: 'app-error-snack-bar',
+      data: messageTranslateKey,
+      duration: ERROR_NOTIFICATION_DURATION,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+}
