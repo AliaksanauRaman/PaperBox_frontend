72d01ec46273de663e930796d2941cfd42d9cefd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-24 16:58:58 +0100

    Finish SuccessCreateHelpOfferSectionComponent implementation

--- src/app/core/sections/success-create-help-offer-section/success-create-help-offer-section.component.ts
+++ src/app/core/sections/success-create-help-offer-section/success-create-help-offer-section.component.ts
@@ -1,11 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-success-create-help-offer-section',
   templateUrl: './success-create-help-offer-section.component.html',
   styleUrls: ['./success-create-help-offer-section.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SuccessCreateHelpOfferSectionComponent {
+  @Input()
+  public set helpOfferPublicId(value: string) {
+    this._helpOfferPublicId = value;
+  }
 
+  @Output()
+  public readonly ok = new EventEmitter<void>();
+
+  protected _helpOfferPublicId = '';
+
+  protected handleOk(): void {
+    this.ok.emit();
+  }
 }
