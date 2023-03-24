e6ca2ee29b6299b34681674d14f589e633497f5a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-24 16:54:58 +0100

    Create InfoNotificationService

---
+++ src/app/core/services/info-notification.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { InfoSnackBarComponent } from '../components/info-snack-bar/info-snack-bar.component';
+
+const INFO_NOTIFICATION_DURATION = 7000; // In milliseconds
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoNotificationService {
+  constructor(private readonly _matSnackBar: MatSnackBar) {}
+
+  public showMessage(messageTranslateKey: string): void {
+    this._matSnackBar.openFromComponent(InfoSnackBarComponent, {
+      panelClass: 'app-info-snack-bar',
+      data: messageTranslateKey,
+      duration: INFO_NOTIFICATION_DURATION,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+}
