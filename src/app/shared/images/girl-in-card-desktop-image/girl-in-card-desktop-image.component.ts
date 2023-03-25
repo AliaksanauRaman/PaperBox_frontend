94fc311b52269a98384fd98d280520594092daf7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-25 13:01:25 +0100

    Create GirlInCardDesktopImageComponent

---
+++ src/app/shared/images/girl-in-card-desktop-image/girl-in-card-desktop-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-girl-in-card-desktop-image',
+  templateUrl: './girl-in-card-desktop-image.component.html',
+  styleUrls: ['./girl-in-card-desktop-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class GirlInCardDesktopImageComponent {}
