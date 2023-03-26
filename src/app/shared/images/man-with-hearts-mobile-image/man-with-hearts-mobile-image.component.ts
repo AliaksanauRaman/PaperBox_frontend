61ddc03359890d5f4dd3e0f8ad6b0b8060f07fb6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-26 13:00:13 +0200

    Create shared ManWithHeartsMobileImageComponent

---
+++ src/app/shared/images/man-with-hearts-mobile-image/man-with-hearts-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-with-hearts-mobile-image',
+  templateUrl: './man-with-hearts-mobile-image.component.html',
+  styleUrls: ['./man-with-hearts-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class ManWithHeartsMobileImageComponent {}
