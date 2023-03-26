d12b8306b45525054150aedcca240007886d283c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-26 12:43:35 +0200

    Create shared GirlWithHeartMobileImageComponent

---
+++ src/app/shared/images/girl-with-heart-mobile-image/girl-with-heart-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-girl-with-heart-mobile-image',
+  templateUrl: './girl-with-heart-mobile-image.component.html',
+  styleUrls: ['./girl-with-heart-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class GirlWithHeartMobileImageComponent {}
