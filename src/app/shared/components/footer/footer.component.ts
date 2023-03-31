52c6a288a90a847187e4b8c7c2efa136c193f3f2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-31 15:39:18 +0200

    Add leave feedback button to FooterComponent

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,10 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { LeaveFeedbackButtonComponent } from '../leave-feedback-button/leave-feedback-button.component';
+
+import { CreateFeedbackDialogService } from '../../../core/services/create-feedback-dialog.service';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [LeaveFeedbackButtonComponent],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor(
+    private readonly createFeedbackDialogService: CreateFeedbackDialogService
+  ) {}
+
+  protected handleLeaveFeedbackButtonClick(): void {
+    this.createFeedbackDialogService.openDialog();
+  }
+}
