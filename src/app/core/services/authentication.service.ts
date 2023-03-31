80b0e7648ba2a8e7a01c21b6baf7aa90444518f3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-31 15:09:36 +0200

    Create draft AuthenticationService

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+// TODO: !IMPORTANT Finish the service
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  public isUserLoggedIn(): boolean {
+    return false;
+  }
+}
