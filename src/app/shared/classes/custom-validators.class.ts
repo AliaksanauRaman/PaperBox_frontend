f2fd8aec1a204e113032e2fb99077693032664dc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-03-31 16:06:29 +0200

    Add passwordsMatchValidator to CustomValidators class

--- src/app/shared/classes/custom-validators.class.ts
+++ src/app/shared/classes/custom-validators.class.ts
@@ -1,11 +1,18 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormGroup, ValidationErrors } from '@angular/forms';
 
-import { emailValidator } from './../validators/email.validator';
+import { emailValidator } from '../validators/email.validator';
+import { passwordsMatchValidator } from '../validators/passwords-match.validator';
 
 export class CustomValidators {
   public static email(
     control: AbstractControl<string>
   ): ValidationErrors | null {
     return emailValidator(control);
+  }
+
+  public static passwordsMatch(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    return passwordsMatchValidator(control);
   }
 }
