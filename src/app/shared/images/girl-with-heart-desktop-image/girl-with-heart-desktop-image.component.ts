f352b9e1c0e89b04cb7bde09aef942a7b8eace8b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-03 18:46:35 +0200

    Create shared GirlWithHeartDesktopImageComponent

---
+++ src/app/shared/images/girl-with-heart-desktop-image/girl-with-heart-desktop-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-girl-with-heart-desktop-image',
+  templateUrl: './girl-with-heart-desktop-image.component.html',
+  styleUrls: ['./girl-with-heart-desktop-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class GirlWithHeartDesktopImageComponent {}
