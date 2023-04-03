9144069cbe96208195492f10f9bcd7b9f611c563
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-03 13:48:32 +0200

    Finish passwordsMatchValidator implementation

--- src/app/shared/validators/passwords-match.validator.ts
+++ src/app/shared/validators/passwords-match.validator.ts
@@ -1,5 +1,6 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
+// TODO: Probably better to make it more reusable
 export const passwordsMatchValidator = (
   control: AbstractControl
 ): ValidationErrors | null => {
@@ -10,13 +11,18 @@ export const passwordsMatchValidator = (
     throw new Error('The validator is not applicable!');
   }
 
-  if (passwordControl.invalid || confirmPasswordControl.invalid) {
+  if (
+    confirmPasswordControl.errors &&
+    !confirmPasswordControl.errors['passwordsMatch']
+  ) {
     return null;
   }
 
-  if (passwordControl.value === confirmPasswordControl.value) {
-    return null;
+  if (passwordControl.value !== confirmPasswordControl.value) {
+    confirmPasswordControl.setErrors({ passwordsMatch: true });
+  } else {
+    confirmPasswordControl.setErrors(null);
   }
 
-  return { passwordsMatch: true };
+  return null;
 };
