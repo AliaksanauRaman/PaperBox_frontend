755d55969a403b36971fc09ee37fd2a49f6e6677
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-04 14:28:55 +0200

    Extend AdminFullHelpRequestsListComponent with fullHelpRequestsList @Input

--- src/app/admin/components/admin-full-help-requests-list/admin-full-help-requests-list.component.ts
+++ src/app/admin/components/admin-full-help-requests-list/admin-full-help-requests-list.component.ts
@@ -1,11 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+import { FullHelpRequestType } from '../../types/full-help-request.type';
 
 @Component({
   selector: 'app-admin-full-help-requests-list',
   templateUrl: './admin-full-help-requests-list.component.html',
   styleUrls: ['./admin-full-help-requests-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminFullHelpRequestsListComponent {
+  @Input()
+  public set fullHelpRequestsList(value: ReadonlyArray<FullHelpRequestType>) {
+    this._fullHelpRequestsList = value;
+  }
 
+  public _fullHelpRequestsList: ReadonlyArray<FullHelpRequestType> = [];
 }
