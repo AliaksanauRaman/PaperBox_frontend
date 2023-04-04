785665e94768600ad24e97caea8b3fe6e1fa0913
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-04 17:43:03 +0200

    Create shared ManWithGlobeDesktopImageComponent

---
+++ src/app/shared/images/man-with-globe-desktop-image/man-with-globe-desktop-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-with-globe-desktop-image',
+  templateUrl: './man-with-globe-desktop-image.component.html',
+  styleUrls: ['./man-with-globe-desktop-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class ManWithGlobeDesktopImageComponent {}
