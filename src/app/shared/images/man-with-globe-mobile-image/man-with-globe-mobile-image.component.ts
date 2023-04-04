91975fa5c8d63216f7ae307ba734e6315101c8da
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-04 17:58:33 +0200

    Rename ManWithGlobeImageComponent to ManWithGlobeMobileImageComponent

---
+++ src/app/shared/images/man-with-globe-mobile-image/man-with-globe-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-with-globe-mobile-image',
+  templateUrl: './man-with-globe-mobile-image.component.html',
+  styleUrls: ['./man-with-globe-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class ManWithGlobeMobileImageComponent {}
