2b3155a0c2a6d1c70c8d977ccc8d0f20d4d0407b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-04 14:27:46 +0200

    Create AdminLoggedInGuard

---
+++ src/app/admin/guards/admin-logged-in.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+// TODO: Finish
+@Injectable()
+export class AdminLoggedInGuard implements CanActivate {
+  public canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return true;
+  }
+}
