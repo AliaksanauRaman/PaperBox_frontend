70d2f2c510099a6b0fed76c2a4f4dd5be4de5fbd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-05 11:11:24 +0200

    Create MainSectionLayoutComponent

---
+++ src/app/core/layouts/main-section-layout/main-section-layout.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-section-layout',
+  templateUrl: './main-section-layout.component.html',
+  styleUrls: ['./main-section-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainSectionLayoutComponent {}
