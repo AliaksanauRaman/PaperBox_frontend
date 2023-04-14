8f24cfdddee3f3fbb07f52a7700ace29742dd489
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-14 17:13:29 +0200

    Finish LogoutService

--- src/app/core/services/logout.service.ts
+++ src/app/core/services/logout.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import { UserTokenService } from '../../shared/services/user-token.service';
+import { InfoNotificationService } from './info-notification.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class LogoutService {
+  constructor(
+    private readonly _userTokenService: UserTokenService,
+    private readonly _infoNotificationService: InfoNotificationService
+  ) {}
+
   public doLogout(): void {
-    // TODO
-    console.log('aga');
+    this._userTokenService.removeUserToken();
+    this._infoNotificationService.showMessage('info.loggedOutSuccessfully');
   }
 }
