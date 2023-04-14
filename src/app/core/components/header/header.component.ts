9ae35799402c2ed51338bc22e4c1cadb54592d92
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-14 18:11:47 +0200

    Update HeaderComponent, handle userIsLoggedIn state

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { UserService } from '../../../shared/services/user.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(protected readonly _userService: UserService) {}
+}
