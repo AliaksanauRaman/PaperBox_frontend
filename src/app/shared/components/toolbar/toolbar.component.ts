fd2ae6b20666022a7447d21b4db1aa91153373e7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-14 17:00:44 +0200

    Update ToolbarComponent, remove isLoginFlow input, add isUserLoggedIn input, remove handleLeaveFeedbackButtonClick method and related dependencies

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -6,7 +6,6 @@ import { ToolsComponent } from '../tools/tools.component';
 import { MenuButtonComponent } from '../menu-button/menu-button.component';
 
 import { ScreenSizeObserverService } from '../../../core/services/screen-size-observer.service';
-import { CreateFeedbackDialogService } from '../../../core/services/create-feedback-dialog.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -18,23 +17,18 @@ import { CreateFeedbackDialogService } from '../../../core/services/create-feedb
 })
 export class ToolbarComponent {
   @Input()
-  public set isLoginFlow(value: boolean) {
-    this._isLoginFlow = value;
+  public set isUserLoggedIn(value: boolean) {
+    this._isUserLoggedIn = value;
   }
 
-  protected _isLoginFlow = false;
+  protected _isUserLoggedIn = false;
   protected _isMenuOpened = false;
   protected readonly _isMobileOrTablet$ =
-    this.screenSizeObserverService.isMobileOrTablet$;
+    this._screenSizeObserverService.isMobileOrTablet$;
 
   constructor(
-    private readonly screenSizeObserverService: ScreenSizeObserverService,
-    private readonly createFeedbackDialogService: CreateFeedbackDialogService
+    private readonly _screenSizeObserverService: ScreenSizeObserverService
   ) {}
-
-  protected handleLeaveFeedbackButtonClick(): void {
-    this.createFeedbackDialogService.openDialog();
-  }
 
   protected handleMenuButtonClick(): void {
     this._isMenuOpened = !this._isMenuOpened;
