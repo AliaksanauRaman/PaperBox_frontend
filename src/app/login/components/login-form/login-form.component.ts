ec0dc24f5ea1ed8b41c1704c3de4f1abaa2bb47f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-14 18:08:26 +0200

    Handle login requests and responses

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { tap } from 'rxjs';
+
+import { LoginService } from '../../../shared/services/login.service';
+import { SuccessLoginHandlerService } from '../../services/success-login-handler.service';
+import { ErrorNotificationService } from '../../../core/services/error-notification.service';
+import { LoginErrorFactory } from '../../factories/login-error.factory';
 
 import { CustomValidators } from '../../../shared/classes/custom-validators.class';
 
@@ -7,15 +13,35 @@ import { CustomValidators } from '../../../shared/classes/custom-validators.clas
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
+  providers: [LoginService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginFormComponent {
+  protected readonly _loginRequestState$ = this._loginService.state$.pipe(
+    tap((state) => {
+      if (state.inProgress) {
+        this._loginForm.disable();
+      } else if (state.data !== null) {
+        this._successLoginHandlerService.handle(state.data);
+      } else if (state.error !== null) {
+        this._loginForm.enable();
+        const loginError = this._loginErrorFactory.build(state.error);
+        this._errorNotificationService.showMessage(loginError.message);
+      }
+    })
+  );
   protected readonly _loginForm = this._formBuilder.group({
     email: ['', [Validators.required, CustomValidators.emailFormat]],
     password: ['', [Validators.required]],
   });
 
-  constructor(private readonly _formBuilder: NonNullableFormBuilder) {}
+  constructor(
+    private readonly _formBuilder: NonNullableFormBuilder,
+    private readonly _loginService: LoginService,
+    private readonly _successLoginHandlerService: SuccessLoginHandlerService,
+    private readonly _errorNotificationService: ErrorNotificationService,
+    private readonly _loginErrorFactory: LoginErrorFactory
+  ) {}
 
   protected handleLoginSubmit(event: SubmitEvent): void {
     if (!event.isTrusted) {
@@ -27,6 +53,6 @@ export class LoginFormComponent {
       throw new Error('Login form is invalid!');
     }
 
-    // TODO: !IMPORTANT Make http request
+    this._loginService.performRequest(this._loginForm.getRawValue());
   }
 }
