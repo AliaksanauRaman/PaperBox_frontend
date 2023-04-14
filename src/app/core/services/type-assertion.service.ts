f8835034d3f9f755bff409ccdf2d71cfd5dce80f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-14 16:54:57 +0200

    Add isNotNull method to TypeAssertionService, remove isDefined method

--- src/app/core/services/type-assertion.service.ts
+++ src/app/core/services/type-assertion.service.ts
@@ -3,9 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-// TODO: Unused
 export class TypeAssertionService {
-  public isDefined<T>(value: T): value is NonNullable<T> {
-    return value !== undefined && value !== null;
+  public isNotNull<T>(value: T): value is NonNullable<T> {
+    return value !== null;
   }
 }
