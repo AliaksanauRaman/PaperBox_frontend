59ef7486a70ffd6fef448eeffd8180dbdf6e778c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-15 13:22:21 +0200

    Enhance application set up, add loading user token from local storage, add user token issues notifier set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  Inject,
+} from '@angular/core';
 import { tap } from 'rxjs';
 
+import {
+  LOCAL_STORAGE,
+  LocalStorageType,
+} from './core/dependencies/local-storage';
 import { AppLanguagesService } from './core/services/app-languages.service';
 import { KeyboardService } from './core/services/keyboard.service';
 import { CommandsPanelService } from './core/commands/commands-panel.service';
+import { UserTokenIssuesNotifierService } from './shared/services/user-token-issues-notifier.service';
+import { UserTokenService } from './shared/services/user-token.service';
+
+import { LocalStorageKey } from './shared/enums/local-storage-key.enum';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +26,35 @@ import { CommandsPanelService } from './core/commands/commands-panel.service';
 })
 export class AppComponent implements OnInit {
   constructor(
-    private readonly languagesService: AppLanguagesService,
-    private readonly keyboardService: KeyboardService,
-    private readonly commandsPanelService: CommandsPanelService
+    @Inject(LOCAL_STORAGE) private readonly _localStorage: LocalStorageType,
+    private readonly _languagesService: AppLanguagesService,
+    private readonly _keyboardService: KeyboardService,
+    private readonly _commandsPanelService: CommandsPanelService,
+    private readonly _userTokenIssuesNotifierService: UserTokenIssuesNotifierService,
+    private readonly _userTokenService: UserTokenService
   ) {}
 
   public ngOnInit(): void {
-    this.languagesService.setUp();
+    this._userTokenIssuesNotifierService.startWatching();
+    this.tryToLoadUserTokenFromLocalStorage();
+    this.setUpListenerForCommandsPanel();
+    this._languagesService.setUp();
+  }
 
-    this.keyboardService
+  private tryToLoadUserTokenFromLocalStorage(): void {
+    const userToken = this._localStorage.getItem(LocalStorageKey.USER_TOKEN);
+
+    if (userToken === null) {
+      return;
+    }
+
+    this._userTokenService.setUserToken(userToken);
+  }
+
+  private setUpListenerForCommandsPanel(): void {
+    this._keyboardService
       .onKeyup(KeyboardService.KeyCode.F8)
-      .pipe(tap(() => this.commandsPanelService.show()))
+      .pipe(tap(() => this._commandsPanelService.show()))
       .subscribe();
   }
 }
