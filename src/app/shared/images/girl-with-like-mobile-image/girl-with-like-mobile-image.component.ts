8f0aaca0a00b2a9009157ca997b0513a4e7ca844
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-16 08:53:05 +0200

    Create GirlWithLikeMobileImageComponent

---
+++ src/app/shared/images/girl-with-like-mobile-image/girl-with-like-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-girl-with-like-mobile-image',
+  templateUrl: './girl-with-like-mobile-image.component.html',
+  styleUrls: ['./girl-with-like-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class GirlWithLikeMobileImageComponent {}
