6b53f2a44133af5e21ce748470362e81e5ff7483
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-16 08:14:39 +0200

    Import VisibilityToggleComponent to InputControlModule

--- src/app/shared/components/input-control/input-control.module.ts
+++ src/app/shared/components/input-control/input-control.module.ts
@@ -1,17 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { VisibilityToggleComponent } from '../visibility-toggle/visibility-toggle.component';
 import { InputControlComponent } from './input-control.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
-  declarations: [
-    InputControlComponent,
-  ],
-  exports: [
-    InputControlComponent,
-  ],
+  imports: [CommonModule, VisibilityToggleComponent],
+  declarations: [InputControlComponent],
+  exports: [InputControlComponent],
 })
 export class InputControlModule {}
