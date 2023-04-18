601b6bd8c8a45dc7e672c6de69c89902ecb74ea9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 13:43:02 +0200

    Create WelcomeGirlDesktopImageComponent

---
+++ src/app/shared/images/welcome-girl-desktop-image/welcome-girl-desktop-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-girl-desktop-image',
+  templateUrl: './welcome-girl-desktop-image.component.html',
+  styleUrls: ['./welcome-girl-desktop-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class WelcomeGirlDesktopImageComponent {}
