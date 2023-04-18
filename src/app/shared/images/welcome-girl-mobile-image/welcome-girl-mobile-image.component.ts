848417829c61e0b1f58c1374808d71f896db00a7
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 13:43:17 +0200

    Create WelcomeGirlMobileImageComponent

---
+++ src/app/shared/images/welcome-girl-mobile-image/welcome-girl-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-girl-mobile-image',
+  templateUrl: './welcome-girl-mobile-image.component.html',
+  styleUrls: ['./welcome-girl-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class WelcomeGirlMobileImageComponent {}
