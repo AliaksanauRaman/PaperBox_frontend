f677cfc428c30a41624dac454f46f16924501254
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 13:56:00 +0200

    Create FailedConfirmUserViewComponent

---
+++ src/app/login/views/failed-confirm-user-view/failed-confirm-user-view.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-failed-confirm-user-view',
+  templateUrl: './failed-confirm-user-view.component.html',
+  styleUrls: ['./failed-confirm-user-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FailedConfirmUserViewComponent {}
