b16235efa304c12e9fdcc668cabec4d9922d53d8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 14:07:22 +0200

    Add all new image and confirm user related components to LoginModule

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -11,13 +11,19 @@ import { GirlWithHeartMobileImageComponent } from '../shared/images/girl-with-he
 import { ManWithHeartsMobileImageComponent } from '../shared/images/man-with-hearts-mobile-image/man-with-hearts-mobile-image.component';
 import { GirlWithHeartDesktopImageComponent } from '../shared/images/girl-with-heart-desktop-image/girl-with-heart-desktop-image.component';
 import { ManWithHeartsDesktopImageComponent } from '../shared/images/man-with-hearts-desktop-image/man-with-hearts-desktop-image.component';
+import { WelcomeGirlMobileImageComponent } from '../shared/images/welcome-girl-mobile-image/welcome-girl-mobile-image.component';
+import { WelcomeGirlDesktopImageComponent } from '../shared/images/welcome-girl-desktop-image/welcome-girl-desktop-image.component';
+import { ManWithPhoneMobileImageComponent } from '../shared/images/man-with-phone-mobile-image/man-with-phone-mobile-image.component';
 import { InputControlModule } from '../shared/components/input-control/input-control.module';
 import { LoginRoutingModule } from './login-routing.module';
 
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { ConfirmUserPageComponent } from './pages/confirm-user-page/confirm-user-page.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { SuccessConfirmUserViewComponent } from './views/success-confirm-user-view/success-confirm-user-view.component';
+import { FailedConfirmUserViewComponent } from './views/failed-confirm-user-view/failed-confirm-user-view.component';
 
 @NgModule({
   imports: [
@@ -32,14 +38,20 @@ import { SignupFormComponent } from './components/signup-form/signup-form.compon
     ManWithHeartsMobileImageComponent,
     GirlWithHeartDesktopImageComponent,
     ManWithHeartsDesktopImageComponent,
+    WelcomeGirlMobileImageComponent,
+    WelcomeGirlDesktopImageComponent,
+    ManWithPhoneMobileImageComponent,
     InputControlModule,
     LoginRoutingModule,
   ],
   declarations: [
     LoginPageComponent,
     SignupPageComponent,
+    ConfirmUserPageComponent,
     LoginFormComponent,
     SignupFormComponent,
+    SuccessConfirmUserViewComponent,
+    FailedConfirmUserViewComponent,
   ],
   providers: [],
   exports: [],
