cc89bdecedb0d21c0de3d73e4829b98e2edf515f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 14:06:20 +0200

    Add ConfirmUserPageComponent and ConfirmUserResolver to LoginRoutingModule

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ConfirmUserResolver } from './resolvers/confirm-user.resolver';
+
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { ConfirmUserPageComponent } from './pages/confirm-user-page/confirm-user-page.component';
 
 const loginRoutes: Routes = [
   {
@@ -19,6 +22,14 @@ const loginRoutes: Routes = [
     path: 'signup',
     title: 'Pushka | Sign Up',
     component: SignupPageComponent,
+  },
+  {
+    path: 'confirm-user',
+    title: 'Pushka | Confirm user',
+    component: ConfirmUserPageComponent,
+    resolve: {
+      userConfirmationState: ConfirmUserResolver,
+    },
   },
 ];
 
