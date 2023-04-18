23bb5cb51078cbfe3b0122a487a601c68b394017
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 14:01:23 +0200

    Add isObject method to TypeAssertionService

--- src/app/core/services/type-assertion.service.ts
+++ src/app/core/services/type-assertion.service.ts
@@ -7,4 +7,8 @@ export class TypeAssertionService {
   public isNotNull<T>(value: T): value is NonNullable<T> {
     return value !== null;
   }
+
+  public isObject(value: unknown): value is Record<string, unknown> {
+    return typeof value === 'object' && value !== null;
+  }
 }
