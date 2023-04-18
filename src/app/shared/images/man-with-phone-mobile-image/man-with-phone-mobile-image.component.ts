4d21de63044c1f4c2cd7542bda2fe28c6acf0906
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 13:43:30 +0200

    Create ManWithPhoneMobileImageComponent

---
+++ src/app/shared/images/man-with-phone-mobile-image/man-with-phone-mobile-image.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-with-phone-mobile-image',
+  templateUrl: './man-with-phone-mobile-image.component.html',
+  styleUrls: ['./man-with-phone-mobile-image.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class ManWithPhoneMobileImageComponent {}
