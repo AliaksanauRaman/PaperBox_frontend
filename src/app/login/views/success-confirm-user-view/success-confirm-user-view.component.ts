1fc139e608d60a36f3eb9907d1f384a5162f1fd8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-18 13:51:57 +0200

    Create SuccessConfirmUserViewComponent

---
+++ src/app/login/views/success-confirm-user-view/success-confirm-user-view.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-confirm-user-view',
+  templateUrl: './success-confirm-user-view.component.html',
+  styleUrls: ['./success-confirm-user-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SuccessConfirmUserViewComponent {}
