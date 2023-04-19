274c1685322eccdef3dd5cda9b408de90b33a8bc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-19 12:58:44 +0200

    Update SignupHttpService so it returns an object as a result instead of null

--- src/app/shared/services/signup-http.service.ts
+++ src/app/shared/services/signup-http.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpStatusCode } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 import { API_URL } from '../dependencies/api-url';
 import { AppLanguagesService } from '../../core/services/app-languages.service';
@@ -24,10 +24,19 @@ export class SignupHttpService {
   ): Observable<SuccessSignupResponseDataType> {
     const params = new HttpParams();
     params.append('lang', this.languagesService.currentLanguage.value);
-    return this.httpClient.post<SuccessSignupResponseDataType>(
-      `${this.apiUrl}/api/registration`,
-      signUpDto,
-      { params }
-    );
+    return this.httpClient
+      .post<null>(`${this.apiUrl}/api/registration`, signUpDto, {
+        params,
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => {
+          if (response.status === HttpStatusCode.Ok) {
+            return { ok: true };
+          }
+
+          throw new Error('Unknown error');
+        })
+      );
   }
 }
