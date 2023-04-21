cd1a186b6bc4dc0f40b959ea5a95ecbc3ceb8a03
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-21 13:30:53 +0200

    Add matTooltip to LoginButtonComponent

--- src/app/shared/components/login-button/login-button.component.ts
+++ src/app/shared/components/login-button/login-button.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { WhiteSquareButtonComponent } from '../white-square-button/white-square-button.component';
 
@@ -14,6 +15,11 @@ import { WhiteSquareButtonComponent } from '../white-square-button/white-square-
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [NgTemplateOutlet, TranslateModule, WhiteSquareButtonComponent],
+  imports: [
+    NgTemplateOutlet,
+    TranslateModule,
+    MatTooltipModule,
+    WhiteSquareButtonComponent,
+  ],
 })
 export class LoginButtonComponent {}
