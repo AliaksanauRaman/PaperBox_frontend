ab4a331385c9d24d316a218510a918130a732656
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-24 08:37:17 +0200

    Create AboutUsThirdSectionComponent

---
+++ src/app/core/sections/about-us-third-section/about-us-third-section.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-third-section',
+  templateUrl: './about-us-third-section.component.html',
+  styleUrls: ['../../styles/about-us-section-common.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutUsThirdSectionComponent {}
