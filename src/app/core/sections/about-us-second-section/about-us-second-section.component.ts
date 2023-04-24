28aed8b28779374c4ef02b89aee0ddfca071816e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-24 08:37:04 +0200

    Create AboutUsSecondSectionComponent

---
+++ src/app/core/sections/about-us-second-section/about-us-second-section.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-second-section',
+  templateUrl: './about-us-second-section.component.html',
+  styleUrls: ['../../styles/about-us-section-common.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutUsSecondSectionComponent {}
