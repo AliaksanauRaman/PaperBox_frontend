f6f7622e67fda6c7ae32449c8ddc6c1139ca1071
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-24 08:37:33 +0200

    Create AboutUsFourthSectionComponent

---
+++ src/app/core/sections/about-us-fourth-section/about-us-fourth-section.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-fourth-section',
+  templateUrl: './about-us-fourth-section.component.html',
+  styleUrls: ['../../styles/about-us-section-common.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutUsFourthSectionComponent {}
