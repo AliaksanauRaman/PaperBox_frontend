944ecb68cfdaf1932acf2ce4ee0ff9292ebffa73
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-25 08:10:47 +0200

    Enhance DialogComponent class, make it abstract, add the possibility to resolve dialogs with a result

--- src/app/shared/abstracts/dialog-component.class.ts
+++ src/app/shared/abstracts/dialog-component.class.ts
@@ -1,9 +1,13 @@
 import { DialogRef } from '@angular/cdk/dialog';
 
-export class DialogComponent {
-  constructor(protected readonly dialogRef: DialogRef<void>) {}
+export abstract class DialogComponent<T = void> {
+  constructor(protected readonly _dialogRef: DialogRef<T>) {}
 
-  protected closeDialog(): void {
-    this.dialogRef.close();
+  protected closeDialog(result?: T): void {
+    if (result !== undefined) {
+      this._dialogRef.close(result);
+    } else {
+      this._dialogRef.close();
+    }
   }
 }
