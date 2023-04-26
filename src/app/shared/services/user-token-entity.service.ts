9030acacbabb998d7a086a46e295b912a9c1ef7e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-26 22:13:23 +0200

    Update UserTokenEntityService

--- src/app/shared/services/user-token-entity.service.ts
+++ src/app/shared/services/user-token-entity.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { BehaviorSubject, map, tap } from 'rxjs';
 
 import { UserTokenService } from './user-token.service';
 
@@ -12,24 +12,43 @@ import {
   providedIn: 'root',
 })
 export class UserTokenEntityService {
-  public readonly value$ = this._userTokenService.value$.pipe(
-    map((userToken) => {
-      if (userToken === null) {
-        return null;
-      }
-
-      const entity = new UserTokenEntity(userToken);
-      entity.decode();
-      return entity;
-    }),
-    tap((userTokenEntity) => (this._value = userTokenEntity))
-  );
-
-  public get value(): NullableUserTokenEntity {
-    return this._value;
-  }
-
-  private _value: NullableUserTokenEntity = null;
+  private readonly _value$ = new BehaviorSubject<NullableUserTokenEntity>(null);
+  public readonly value$ = this._value$.asObservable();
 
   constructor(private readonly _userTokenService: UserTokenService) {}
+
+  public setUp(): void {
+    this.subToUserTokenChanges();
+  }
+
+  public getValueOrNull(): NullableUserTokenEntity {
+    return this._value$.getValue();
+  }
+
+  public getValue(): UserTokenEntity {
+    const value = this.getValueOrNull();
+
+    if (value === null) {
+      throw new Error('An instance of UserTokenEntity is expected!');
+    }
+
+    return value;
+  }
+
+  private subToUserTokenChanges(): void {
+    this._userTokenService.value$
+      .pipe(
+        map((userToken) => {
+          if (userToken === null) {
+            return null;
+          }
+
+          const entity = new UserTokenEntity(userToken);
+          entity.decode();
+          return entity;
+        }),
+        tap((userTokenEntity) => this._value$.next(userTokenEntity))
+      )
+      .subscribe();
+  }
 }
