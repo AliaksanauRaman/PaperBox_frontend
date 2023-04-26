705336a3494478e0fd298387d6899a2ca8ca57a8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-26 22:27:01 +0200

    Update AppComponent, lightweight it and remove all redundant depenencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  Inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
 
-import {
-  LOCAL_STORAGE,
-  LocalStorageType,
-} from './core/dependencies/local-storage';
 import { AppLanguagesService } from './core/services/app-languages.service';
 import { KeyboardService } from './core/services/keyboard.service';
 import { CommandsPanelService } from './core/commands/commands-panel.service';
 import { UserTokenIssuesNotifierService } from './shared/services/user-token-issues-notifier.service';
-import { UserTokenService } from './shared/services/user-token.service';
-
-import { LocalStorageKey } from './shared/enums/local-storage-key.enum';
 
 @Component({
   selector: 'app-root',
@@ -26,29 +14,16 @@ import { LocalStorageKey } from './shared/enums/local-storage-key.enum';
 })
 export class AppComponent implements OnInit {
   constructor(
-    @Inject(LOCAL_STORAGE) private readonly _localStorage: LocalStorageType,
     private readonly _languagesService: AppLanguagesService,
     private readonly _keyboardService: KeyboardService,
     private readonly _commandsPanelService: CommandsPanelService,
-    private readonly _userTokenIssuesNotifierService: UserTokenIssuesNotifierService,
-    private readonly _userTokenService: UserTokenService
+    private readonly _userTokenIssuesNotifierService: UserTokenIssuesNotifierService
   ) {}
 
   public ngOnInit(): void {
     this._userTokenIssuesNotifierService.startWatching();
-    this.tryToLoadUserTokenFromLocalStorage();
     this.setUpListenerForCommandsPanel();
     this._languagesService.setUp();
-  }
-
-  private tryToLoadUserTokenFromLocalStorage(): void {
-    const userToken = this._localStorage.getItem(LocalStorageKey.USER_TOKEN);
-
-    if (userToken === null) {
-      return;
-    }
-
-    this._userTokenService.setUserToken(userToken);
   }
 
   private setUpListenerForCommandsPanel(): void {
