9affd62d420d8fa478f5b1d794cf3b3affbc62fa
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-26 21:40:34 +0200

    Update UserTokenService, make less dependencies and make it more lightweight

--- src/app/shared/services/user-token.service.ts
+++ src/app/shared/services/user-token.service.ts
@@ -1,33 +1,18 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
-import {
-  LOCAL_STORAGE,
-  LocalStorageType,
-} from '../../core/dependencies/local-storage';
-
-import { LocalStorageKey } from '../enums/local-storage-key.enum';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserTokenService {
   private readonly _value$ = new BehaviorSubject<string | null>(null);
-
   public readonly value$ = this._value$.asObservable();
 
-  constructor(
-    @Inject(LOCAL_STORAGE)
-    private readonly _localStorage: LocalStorageType
-  ) {}
-
-  public setUserToken(userToken: string): void {
-    this._localStorage.setItem(LocalStorageKey.USER_TOKEN, userToken);
+  public set(userToken: string): void {
     this._value$.next(userToken);
   }
 
-  public removeUserToken(): void {
-    this._localStorage.removeItem(LocalStorageKey.USER_TOKEN);
+  public remove(): void {
     this._value$.next(null);
   }
 }
