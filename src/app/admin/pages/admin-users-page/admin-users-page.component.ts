54b418030dde3894de1fd20e0b3af497bcb75005
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-28 14:32:08 +0200

    Make a default template for AdminUsersPageComponent

--- src/app/admin/pages/admin-users-page/admin-users-page.component.ts
+++ src/app/admin/pages/admin-users-page/admin-users-page.component.ts
@@ -3,9 +3,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-admin-users-page',
   templateUrl: './admin-users-page.component.html',
-  styleUrls: ['./admin-users-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: [
+    './admin-users-page.component.scss',
+    '../../styles/_admin-page-common.scss',
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminUsersPageComponent {
-
-}
+export class AdminUsersPageComponent {}
