2d655e6c461257a401ddbdfd576f0358ba0c3e20
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-04-29 21:25:58 +0200

    Create AdminApplicationsHttpService

---
+++ src/app/admin/services/admin-applications-http.service.ts
@@ -0,0 +1,90 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpStatusCode } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+import { API_URL } from '../../shared/dependencies/api-url';
+
+import { FullApplicationStatus } from '../enums/full-application-status.enum';
+import { FullApplicationListType } from '../types/full-application.type';
+import { UpdateApplicationStatusResponseDataType } from '../types/update-application-status-response-data.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminApplicationsHttpService {
+  constructor(
+    @Inject(API_URL)
+    private readonly _apiUrl: string,
+    private readonly _httpClient: HttpClient
+  ) {}
+
+  public getAllHelpOffers(): Observable<FullApplicationListType> {
+    return this._httpClient.get<FullApplicationListType>(
+      `${this._apiUrl}/admin/help-offers`,
+      { headers: this.buildDefaultHeaders() }
+    );
+  }
+
+  public getAllHelpRequests(): Observable<FullApplicationListType> {
+    return this._httpClient.get<FullApplicationListType>(
+      `${this._apiUrl}/admin/help-requests`,
+      { headers: this.buildDefaultHeaders() }
+    );
+  }
+
+  public updateHelpOfferStatus(
+    id: number,
+    newStatus: FullApplicationStatus
+  ): Observable<UpdateApplicationStatusResponseDataType> {
+    return this._httpClient
+      .patch<null>(
+        `${this._apiUrl}/api/help-offers/${id}`,
+        {
+          status: newStatus,
+        },
+        { headers: this.buildDefaultHeaders(), observe: 'response' }
+      )
+      .pipe(
+        map((response) => {
+          if (response.status === HttpStatusCode.Ok) {
+            return {
+              id,
+              ok: true,
+            };
+          }
+
+          throw new Error('Error during help offer status update!');
+        })
+      );
+  }
+
+  public updateHelpRequestStatus(
+    id: number,
+    newStatus: FullApplicationStatus
+  ): Observable<UpdateApplicationStatusResponseDataType> {
+    return this._httpClient
+      .patch<null>(
+        `${this._apiUrl}/api/help-requests/${id}`,
+        {
+          status: newStatus,
+        },
+        { headers: this.buildDefaultHeaders(), observe: 'response' }
+      )
+      .pipe(
+        map((response) => {
+          if (response.status === HttpStatusCode.Ok) {
+            return {
+              id,
+              ok: true,
+            };
+          }
+
+          throw new Error('Error during help request status update!');
+        })
+      );
+  }
+
+  private buildDefaultHeaders(): HttpHeaders {
+    return new HttpHeaders().set('showGlobalLoader', 'true');
+  }
+}
