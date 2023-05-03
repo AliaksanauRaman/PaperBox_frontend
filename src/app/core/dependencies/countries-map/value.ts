220ee27a9ca38d4cc16437963355841952d28d91
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-03 15:11:33 +0200

    Extend COUNTRIES_MAP_VALUE with UKRAINE and LITHUANIA

--- src/app/core/dependencies/countries-map/value.ts
+++ src/app/core/dependencies/countries-map/value.ts
@@ -13,9 +13,19 @@ const GEORGIA = {
   value: CountryValue.GEORGIA,
   label: 'country.georgia',
 };
+const UKRAINE = {
+  value: CountryValue.UKRAINE,
+  label: 'country.ukraine',
+};
+const LITHUANIA = {
+  value: CountryValue.LITHUANIA,
+  label: 'country.lithuania',
+};
 
 export const COUNTRIES_MAP_VALUE: CountriesMapType = new Map([
   [CountryValue.BELARUS, BELARUS],
   [CountryValue.POLAND, POLAND],
   [CountryValue.GEORGIA, GEORGIA],
+  [CountryValue.UKRAINE, UKRAINE],
+  [CountryValue.LITHUANIA, LITHUANIA],
 ]);
