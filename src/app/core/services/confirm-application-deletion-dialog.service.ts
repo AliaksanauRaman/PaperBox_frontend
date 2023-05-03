2538657bca6617346eb38a6e60e70715de59113b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-03 12:12:00 +0200

    Rename ConfirmDialogService to ConfirmApplicationDeletionDialogService, update it, remove data parameter

--- src/app/core/services/confirm-application-deletion-dialog.service.ts
+++ src/app/core/services/confirm-application-deletion-dialog.service.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
 import { Observable, map } from 'rxjs';
 
-import { ConfirmDialogComponent } from '../dialogs/confirm-dialog/confirm-dialog.component';
+import { ConfirmApplicationDeletionDialogComponent } from '../dialogs/confirm-application-deletion-dialog/confirm-application-deletion-dialog.component';
 
-import { ConfirmDialogDataType } from '../../shared/types/confirm-dialog-data.type';
 import { CUSTOM_DIALOG_PANEL_CLASS } from '../../shared/constants/custom-dialog-panel-class';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ConfirmDialogService {
+export class ConfirmApplicationDeletionDialogService {
   constructor(private readonly _dialog: Dialog) {}
 
-  public openDialog(data: ConfirmDialogDataType = {}): Observable<boolean> {
+  public openDialog(): Observable<boolean> {
     return this._dialog
-      .open<boolean>(ConfirmDialogComponent, {
+      .open<boolean>(ConfirmApplicationDeletionDialogComponent, {
         panelClass: CUSTOM_DIALOG_PANEL_CLASS,
         disableClose: true,
-        data,
       })
       .closed.pipe(
         map((dialogResult) => {
