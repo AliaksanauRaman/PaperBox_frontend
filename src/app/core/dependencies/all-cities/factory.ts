850987f7b43aedcc507da71be34106fb828e8441
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-03 15:12:51 +0200

    Update allCitiesFactory with ukrainianCities and lithuanianCities params

--- src/app/core/dependencies/all-cities/factory.ts
+++ src/app/core/dependencies/all-cities/factory.ts
@@ -1,12 +1,22 @@
 import { BelarusianCitiesType } from '../belarusian-cities';
 import { PolishCitiesType } from '../polish-cities';
 import { GeorgianCitiesType } from '../georgian-cities';
+import { UkrainianCitiesType } from '../ukrainian-cities';
+import { LithuanianCitiesType } from '../lithuanian-cities';
 import { AllCitiesType } from './type';
 
 export const allCitiesFactory = (
   belarusianCities: BelarusianCitiesType,
   polishCities: PolishCitiesType,
-  georgianCities: GeorgianCitiesType
+  georgianCities: GeorgianCitiesType,
+  ukrainianCities: UkrainianCitiesType,
+  lithuanianCities: LithuanianCitiesType
 ): AllCitiesType => {
-  return [...belarusianCities, ...polishCities, ...georgianCities];
+  return [
+    ...belarusianCities,
+    ...polishCities,
+    ...georgianCities,
+    ...ukrainianCities,
+    ...lithuanianCities,
+  ];
 };
