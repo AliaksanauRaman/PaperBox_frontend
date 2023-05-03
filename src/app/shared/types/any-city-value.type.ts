10f164ac3481190c51bcaf6f8b3f44a892999c28
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-03 15:09:49 +0200

    Extend AnyCityValueType with UkrainianCityValue and LithuanianCityValue

--- src/app/shared/types/any-city-value.type.ts
+++ src/app/shared/types/any-city-value.type.ts
@@ -1,8 +1,12 @@
 import { BelarusianCityValue } from '../enums/belarusian-city-value.enum';
 import { PolishCityValue } from '../enums/polish-city-value.enum';
 import { GeorgianCityValue } from '../enums/georgian-city-value.enum';
+import { UkrainianCityValue } from '../enums/ukrainian-city-value.enum';
+import { LithuanianCityValue } from '../enums/lithuanian-city-value.enum';
 
 export type AnyCityValueType =
   | BelarusianCityValue
   | PolishCityValue
-  | GeorgianCityValue;
+  | GeorgianCityValue
+  | UkrainianCityValue
+  | LithuanianCityValue;
