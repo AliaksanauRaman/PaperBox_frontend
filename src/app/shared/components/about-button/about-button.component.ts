0db9d80d83dea8d8f4b84af7d4ddaf769014ff2a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-06 16:17:05 +0200

    Enhance AboutButtonComponent so that the image there loads immediately

--- src/app/shared/components/about-button/about-button.component.ts
+++ src/app/shared/components/about-button/about-button.component.ts
@@ -1,19 +1,33 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ViewEncapsulation,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRippleModule } from '@angular/material/core';
 
-import { WhiteSquareButtonComponent } from '../white-square-button/white-square-button.component';
+import { ImagesService } from '../../services/images.service';
 
 @Component({
   selector: 'app-about-button',
   templateUrl: './about-button.component.html',
+  styleUrls: ['./about-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [TranslateModule, MatTooltipModule, WhiteSquareButtonComponent],
+  imports: [TranslateModule, MatTooltipModule, MatRippleModule],
 })
-export class AboutButtonComponent {}
+export class AboutButtonComponent implements AfterViewInit {
+  @ViewChild('aboutButtonRef')
+  private readonly _aboutButtonElementRef!: ElementRef<HTMLButtonElement>;
+
+  protected readonly _infoImage = this._imagesService.getInfoImage();
+
+  constructor(private readonly _imagesService: ImagesService) {}
+
+  public ngAfterViewInit(): void {
+    this._aboutButtonElementRef.nativeElement.append(this._infoImage);
+  }
+}
