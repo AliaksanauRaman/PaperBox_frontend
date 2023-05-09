08b7c57bea60f914d069c0c49680ffda9652b0a9
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-09 10:49:34 +0200

    Create shared HoverDirective

---
+++ src/app/shared/directives/hover.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHover]',
+  standalone: true,
+})
+export class HoverDirective {
+  @HostListener('mouseenter')
+  public handleHostMouseenter(): void {
+    this._isHovered = true;
+  }
+
+  @HostListener('mouseover')
+  public handleHostMouseover(): void {
+    if (!this._isHovered) {
+      this._isHovered = true;
+    }
+  }
+
+  @HostListener('mouseleave')
+  public handleHostMouseleave(): void {
+    this._isHovered = false;
+  }
+
+  public get isHovered(): boolean {
+    return this._isHovered;
+  }
+
+  private _isHovered = false;
+
+  public unhover(): void {
+    this._isHovered = false;
+  }
+}
