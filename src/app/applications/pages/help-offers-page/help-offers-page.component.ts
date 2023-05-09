2332057f8682c9196be9acb3a3f3a88e0f90bce5
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-09 15:08:52 +0200

    Provide help offers related dependencies in HelpOffersPageComponent

--- src/app/applications/pages/help-offers-page/help-offers-page.component.ts
+++ src/app/applications/pages/help-offers-page/help-offers-page.component.ts
@@ -1,18 +1,24 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { ActiveViewService } from '../../../core/services/active-view.service';
+import { GET_PUBLISHED_APPLICATIONS_STATE_SERVICE } from '../../dependencies/get-published-applications-state-service';
+import { GetPublishedHelpOffersService } from '../../services/get-published-help-offers-state.service';
+import { DELETE_APPLICATION_STATE_SERVICE } from '../../dependencies/delete-application-state-service';
+import { DeleteHelpOfferStateService } from '../../services/delete-help-offer-state.service';
 
 @Component({
   selector: 'app-help-offers-page',
   templateUrl: './help-offers-page.component.html',
   styleUrls: ['./help-offers-page.component.scss'],
+  providers: [
+    {
+      provide: GET_PUBLISHED_APPLICATIONS_STATE_SERVICE,
+      useClass: GetPublishedHelpOffersService,
+    },
+    {
+      provide: DELETE_APPLICATION_STATE_SERVICE,
+      useClass: DeleteHelpOfferStateService,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-// TODO: Finish
-export class HelpOffersPageComponent implements OnDestroy {
-  constructor(private readonly _activeViewService: ActiveViewService) {}
-
-  public ngOnDestroy(): void {
-    this._activeViewService.resetActiveView();
-  }
-}
+export class HelpOffersPageComponent {}
