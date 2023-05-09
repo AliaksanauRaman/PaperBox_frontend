51455609357f32e5079012e016448e8f69ccb22a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-09 15:09:18 +0200

    Provide help requests related dependencies in HelpRequestsPageComponent

--- src/app/applications/pages/help-requests-page/help-requests-page.component.ts
+++ src/app/applications/pages/help-requests-page/help-requests-page.component.ts
@@ -1,18 +1,24 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { ActiveViewService } from '../../../core/services/active-view.service';
+import { GET_PUBLISHED_APPLICATIONS_STATE_SERVICE } from '../../dependencies/get-published-applications-state-service';
+import { GetPublishedHelpRequestsStateService } from '../../services/get-published-help-requests-state.service';
+import { DELETE_APPLICATION_STATE_SERVICE } from '../../dependencies/delete-application-state-service';
+import { DeleteHelpRequestStateService } from '../../services/delete-help-request-state.service';
 
 @Component({
   selector: 'app-help-requests-page',
   templateUrl: './help-requests-page.component.html',
   styleUrls: ['./help-requests-page.component.scss'],
+  providers: [
+    {
+      provide: GET_PUBLISHED_APPLICATIONS_STATE_SERVICE,
+      useClass: GetPublishedHelpRequestsStateService,
+    },
+    {
+      provide: DELETE_APPLICATION_STATE_SERVICE,
+      useClass: DeleteHelpRequestStateService,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-// TODO: Finish
-export class HelpRequestsPageComponent implements OnDestroy {
-  constructor(private readonly _activeViewService: ActiveViewService) {}
-
-  public ngOnDestroy(): void {
-    this._activeViewService.resetActiveView();
-  }
-}
+export class HelpRequestsPageComponent {}
