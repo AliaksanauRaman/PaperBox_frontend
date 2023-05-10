64385be4f3f22d171d7609d0fc0c849143d94df2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-10 16:00:10 +0200

    Remove DELETE_APPLICATION_STATE_SERVICE provider from and add APPLICATION_TYPE provider to HelpRequestsPageComponent

--- src/app/applications/pages/help-requests-page/help-requests-page.component.ts
+++ src/app/applications/pages/help-requests-page/help-requests-page.component.ts
@@ -2,8 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { GET_PUBLISHED_APPLICATIONS_STATE_SERVICE } from '../../dependencies/get-published-applications-state-service';
 import { GetPublishedHelpRequestsStateService } from '../../services/get-published-help-requests-state.service';
-import { DELETE_APPLICATION_STATE_SERVICE } from '../../dependencies/delete-application-state-service';
-import { DeleteHelpRequestStateService } from '../../services/delete-help-request-state.service';
+import {
+  APPLICATION_TYPE,
+  ApplicationType,
+} from '../../dependencies/application-type';
 
 @Component({
   selector: 'app-help-requests-page',
@@ -15,8 +17,8 @@ import { DeleteHelpRequestStateService } from '../../services/delete-help-reques
       useClass: GetPublishedHelpRequestsStateService,
     },
     {
-      provide: DELETE_APPLICATION_STATE_SERVICE,
-      useClass: DeleteHelpRequestStateService,
+      provide: APPLICATION_TYPE,
+      useValue: ApplicationType.HELP_REQUEST,
     },
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
