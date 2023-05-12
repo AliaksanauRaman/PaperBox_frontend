f7a15fa5f4ec43d60358dfbefda24a6711aea61e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-12 12:02:09 +0200

    Create AdminFeedbacksHttpService

---
+++ src/app/admin/services/admin-feedbacks-http.service.ts
@@ -0,0 +1,60 @@
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpStatusCode } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+import { API_URL } from '../../shared/dependencies/api-url/injection-token';
+import { AdminHeadersService } from './admin-headers.service';
+
+import { FeedbackListType } from '../types/feedback.type';
+import { FeedbackStatus } from '../enums/feedback-status.enum';
+import { UpdateFeedbackStatusResponseDataType } from '../types/update-feedback-status-response-data.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminFeedbacksHttpService {
+  constructor(
+    @Inject(API_URL)
+    private readonly _apiUrl: string,
+    private readonly _httpClient: HttpClient,
+    private readonly _headersService: AdminHeadersService
+  ) {}
+
+  public getAll(): Observable<FeedbackListType> {
+    return this._httpClient.get<FeedbackListType>(
+      `${this._apiUrl}/admin/feedbacks`,
+      {
+        headers: this._headersService.buildDefault(),
+      }
+    );
+  }
+
+  public updateStatusOfOne(
+    feedbackId: number,
+    newStatus: FeedbackStatus
+  ): Observable<UpdateFeedbackStatusResponseDataType> {
+    const httpParams = new HttpParams().set('status', newStatus);
+    return this._httpClient
+      .patch<null>(
+        `${this._apiUrl}/admin/feedbacks/${feedbackId}`,
+        {},
+        {
+          headers: this._headersService.buildDefault(),
+          params: httpParams,
+          observe: 'response',
+        }
+      )
+      .pipe(
+        map((response) => {
+          if (response.status === HttpStatusCode.Ok) {
+            return {
+              id: feedbackId,
+              ok: true,
+            };
+          }
+
+          throw new Error('Error during feedback status update!');
+        })
+      );
+  }
+}
