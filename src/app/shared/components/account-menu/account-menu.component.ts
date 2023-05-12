6ec015fd969674a4ba1515ba94e5354ad2d3cd08
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-12 09:59:19 +0200

    Add Admin button for admin users only to the account menu component

--- src/app/shared/components/account-menu/account-menu.component.ts
+++ src/app/shared/components/account-menu/account-menu.component.ts
@@ -4,10 +4,15 @@ import {
   ViewEncapsulation,
   ViewChild,
 } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { MatMenuModule, MatMenu } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { LogoutService } from '../../../core/services/logout.service';
+import { UserService } from '../../services/user.service';
+import { RoutingService } from '../../../core/services/routing.service';
+import { UserRole } from '../../enums/user-role.enum';
 
 @Component({
   exportAs: 'accountMenu',
@@ -16,15 +21,26 @@ import { LogoutService } from '../../../core/services/logout.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [MatMenuModule, TranslateModule],
+  imports: [NgIf, AsyncPipe, MatMenuModule, MatIconModule, TranslateModule],
 })
 export class AccountMenuComponent {
   @ViewChild(MatMenu)
   public readonly menuRef!: MatMenu;
 
-  constructor(private readonly logoutService: LogoutService) {}
+  protected readonly _user$ = this._userService.value$;
+  protected readonly _UserRole = UserRole;
 
-  public handleLogoutMenuItemClick(): void {
-    this.logoutService.doLogout();
+  constructor(
+    private readonly _logoutService: LogoutService,
+    private readonly _userService: UserService,
+    private readonly _routingService: RoutingService
+  ) {}
+
+  protected handleAdminClick(): void {
+    this._routingService.navigateToAdminHome();
+  }
+
+  protected handleLogoutMenuItemClick(): void {
+    this._logoutService.doLogout();
   }
 }
