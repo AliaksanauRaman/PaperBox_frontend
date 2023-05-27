bfb58f412c8d26013133b165ef8287013b2cb383
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-27 13:15:37 +0200

    Extend FeedbackType with status field

--- src/app/admin/types/feedback.type.ts
+++ src/app/admin/types/feedback.type.ts
@@ -1,7 +1,10 @@
+import { FeedbackStatus } from '../enums/feedback-status.enum';
+
 export type FeedbackType = Readonly<{
   id: number;
   fullName: string;
   email: string;
   comment: string;
+  status: FeedbackStatus;
 }>;
 export type FeedbackListType = ReadonlyArray<FeedbackType>;
