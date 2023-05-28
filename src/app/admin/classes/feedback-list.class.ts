38bac255aa43fabbd27c62f0f3c782722273c262
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-05-28 20:43:00 +0200

    Create FeedbackList class

---
+++ src/app/admin/classes/feedback-list.class.ts
@@ -0,0 +1,47 @@
+import { BehaviorSubject } from 'rxjs';
+
+import { FeedbackListType } from '../types/feedback.type';
+import { FeedbackStatus } from '../enums/feedback-status.enum';
+
+export class FeedbackList {
+  private readonly _feedbackList$ = new BehaviorSubject<FeedbackListType>([]);
+
+  public readonly value$ = this._feedbackList$.asObservable();
+
+  constructor(initialValue: FeedbackListType) {
+    this._feedbackList$.next(initialValue);
+  }
+
+  public waitOne(feedbackId: number): void {
+    this.updateFeedbackStatus(feedbackId, FeedbackStatus.WAITING);
+  }
+
+  public processOne(feedbackId: number): void {
+    this.updateFeedbackStatus(feedbackId, FeedbackStatus.PROCESSED);
+  }
+
+  public deleteOne(feedbackId: number): void {
+    this.updateFeedbackStatus(feedbackId, FeedbackStatus.DELETED);
+  }
+
+  private updateFeedbackStatus(
+    feedbackId: number,
+    status: FeedbackStatus
+  ): void {
+    this._feedbackList$.next(
+      this._feedbackList$
+        .getValue()
+        .map((feedback) => {
+          if (feedback.id === feedbackId) {
+            return {
+              ...feedback,
+              status,
+            };
+          }
+
+          return feedback;
+        })
+        .filter(({ status }) => status !== FeedbackStatus.DELETED)
+    );
+  }
+}
