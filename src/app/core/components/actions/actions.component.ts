331ae5d6a86548ffa8c83f5a976e8497db1714d0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-12 22:10:41 +0200

    Replace UserService with UserIsLoggedInStateService in ActionsComponent

--- src/app/core/components/actions/actions.component.ts
+++ src/app/core/components/actions/actions.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
+import { UserIsLoggedInStateService } from '../../../state/user-is-logged-in/user-is-logged-in-state.service';
 import { ActivePageService } from '../../services/active-page.service';
 import { CreateHelpOfferDialogService } from '../../services/create-help-offer-dialog.service';
 import { CreateHelpRequestDialogService } from '../../services/create-help-request-dialog.service';
-import { UserService } from '../../../shared/services/user.service';
 import { LoginOrSignupDialogService } from '../../services/login-or-signup-dialog.service';
 
 import { Page } from '../../../shared/enums/page.enum';
@@ -15,6 +15,10 @@ import { Page } from '../../../shared/enums/page.enum';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActionsComponent {
+  private readonly _userIsLoggedInStateService = inject(
+    UserIsLoggedInStateService
+  );
+
   protected readonly _activePage$ = this._activePageService.value$;
   protected readonly _Page = Page;
 
@@ -22,12 +26,11 @@ export class ActionsComponent {
     private readonly _activePageService: ActivePageService,
     private readonly _createHelpOfferDialogService: CreateHelpOfferDialogService,
     private readonly _createHelpRequestDialogService: CreateHelpRequestDialogService,
-    private readonly _userService: UserService,
     private readonly _loginOrSignupDialogService: LoginOrSignupDialogService
   ) {}
 
   protected handleOfferHelpButtonClick(): void {
-    if (this._userService.isLoggedIn()) {
+    if (this._userIsLoggedInStateService.get()) {
       this._createHelpOfferDialogService.openDialog();
     } else {
       this._loginOrSignupDialogService.openDialog();
@@ -35,7 +38,7 @@ export class ActionsComponent {
   }
 
   protected handleFindHelpButtonClick(): void {
-    if (this._userService.isLoggedIn()) {
+    if (this._userIsLoggedInStateService.get()) {
       this._createHelpRequestDialogService.openDialog();
     } else {
       this._loginOrSignupDialogService.openDialog();
