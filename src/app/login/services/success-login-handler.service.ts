931808da1a097aaa5cd00619709ca4c3af82dfa5
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-12 22:16:52 +0200

    Replace UserTokenService with UserTokenStateService in SuccessLoginHandlerService

--- src/app/login/services/success-login-handler.service.ts
+++ src/app/login/services/success-login-handler.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { UserTokenService } from '../../shared/services/user-token.service';
+import { UserTokenStateService } from '../../state/user-token/user-token-state.service';
 import { RoutingService } from '../../core/services/routing.service';
 import { InfoNotificationService } from '../../core/services/info-notification.service';
 
@@ -11,13 +11,13 @@ import { SuccessLoginResponseDataType } from '../../shared/types/success-login-r
 })
 export class SuccessLoginHandlerService {
   constructor(
-    private readonly _userTokenService: UserTokenService,
+    private readonly _userTokenStateService: UserTokenStateService,
     private readonly _infoNotificationService: InfoNotificationService,
     private readonly _routingService: RoutingService
   ) {}
 
   public handle(data: SuccessLoginResponseDataType): void {
-    this._userTokenService.set(data.token);
+    this._userTokenStateService.set(data.token);
     this._infoNotificationService.showMessage('info.loggedInSuccessfully');
     this._routingService.navigateToHome();
   }
