58d9bb9447a9a37ae2e78164c791477fff0b2fe6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-12 21:50:48 +0200

    Create abstract NullableStateService class

---
+++ src/app/state/nullable-state.service.ts
@@ -0,0 +1,29 @@
+import { BehaviorSubject } from 'rxjs';
+
+export abstract class NullableStateService<T> {
+  private readonly _stream$ = new BehaviorSubject<T | null>(
+    this.getDefaultValue()
+  );
+  public readonly stream$ = this._stream$.asObservable();
+
+  constructor() {
+    this._stream$.subscribe((value) =>
+      console.log(`[${this.getStateName()}]`, value)
+    );
+  }
+
+  public abstract getStateName(): string;
+  public abstract getDefaultValue(): T | null;
+
+  public get(): T | null {
+    return this._stream$.getValue();
+  }
+
+  public set(value: T): void {
+    this._stream$.next(value);
+  }
+
+  public remove(): void {
+    this._stream$.next(null);
+  }
+}
