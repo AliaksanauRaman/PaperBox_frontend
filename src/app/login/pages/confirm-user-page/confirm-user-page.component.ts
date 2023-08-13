e45b9ff6f96c34d94c83e552fd4848344d1bfa6a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-13 09:49:07 +0200

    Make a use of ConfirmUserPageService in ConfirmUserPageComponent

--- src/app/login/pages/confirm-user-page/confirm-user-page.component.ts
+++ src/app/login/pages/confirm-user-page/confirm-user-page.component.ts
@@ -1,39 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
-import { TypeAssertionService } from '../../../core/services/type-assertion.service';
-
-import { ConfirmUserResponseDataType } from '../../../shared/types/confirm-user-response-data.type';
+import { ConfirmUserPageService } from '../../services/confirm-user-page.service';
 
 @Component({
   selector: 'app-confirm-user-page',
   templateUrl: './confirm-user-page.component.html',
   styleUrls: ['./confirm-user-page.component.scss'],
+  providers: [ConfirmUserPageService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConfirmUserPageComponent {
-  protected readonly _userConfirmationState$ = this._activatedRoute.data.pipe(
-    filter(this.isResolvedUserConfirmation.bind(this)),
-    map(({ userConfirmationState }) => userConfirmationState)
-  );
+  private readonly _confirmUserPageService = inject(ConfirmUserPageService);
 
-  constructor(
-    private readonly _activatedRoute: ActivatedRoute,
-    private readonly _typeAssertionService: TypeAssertionService
-  ) {}
-
-  private isResolvedUserConfirmation(
-    value: unknown
-  ): value is { userConfirmationState: ConfirmUserResponseDataType } {
-    if (
-      this._typeAssertionService.isObject(value) &&
-      this._typeAssertionService.isObject(value['userConfirmationState']) &&
-      typeof value['userConfirmationState']['ok'] === 'boolean'
-    ) {
-      return true;
-    }
-
-    throw new Error('Resolved user confirmation has wrong shape!');
-  }
+  protected readonly userConfirmationState$ =
+    this._confirmUserPageService.userConfirmationState$;
 }
