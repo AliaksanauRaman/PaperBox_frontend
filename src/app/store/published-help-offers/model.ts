ae24522d37551e4342750561f24451888f268573
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-18 16:18:02 +0200

    Create PublishedHelpOffersStateModel

---
+++ src/app/store/published-help-offers/model.ts
@@ -0,0 +1,5 @@
+import { AsyncData } from '@shared/classes/async-data.class';
+import { ListOfPublishedHelpOffersType } from '@shared/types/list-of-published-help-offers.type';
+
+export type PublishedHelpOffersStateModel =
+  AsyncData<ListOfPublishedHelpOffersType>;
