073571e29d64eb87d9f76454da4683fa19ea707b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-19 13:21:53 +0200

    Add PublishedHelpRequestEntity and ListOfPublishedHelpRequestEntities types

---
+++ src/app/shared/entities/published-help-request.entity.ts
@@ -0,0 +1,16 @@
+import { ListOfPhoneEntity } from './phone.entity';
+
+export type PublishedHelpRequestEntity = Readonly<{
+  id: number;
+  userId: number;
+  publicId: string;
+  locationFrom: string;
+  locationTo: string;
+  comment: string;
+  fullName: string;
+  phones: ListOfPhoneEntity;
+  startDate: Date;
+  endDate: Date | null;
+}>;
+export type ListOfPublishedHelpRequestEntities =
+  ReadonlyArray<PublishedHelpRequestEntity>;
