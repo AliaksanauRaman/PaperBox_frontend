86e9db1d312c76f2702f5754ce3e4fb90365ccc2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-20 22:58:38 +0200

    Enhance FoldComponent, add isUnfolded$ stream with actual value, add exportAs, aligh class names

--- src/app/shared/components/fold/fold.component.ts
+++ src/app/shared/components/fold/fold.component.ts
@@ -8,6 +8,8 @@ import {
   Renderer2,
   Input,
 } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 // TODO: Constants/Inputs
 const PADDING = 20;
@@ -18,8 +20,11 @@ const MARGIN = 22;
   templateUrl: './fold.component.html',
   styleUrls: ['./fold.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  exportAs: 'fold',
   standalone: true,
+  imports: [AsyncPipe],
 })
+// TODO: Cleanup and refactor
 export class FoldComponent implements AfterViewInit {
   @Input()
   public set disabled(value: boolean) {
@@ -38,6 +43,9 @@ export class FoldComponent implements AfterViewInit {
 
   protected _disabled = false;
   private _isUnfolded = false;
+  private readonly _isUnfolded$ = new BehaviorSubject<boolean>(false);
+
+  public readonly isUnfolded$ = this._isUnfolded$.asObservable();
 
   constructor(
     private readonly _elementRef: ElementRef<HTMLElement>,
@@ -51,12 +59,14 @@ export class FoldComponent implements AfterViewInit {
 
   public fold(): void {
     this._isUnfolded = false;
+    this._isUnfolded$.next(false);
     this.showOnlyStaticPart();
     this._cdRef.markForCheck();
   }
 
   public unfold(): void {
     this._isUnfolded = true;
+    this._isUnfolded$.next(true);
     this.showStaticAndFoldableParts();
     this._cdRef.markForCheck();
   }
