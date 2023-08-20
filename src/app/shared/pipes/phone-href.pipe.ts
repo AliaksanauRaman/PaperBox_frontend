207e7ba36a355c83a63fd4fb2232aebbf03be10e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-20 23:00:08 +0200

    Make PhoneHrefPipe to depend on PhoneEntity instead of PhoneType

--- src/app/shared/pipes/phone-href.pipe.ts
+++ src/app/shared/pipes/phone-href.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { PhoneType } from '../types/phone.type';
+import { PhoneEntity } from '../entities/phone.entity';
 
 @Pipe({
   name: 'phoneHref',
   standalone: true,
 })
 export class PhoneHrefPipe implements PipeTransform {
-  public transform({ diallingCode, number }: PhoneType): string {
+  public transform({ diallingCode, number }: PhoneEntity): string {
     return `tel:${diallingCode}${number}`;
   }
 }
