fb3570b1a78a367ca55a457ea5d8cd1ad580eec1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-20 23:21:20 +0200

    Create ApplicationCardPhonesComponent

---
+++ src/app/published-applications/components/application-card-phones/application-card-phones.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { PhoneHrefPipe } from '@shared/pipes/phone-href.pipe';
+import { PhoneViewPipe } from '@shared/pipes/phone-view.pipe';
+
+import { ListOfPhoneEntity } from '@shared/entities/phone.entity';
+
+@Component({
+  selector: 'app-application-card-phones',
+  templateUrl: './application-card-phones.component.html',
+  styleUrls: ['./application-card-phones.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NgIf, NgFor, TranslateModule, PhoneHrefPipe, PhoneViewPipe],
+})
+export class ApplicationCardPhonesComponent {
+  @Input()
+  public set listOfPhones(value: ListOfPhoneEntity) {
+    this._listOfPhones = value;
+  }
+
+  protected _listOfPhones: ListOfPhoneEntity = [];
+}
