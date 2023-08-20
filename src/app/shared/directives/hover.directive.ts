b4b6e23a2bd0cf8b76c745230dc303a6336cd41a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-20 22:59:22 +0200

    Enhance HoverDirective, add isHovered$ stream with actual value

--- src/app/shared/directives/hover.directive.ts
+++ src/app/shared/directives/hover.directive.ts
@@ -1,25 +1,30 @@
 import { Directive, HostListener } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Directive({
   selector: '[appHover]',
+  exportAs: 'hover',
   standalone: true,
 })
 export class HoverDirective {
   @HostListener('mouseenter')
   public handleHostMouseenter(): void {
     this._isHovered = true;
+    this._isHovered$.next(true);
   }
 
   @HostListener('mouseover')
   public handleHostMouseover(): void {
     if (!this._isHovered) {
       this._isHovered = true;
+      this._isHovered$.next(true);
     }
   }
 
   @HostListener('mouseleave')
   public handleHostMouseleave(): void {
     this._isHovered = false;
+    this._isHovered$.next(false);
   }
 
   public get isHovered(): boolean {
@@ -27,8 +32,12 @@ export class HoverDirective {
   }
 
   private _isHovered = false;
+  private readonly _isHovered$ = new BehaviorSubject<boolean>(false);
+
+  public readonly isHovered$ = this._isHovered$.asObservable();
 
   public unhover(): void {
     this._isHovered = false;
+    this._isHovered$.next(false);
   }
 }
