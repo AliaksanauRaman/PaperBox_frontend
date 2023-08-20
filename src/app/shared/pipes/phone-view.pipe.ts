7fb94474b8b26d3e5bb64c717c3b8278a72b84fd
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-20 23:00:25 +0200

    Make PhoneViewPipe to depend on PhoneEntity instead of PhoneType

--- src/app/shared/pipes/phone-view.pipe.ts
+++ src/app/shared/pipes/phone-view.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { PhoneType } from '../types/phone.type';
+import { PhoneEntity } from '../entities/phone.entity';
 
 @Pipe({
   name: 'phoneView',
   standalone: true,
 })
 export class PhoneViewPipe implements PipeTransform {
-  public transform({ diallingCode, number }: PhoneType): string {
+  public transform({ diallingCode, number }: PhoneEntity): string {
     return `${diallingCode} ${number}`;
   }
 }
