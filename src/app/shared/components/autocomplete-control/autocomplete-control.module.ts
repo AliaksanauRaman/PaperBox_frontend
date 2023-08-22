dae0774dac3386e4843498274bf692f3256b9eea
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-22 10:23:01 +0200

    Replace title with matTooltip in AutocompleteControlModule

--- src/app/shared/components/autocomplete-control/autocomplete-control.module.ts
+++ src/app/shared/components/autocomplete-control/autocomplete-control.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { MatRippleModule } from '@angular/material/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AutocompleteControlComponent } from './autocomplete-control.component';
 import { DestructureLocationLabelPipe } from './destructure-location-label.pipe';
 
 @NgModule({
-  imports: [CommonModule, OverlayModule, MatRippleModule, TranslateModule],
+  imports: [
+    CommonModule,
+    OverlayModule,
+    MatRippleModule,
+    MatTooltipModule,
+    TranslateModule,
+  ],
   declarations: [AutocompleteControlComponent, DestructureLocationLabelPipe],
   providers: [],
   exports: [AutocompleteControlComponent],
