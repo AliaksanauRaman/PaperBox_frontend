0199ac6d6a8d9fbb5296a9aa93e2ee69a54d1ce1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-22 14:59:19 +0200

    Update return type of getPublished method of HelpRequestsHttpServiceInterface

--- src/app/core/interfaces/help-requests-http-service.interface.ts
+++ src/app/core/interfaces/help-requests-http-service.interface.ts
@@ -1,12 +1,12 @@
 import { Observable } from 'rxjs';
 
-import { ListOfPublishedHelpRequestsType } from '../../shared/types/list-of-published-help-requests.type';
+import { ListOfPublishedApplicationEntities } from '../../shared/entities/published-application.entity';
 import { CreateHelpRequestDto } from '../../shared/dtos/create-help-request.dto';
 import { SuccessCreateHelpRequestResponseDataType } from '../../shared/types/success-create-help-request-response-data.type';
 import { DeleteHelpRequestResponseDataType } from '../../shared/types/delete-help-request-response-data.type';
 
 export interface HelpRequestsHttpServiceInterface {
-  getPublished(): Observable<ListOfPublishedHelpRequestsType>;
+  getPublished(): Observable<ListOfPublishedApplicationEntities>;
   createOne(
     createHelpRequestDto: CreateHelpRequestDto
   ): Observable<SuccessCreateHelpRequestResponseDataType>;
