014251f715fe501c4747a3d66be00523f4de0082
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-22 13:49:20 +0200

    Create first zod schemas, start with PhoneEntity and ListOfPhoneEntities

--- src/app/shared/entities/phone.entity.ts
+++ src/app/shared/entities/phone.entity.ts
@@ -1,5 +1,13 @@
-export type PhoneEntity = Readonly<{
-  diallingCode: string;
-  number: string;
-}>;
-export type ListOfPhoneEntity = ReadonlyArray<PhoneEntity>;
+import { z } from 'zod';
+
+export const phoneEntity = z
+  .object({
+    diallingCode: z.string(),
+    number: z.string(),
+  })
+  .strict()
+  .readonly();
+export const listOfPhoneEntities = z.array(phoneEntity).max(3).readonly();
+
+export type PhoneEntity = z.infer<typeof phoneEntity>;
+export type ListOfPhoneEntities = z.infer<typeof listOfPhoneEntities>;
