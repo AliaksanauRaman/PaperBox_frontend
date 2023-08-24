fec6afc46e6e02b759f287fa30b588c2156a10b2
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-24 19:11:52 +0200

    Replace LanguageSwitcherComponent with LanguageSelectionContainerComponent in ToolsComponent, remove isCompact @Input there, update related files

--- src/app/shared/components/tools/tools.component.ts
+++ src/app/shared/components/tools/tools.component.ts
@@ -3,7 +3,7 @@ import { NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 
-import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
+import { LanguageSelectionContainerComponent } from '@core/components/language-selection-container/language-selection-container.component';
 import { AboutButtonComponent } from '../about-button/about-button.component';
 import { AccountButtonComponent } from '../account-button/account-button.component';
 import { LoginButtonComponent } from '../login-button/login-button.component';
@@ -22,7 +22,7 @@ import { AboutUsDialogService } from '../../../core/services/about-us-dialog.ser
     RouterLink,
     RouterLinkActive,
     MatMenuModule,
-    LanguageSwitcherComponent,
+    LanguageSelectionContainerComponent,
     AboutButtonComponent,
     AccountButtonComponent,
     LoginButtonComponent,
@@ -31,16 +31,10 @@ import { AboutUsDialogService } from '../../../core/services/about-us-dialog.ser
 })
 export class ToolsComponent {
   @Input()
-  public set isCompact(value: boolean) {
-    this._isCompact = value;
-  }
-
-  @Input()
   public set userIsLoggedIn(value: boolean) {
     this._userIsLoggedIn = value;
   }
 
-  protected _isCompact = false;
   protected _userIsLoggedIn = false;
 
   constructor(private readonly _aboutUsDialogService: AboutUsDialogService) {}
