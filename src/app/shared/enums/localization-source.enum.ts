2f229b6043882fe324c29a9f805a8875ea0f192c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-24 18:31:15 +0200

    Create LocalizationSource enum

---
+++ src/app/shared/enums/localization-source.enum.ts
@@ -0,0 +1,7 @@
+export enum LocalizationSource {
+  NONE = 'None',
+  LOCAL_STORAGE = 'Local storage language',
+  BROWSER = 'Browser language',
+  DEFAULT = 'Default language',
+  SELECTED = 'Selected language',
+}
