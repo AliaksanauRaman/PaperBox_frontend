37c7808d15c50664194c17da811e8a3a2dadab66
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-25 11:49:24 +0200

    Update LOCALIZATIONS_DEFAULTS, determine initial language dynamically instaed of static set

--- src/app/store/localizations/defaults.ts
+++ src/app/store/localizations/defaults.ts
@@ -1,8 +1,15 @@
+import { determineInitialLanguage } from './determine-initial-language.util';
+
 import { LocalizationSource } from '@shared/enums/localization-source.enum';
 import { LocalizationLabel } from '@shared/enums/localization-label.enum';
 import { LocalizationLanguage } from '@shared/enums/localization-language.enum';
 import { LocalizationLocale } from '@shared/enums/localization-locale.enum';
 import { LocalizationsStateModel } from './model';
+
+const initialLanguage = determineInitialLanguage(
+  window.localStorage,
+  window.navigator
+);
 
 export const LOCALIZATIONS_DEFAULTS: LocalizationsStateModel = {
   source: LocalizationSource.NONE,
@@ -11,19 +18,19 @@ export const LOCALIZATIONS_DEFAULTS: LocalizationsStateModel = {
       label: LocalizationLabel.BY,
       language: LocalizationLanguage.BELARUSIAN,
       locale: LocalizationLocale.BY,
-      isSelected: true,
+      isSelected: initialLanguage === LocalizationLanguage.BELARUSIAN,
     },
     {
       label: LocalizationLabel.RU,
       language: LocalizationLanguage.RUSSIAN,
       locale: LocalizationLocale.RU,
-      isSelected: false,
+      isSelected: initialLanguage === LocalizationLanguage.RUSSIAN,
     },
     {
       label: LocalizationLabel.EN,
       language: LocalizationLanguage.ENGLISH,
       locale: LocalizationLocale.EN,
-      isSelected: false,
+      isSelected: initialLanguage === LocalizationLanguage.ENGLISH,
     },
   ],
 };
