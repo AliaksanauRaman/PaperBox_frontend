7cc6d623c4b32bbfaddea91f1bdbefbfcc46d2b0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-25 14:13:00 +0200

    Update CreateHelpOfferDialogComponent to depend on store instead of custom state class

--- src/app/core/dialogs/create-help-offer-dialog/create-help-offer-dialog.component.ts
+++ src/app/core/dialogs/create-help-offer-dialog/create-help-offer-dialog.component.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+} from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DialogRef } from '@angular/cdk/dialog';
-import { BehaviorSubject, tap } from 'rxjs';
+import { Store, Select } from '@ngxs/store';
+import { BehaviorSubject, tap, Observable } from 'rxjs';
 
-import { CreateHelpOfferService } from '../../services/create-help-offer.service';
 import { ErrorNotificationService } from '../../services/error-notification.service';
 
 import { DialogComponent } from '../../../shared/abstracts/dialog-component.class';
@@ -16,6 +21,12 @@ import {
   LOADING_TITLE,
   SUCCESS_TITLE,
 } from './create-help-offer-dialog.config';
+import {
+  PublishedHelpOffers,
+  PublishedHelpOffersState,
+} from '@store/published-help-offers';
+import { AsyncData } from '@shared/classes/async-data.class';
+import { PublishedHelpOffer } from '@shared/models/published-help-offer.model';
 
 @Component({
   selector: 'app-create-help-offer-dialog',
@@ -24,24 +35,33 @@ import {
     './create-help-offer-dialog.component.scss',
     '../../../styles/shared/_create-help-offer-and-request-dialogs-styles.scss',
   ],
-  providers: [CreateHelpOfferService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreateHelpOfferDialogComponent extends DialogComponent {
+export class CreateHelpOfferDialogComponent
+  extends DialogComponent
+  implements OnDestroy
+{
   private readonly _dialogTitle$ = new BehaviorSubject<string>(NORMAL_TITLE);
   public readonly dialogTitle$ = this._dialogTitle$.asObservable();
 
-  public readonly createHelpOfferState$ =
-    this.createHelpOfferService.state$.pipe(
+  private readonly _store = inject(Store);
+
+  @Select(PublishedHelpOffersState.createOne)
+  private readonly createOneHelpOfferState$!: Observable<
+    AsyncData<PublishedHelpOffer>
+  >;
+
+  protected readonly _createHelpOfferState$ =
+    this.createOneHelpOfferState$.pipe(
       tap((state) => {
-        if (state.inProgress) {
+        if (state.loading) {
           this._dialogTitle$.next(LOADING_TITLE);
           this.createHelpOfferForm.disable();
         } else if (state.error !== null) {
           this._dialogTitle$.next(NORMAL_TITLE);
           this.createHelpOfferForm.enable();
           this._errorNotificationService.showMessage('error.createHelpOffer');
-        } else if (state.data !== null) {
+        } else if (state.value !== null) {
           this._dialogTitle$.next(SUCCESS_TITLE);
         }
       })
@@ -76,11 +96,14 @@ export class CreateHelpOfferDialogComponent extends DialogComponent {
 
   constructor(
     private readonly formBuilder: FormBuilder,
-    private readonly createHelpOfferService: CreateHelpOfferService,
     private readonly _errorNotificationService: ErrorNotificationService,
     dialogRef: DialogRef<void>
   ) {
     super(dialogRef);
+  }
+
+  public ngOnDestroy(): void {
+    this._store.dispatch(new PublishedHelpOffers.ResetCreateOne());
   }
 
   public handleCreateHelpOfferSubmit(event: ShortSubmitEventType): void {
@@ -104,15 +127,17 @@ export class CreateHelpOfferDialogComponent extends DialogComponent {
   ): void {
     const { locations, date, comment, fullName, phones } = validFormValue;
 
-    this.createHelpOfferService.performRequest(
-      new CreateHelpOfferDto(
-        locations.from,
-        locations.to,
-        date.start,
-        date.end,
-        comment,
-        fullName,
-        phones
+    this._store.dispatch(
+      new PublishedHelpOffers.CreateOne(
+        new CreateHelpOfferDto(
+          locations.from,
+          locations.to,
+          date.start,
+          date.end,
+          comment,
+          fullName,
+          phones
+        )
       )
     );
   }
