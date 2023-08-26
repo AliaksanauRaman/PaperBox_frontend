86e30dfcaaa723e5af408f7df40d58486fda1810
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-26 17:45:50 +0200

    Add CreateOneApplicationState to STORE

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,9 +1,11 @@
 import { LocalizationsState } from './localizations';
 import { PublishedHelpOffersState } from './published-help-offers';
 import { PublishedHelpRequestsState } from './published-help-requests';
+import { CreateOneApplicationState } from './create-one-application';
 
 export const STORE = [
   LocalizationsState,
   PublishedHelpOffersState,
   PublishedHelpRequestsState,
+  CreateOneApplicationState,
 ];
