fec8b418e1a37cf8e96f4b3559e633e9fa5f47d6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-26 20:41:23 +0200

    Update CreateHelpOfferDialogService, dispatch PublishedHelpOffers.ResetCreateOne before opening the dialog

--- src/app/core/services/create-help-offer-dialog.service.ts
+++ src/app/core/services/create-help-offer-dialog.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
+import { Store } from '@ngxs/store';
+import { finalize } from 'rxjs';
 
 import { CreateHelpOfferDialogComponent } from '../dialogs/create-help-offer-dialog/create-help-offer-dialog.component';
+
+import { PublishedHelpOffers } from '@store/published-help-offers';
+import { CUSTOM_DIALOG_PANEL_CLASS } from '@shared/constants/custom-dialog-panel-class';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CreateHelpOfferDialogService {
-  constructor(private readonly dialog: Dialog) {}
+  private readonly _store = inject(Store);
+  private readonly _dialog = inject(Dialog);
 
   public openDialog(): void {
-    this.dialog.open(CreateHelpOfferDialogComponent, {
-      // TODO: Move to constants
-      panelClass: 'app-custom-dialog',
-    });
+    this._store
+      .dispatch(new PublishedHelpOffers.ResetCreateOne())
+      .pipe(
+        finalize(() =>
+          this._dialog.open(CreateHelpOfferDialogComponent, {
+            panelClass: CUSTOM_DIALOG_PANEL_CLASS,
+          })
+        )
+      )
+      .subscribe();
   }
 }
