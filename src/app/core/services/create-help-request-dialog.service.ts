3379881029726ebae6c8346df6ff1a821b4830d8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-26 20:36:39 +0200

    Update CreateHelpRequestDialogService, dispatch ResetCreateOneApplication before opening the dialog

--- src/app/core/services/create-help-request-dialog.service.ts
+++ src/app/core/services/create-help-request-dialog.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Dialog } from '@angular/cdk/dialog';
+import { Store } from '@ngxs/store';
+import { finalize } from 'rxjs';
 
 import { CreateHelpRequestDialogComponent } from '../dialogs/create-help-request-dialog/create-help-request-dialog.component';
+
+import { ResetCreateOneApplication } from '@store/create-one-application';
+import { CUSTOM_DIALOG_PANEL_CLASS } from '@shared/constants/custom-dialog-panel-class';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CreateHelpRequestDialogService {
-  constructor(private readonly dialog: Dialog) {}
+  private readonly _store = inject(Store);
+  private readonly _dialog = inject(Dialog);
 
   public openDialog(): void {
-    this.dialog.open(CreateHelpRequestDialogComponent, {
-      // TODO: Move to constants
-      panelClass: 'app-custom-dialog',
-    });
+    this._store
+      .dispatch(new ResetCreateOneApplication())
+      .pipe(
+        finalize(() =>
+          this._dialog.open(CreateHelpRequestDialogComponent, {
+            panelClass: CUSTOM_DIALOG_PANEL_CLASS,
+          })
+        )
+      )
+      .subscribe();
   }
 }
