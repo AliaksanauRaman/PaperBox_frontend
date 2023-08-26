7d2e928eda3967a4dcdb7f98b7d578e335c50387
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-08-26 17:43:45 +0200

    Create CreateOneApplicationStateModel

---
+++ src/app/store/create-one-application/model.ts
@@ -0,0 +1,5 @@
+import { AsyncData } from '@shared/classes/async-data.class';
+// TODO: PublishedApplication
+import { PublishedHelpRequest } from '@shared/models/published-help-request.model';
+
+export type CreateOneApplicationStateModel = AsyncData<PublishedHelpRequest>;
