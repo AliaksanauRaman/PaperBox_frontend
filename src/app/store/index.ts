9164379a31a4c1ce1268ba038e7e6bec8a025e41
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:53:04 +0200

    Expose places and add them to the STORE

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,6 @@
 export * from './localizations';
 export * from './published-help-offers';
 export * from './published-help-requests';
+export * from './places';
 
 export * from './store';
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -2,10 +2,12 @@ import { LocalizationsState } from './localizations';
 import { PublishedHelpOffersState } from './published-help-offers';
 import { PublishedHelpRequestsState } from './published-help-requests';
 import { CreateOneApplicationState } from './create-one-application';
+import { PlacesState } from './places';
 
 export const STORE = [
   LocalizationsState,
   PublishedHelpOffersState,
   PublishedHelpRequestsState,
   CreateOneApplicationState,
+  PlacesState,
 ];
