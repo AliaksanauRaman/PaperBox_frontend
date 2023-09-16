17edd8cb82e0d86d65d0d4d622a897046d5bc75f
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:54:43 +0200

    Update RedirectToAdminLoginCommand, make it @Injectable

--- src/app/core/commands/entries/redirect-to-admin-login.command.ts
+++ src/app/core/commands/entries/redirect-to-admin-login.command.ts
@@ -1,7 +1,12 @@
+import { Injectable } from '@angular/core';
+
 import { RoutingService } from '../../services/routing.service';
 
 import { CommandInterface } from '../command.interface';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class RedirectToAdminLoginCommand implements CommandInterface {
   public static readonly Label = 'admin:login';
 
