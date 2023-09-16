eb4955ef6d1117c2ac42e69d3d575abd498c0737
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:42:49 +0200

    Add PUSHKA_DEV_MODE to LocalStorageKey

--- src/app/shared/enums/local-storage-key.enum.ts
+++ src/app/shared/enums/local-storage-key.enum.ts
@@ -1,5 +1,6 @@
 export enum LocalStorageKey {
   PUSHKA_LANGUAGE = 'pushka-language',
+  PUSHKA_DEV_MODE = 'pushka-dev-mode',
   APP_CURRENT_LANGUAGE = 'app-current-language',
   APP_CURRENT_LOCALE = 'app-current-locale',
   USER_TOKEN = 'user-token',
