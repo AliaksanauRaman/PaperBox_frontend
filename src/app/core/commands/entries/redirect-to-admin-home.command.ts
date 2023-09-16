8d9bd6dac72ca19cee21ee78de59768d47327940
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:54:59 +0200

    Update RedirectToAdminHomeCommand, make it @Injectable

--- src/app/core/commands/entries/redirect-to-admin-home.command.ts
+++ src/app/core/commands/entries/redirect-to-admin-home.command.ts
@@ -1,7 +1,12 @@
+import { Injectable } from '@angular/core';
+
 import { RoutingService } from '../../services/routing.service';
 
 import { CommandInterface } from '../command.interface';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class RedirectToAdminHomeCommand implements CommandInterface {
   public static readonly Label = 'admin:home';
 
