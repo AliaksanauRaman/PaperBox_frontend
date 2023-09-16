e8a05c06057a86017b170619225682c740197144
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:56:27 +0200

    Refactor CommandsPanelService

--- src/app/core/commands/commands-panel.service.ts
+++ src/app/core/commands/commands-panel.service.ts
@@ -1,35 +1,13 @@
 import { Injectable, Injector, inject } from '@angular/core';
 
-import { RoutingService } from '../services/routing.service';
-
-import { CommandInterface } from './command.interface';
-import { RedirectToAdminHomeCommand } from './entries/redirect-to-admin-home.command';
-import { RedirectToAdminLoginCommand } from './entries/redirect-to-admin-login.command';
-import { RedirectToHomeCommand } from './entries/redirect-to-home.command';
-import { LogUserCommand } from './entries/log-user.command';
+import { COMMAND_MAP } from './command-map';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommandsPanelService {
   private readonly _injector = inject(Injector);
-  private readonly COMMAND_MAP = new Map<string, CommandInterface>([
-    [
-      RedirectToAdminHomeCommand.Label,
-      new RedirectToAdminHomeCommand(this._routingService),
-    ],
-    [
-      RedirectToAdminLoginCommand.Label,
-      new RedirectToAdminLoginCommand(this._routingService),
-    ],
-    [
-      RedirectToHomeCommand.Label,
-      new RedirectToHomeCommand(this._routingService),
-    ],
-    [LogUserCommand.Label, this._injector.get(LogUserCommand)],
-  ]);
-
-  constructor(private readonly _routingService: RoutingService) {}
+  private readonly _commandMap = inject(COMMAND_MAP);
 
   public show(): void {
     const rawInput = prompt('Run command:');
@@ -43,13 +21,13 @@ export class CommandsPanelService {
   }
 
   private executeCommand(commandLabel: string): void {
-    const command = this.COMMAND_MAP.get(commandLabel);
+    const command = this._commandMap[commandLabel];
 
     if (command === undefined) {
-      console.warn(`No command '${commandLabel}' found :(`);
+      alert(`No command '${commandLabel}' found :(`);
       return;
     }
 
-    command.execute();
+    this._injector.get(command).execute();
   }
 }
