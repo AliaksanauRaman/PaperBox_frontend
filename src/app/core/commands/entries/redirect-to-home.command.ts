4db572d5148ea800575e133cec7c3e6fc45d21d4
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:54:21 +0200

    Update RedirectToHomeCommand, make it @Injectable

--- src/app/core/commands/entries/redirect-to-home.command.ts
+++ src/app/core/commands/entries/redirect-to-home.command.ts
@@ -1,7 +1,12 @@
+import { Injectable } from '@angular/core';
+
 import { RoutingService } from '../../services/routing.service';
 
 import { CommandInterface } from '../command.interface';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class RedirectToHomeCommand implements CommandInterface {
   public static readonly Label = 'home';
 
