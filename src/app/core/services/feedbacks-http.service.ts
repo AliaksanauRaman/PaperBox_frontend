9300b4f1b8fc34b58cfc5c5e4bd2a4bf9e5b4eea
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:59:00 +0200

    Refactor FeedbacksHttpService, make a use of HttpService classand DevModeService

--- src/app/core/services/feedbacks-http.service.ts
+++ src/app/core/services/feedbacks-http.service.ts
@@ -1,44 +1,39 @@
-import { Inject, Injectable, isDevMode } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { DevModeService } from './dev-mode.service';
+
+import { HttpService } from '@shared/abstracts/http-service.class';
 import { FeedbacksHttpServiceInterface } from '../interfaces/feedbacks-http-service.interface';
 import { MockFeedbacksHttpService } from '../mocks/mock-feedbacks-http.service';
-import { API_URL } from '../../shared/dependencies/api-url/injection-token';
 import { CreateFeedbackDto } from '../../shared/dtos/create-feedback.dto';
 import { SuccessCreateFeedbackResponseDataType } from '../../shared/types/success-create-feedback-response-data.type';
 
 @Injectable({
   providedIn: 'root',
   useFactory: feedbacksHttpServiceFactory,
-  deps: [API_URL, HttpClient],
+  deps: [DevModeService],
 })
-export class FeedbacksHttpService implements FeedbacksHttpServiceInterface {
-  private readonly feedbacksApiUrl = `${this.apiUrl}/api/feedbacks`;
-
-  constructor(
-    @Inject(API_URL)
-    private readonly apiUrl: string,
-    private readonly httpClient: HttpClient
-  ) {}
-
+export class FeedbacksHttpService
+  extends HttpService
+  implements FeedbacksHttpServiceInterface
+{
   public createOne(
     createFeedbackDto: CreateFeedbackDto
   ): Observable<SuccessCreateFeedbackResponseDataType> {
-    return this.httpClient.post<SuccessCreateFeedbackResponseDataType>(
-      this.feedbacksApiUrl,
+    return this._httpClient.post<SuccessCreateFeedbackResponseDataType>(
+      `${this._apiUrl}/feedbacks`,
       createFeedbackDto
     );
   }
 }
 
 function feedbacksHttpServiceFactory(
-  apiUrl: string,
-  httpClient: HttpClient
+  devModeService: DevModeService
 ): FeedbacksHttpServiceInterface {
-  if (isDevMode()) {
+  if (devModeService.isOn()) {
     return new MockFeedbacksHttpService();
   }
 
-  return new FeedbacksHttpService(apiUrl, httpClient);
+  return new FeedbacksHttpService();
 }
