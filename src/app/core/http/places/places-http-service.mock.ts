630c17883e778b8fee2a0fcf388099b833312b87
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-16 15:46:25 +0200

    Create PlacesHttpServiceMock

---
+++ src/app/core/http/places/places-http-service.mock.ts
@@ -0,0 +1,20 @@
+import { Observable, of } from 'rxjs';
+
+import { PlacesHttpServiceInterface } from './places-http-service.interface';
+import { Place } from '@shared/types/place';
+import { Country } from '@shared/types/country';
+import { City } from '@shared/types/city';
+
+export class PlacesHttpServiceMock implements PlacesHttpServiceInterface {
+  public getPlaces(): Observable<ReadonlyArray<Place>> {
+    return of(MOCK_PLACES);
+  }
+}
+
+const BELARUS = new Country(1, 'country.belarus');
+const POLAND = new Country(2, 'country.poland');
+
+const MINSK = new City(1, BELARUS.id, 'city.belarusian.minsk');
+const WARSAW = new City(2, POLAND.id, 'city.polish.warsaw');
+
+const MOCK_PLACES = [new Place(BELARUS, MINSK), new Place(POLAND, WARSAW)];
