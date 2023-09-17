f6f85d3060af32233656349c4d817b25b967827b
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-17 18:39:10 +0200

    Create static null method in Country

--- src/app/shared/types/country.ts
+++ src/app/shared/types/country.ts
@@ -1,3 +1,7 @@
 export class Country {
+  public static null(): Country {
+    return new Country(-1, 'Unknown country');
+  }
+
   constructor(public readonly id: number, public readonly label: string) {}
 }
