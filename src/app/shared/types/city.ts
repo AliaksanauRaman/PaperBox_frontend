6973bcc6362bd54638ac37eba1bae38ff777a10c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-17 18:39:03 +0200

    Create static null method in City

--- src/app/shared/types/city.ts
+++ src/app/shared/types/city.ts
@@ -1,4 +1,8 @@
 export class City {
+  public static null(): City {
+    return new City(-1, -1, 'Unknown city');
+  }
+
   constructor(
     public readonly id: number,
     public readonly countryId: number,
