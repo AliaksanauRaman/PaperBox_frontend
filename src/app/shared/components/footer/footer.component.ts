59c2ce2c9854efa2516ffb01bf2850a24ea2c262
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-17 11:35:55 +0200

    Add the possibility to open command panel on Pushka dblckick in footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { LeaveFeedbackButtonComponent } from '../leave-feedback-button/leave-feedback-button.component';
 
+import { CommandsPanelService } from '@core/commands/commands-panel.service';
 import { CreateFeedbackDialogService } from '../../../core/services/create-feedback-dialog.service';
 import { UserStateService } from '../../../state/user/user-state.service';
 
@@ -14,10 +15,15 @@ import { UserStateService } from '../../../state/user/user-state.service';
   imports: [LeaveFeedbackButtonComponent],
 })
 export class FooterComponent {
+  private readonly _commandPanelService = inject(CommandsPanelService);
   private readonly _userStateService = inject(UserStateService);
   private readonly _createFeedbackDialogService = inject(
     CreateFeedbackDialogService
   );
+
+  protected handlePushkaDoubleClick(): void {
+    this._commandPanelService.show();
+  }
 
   protected handleLeaveFeedbackButtonClick(): void {
     this._createFeedbackDialogService.openDialog(
