946838e260568c2419bae566eafc738b40432bdb
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2023-09-19 17:30:05 +0200

    Create abstract BaseReactiveField directive

---
+++ src/app/shared/base/base-reactive-field.directive.ts
@@ -0,0 +1,28 @@
+import { signal, Directive } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+
+@Directive()
+export abstract class BaseReactiveField<T = unknown>
+  implements ControlValueAccessor
+{
+  protected readonly _value = signal(this.getDefaultValue());
+  protected readonly _isDisabled = signal(false);
+
+  public abstract writeValue(value: unknown): void;
+
+  public registerOnChange(fn: (value: T) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this._isDisabled.set(isDisabled);
+  }
+
+  protected abstract getDefaultValue(): T;
+  protected onChange(value: T): void {}
+  protected onTouched(): void {}
+}
